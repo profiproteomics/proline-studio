cdad6418d0b6b89e576c5b49c8c308903626c461
Veronique Dupierris <veronique.dupierris@cea.fr>
2012-08-10 11:40:55 +0000

    MAJ des dependances

---
+++ ProlineStudio-IDFImport/src/main/java/fr/proline/studio/idfimport/IdentificationImporter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.idfimport;
+
+/**
+ *
+ * @author VD225637
+ */
+public class IdentificationImporter {
+    //TODO migration de ImportResultFile de COMMONS
+}

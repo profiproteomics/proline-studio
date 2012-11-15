55581cf0fb4ba31ceb434197c17038a967cceb2b
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2012-11-15 08:13:50 +0000

    - loading of a Spectrum

---
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/DatabaseLoadSpectrumsTask.java
@@ -0,0 +1,84 @@
+package fr.proline.studio.dam.tasks;
+
+import fr.proline.core.orm.msi.MsQuery;
+import fr.proline.core.orm.msi.PeptideMatch;
+import fr.proline.core.orm.msi.Spectrum;
+import fr.proline.repository.ProlineRepository;
+import fr.proline.studio.dam.AccessDatabaseThread;
+import fr.proline.studio.repositorymgr.ProlineDBManagement;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+/**
+ * Load Spectrum from a MsQuery
+ *
+ * @author JM235353
+ */
+public class DatabaseLoadSpectrumsTask extends AbstractDatabaseTask {
+
+    private PeptideMatch peptideMatch = null;
+
+    public DatabaseLoadSpectrumsTask(AbstractDatabaseCallback callback, PeptideMatch peptideMatch) {
+        super(callback);
+        this.peptideMatch = peptideMatch;
+    }
+
+    @Override
+    public boolean needToFetch() {
+        return ((peptideMatch.getTransientData() == null) ||
+                (! peptideMatch.getTransientData().getIsMsQuerySet()) ||
+                (! peptideMatch.getMsQuery().getTransientIsSpectrumSet()));
+    }
+
+    @Override
+    public boolean fetchData() {
+
+        
+        EntityManager entityManagerMSI = ProlineDBManagement.getProlineDBManagement().getProjectEntityManager(ProlineRepository.Databases.MSI, true, AccessDatabaseThread.getProjectIdTMP());  //JPM.TODO : project id
+        try {
+
+            entityManagerMSI.getTransaction().begin();
+
+            // Load MsQuery if needed
+            if ((peptideMatch.getTransientData() == null) ||
+                (! peptideMatch.getTransientData().getIsMsQuerySet())) {
+
+               TypedQuery<MsQuery> msQueryQuery = entityManagerMSI.createQuery("SELECT ms FROM fr.proline.core.orm.msi.PeptideMatch pm, fr.proline.core.orm.msi.MsQuery ms WHERE pm.msQuery=ms AND pm.id=:peptideMatchId", MsQuery.class);
+           
+               msQueryQuery.setParameter("peptideMatchId", peptideMatch.getId());
+
+                MsQuery msq = msQueryQuery.getSingleResult();
+                peptideMatch.setMsQuery(msq);
+                if (peptideMatch.getTransientData() == null) {
+                    peptideMatch.setTransientData(new PeptideMatch.TransientData());
+                }
+                peptideMatch.getTransientData().setIsMsQuerySet(true);
+                
+            }
+            
+
+            // Load Spectrum
+            MsQuery msQuery = peptideMatch.getMsQuery();
+            
+            TypedQuery<Spectrum> spectrumQuery = entityManagerMSI.createQuery("SELECT sp FROM fr.proline.core.orm.msi.MsQuery ms, fr.proline.core.orm.msi.Spectrum sp WHERE ms.spectrum=sp AND ms.id=:MsQueryId", Spectrum.class);
+           
+            spectrumQuery.setParameter("MsQueryId", msQuery.getId());
+
+            Spectrum s = spectrumQuery.getSingleResult();
+            msQuery.setSpectrum(s);
+            msQuery.setTransientIsSpectrumSet(true);
+            
+            
+            entityManagerMSI.getTransaction().commit();
+        } catch (RuntimeException e) {
+            logger.error(getClass().getSimpleName()+" failed", e);
+            return false;
+        } finally {
+            entityManagerMSI.close();
+        }
+
+ 
+        
+        return true;
+    }
+}

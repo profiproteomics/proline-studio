c839f14efdd2cb7b0f77b51bbd063a7255b8a6a2
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2012-12-19 13:36:01 +0000

    validation of identifications

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/ValidationDialog.java
@@ -0,0 +1,45 @@
+package fr.proline.studio.rsmexplorer.gui.dialog;
+
+import fr.proline.studio.gui.DefaultDialog;
+import java.awt.Dialog;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Window;
+
+/**
+ * Dialog used to validate an identification and ask the creation
+ * of a Result Summary from a Result Set
+ * @author JM235353
+ */
+public class ValidationDialog extends DefaultDialog {
+
+    private ValidationPanel validationPanel = null;
+    
+    public ValidationDialog(Window parent) {
+        super(parent, Dialog.ModalityType.APPLICATION_MODAL);
+
+
+        setTitle("Identification Validation");
+
+
+        validationPanel = new ValidationPanel();
+
+        setInternalComponent(validationPanel);
+
+
+    }
+    
+    @Override
+    protected boolean okCalled() {
+        //JPM.TODO
+        
+        return true;
+    }
+    
+    @Override
+    protected boolean defaultCalled() {
+        validationPanel.initDefaults();
+        
+        return false;
+    }
+}

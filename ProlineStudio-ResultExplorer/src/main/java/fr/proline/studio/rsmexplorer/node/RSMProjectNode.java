5f5d708ef0da1eb63c1889d0aa6ee80491565766
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-03-22 10:43:35 +0000

    modified rename method

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/node/RSMProjectNode.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/node/RSMProjectNode.java
@@ -1,10 +1,15 @@
 package fr.proline.studio.rsmexplorer.node;
 
 import fr.proline.core.orm.uds.Project;
+import fr.proline.studio.dam.AccessDatabaseThread;
 import fr.proline.studio.dam.data.AbstractData;
 import fr.proline.studio.dam.data.ProjectData;
+import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
+import fr.proline.studio.dam.tasks.DatabaseProjectTask;
+import fr.proline.studio.dam.tasks.SubTask;
 import fr.proline.studio.utils.IconManager;
 import javax.swing.ImageIcon;
+import javax.swing.tree.DefaultTreeModel;
 
 /**
  * Node for the opened Projects
@@ -27,6 +32,40 @@ public class RSMProjectNode extends RSMNode {
         return getIcon(IconManager.IconType.PROJECT);
     }
 
+    public void rename(final String newName) {
+        
+        final Project project = getProject();
+        String name = project.getName();
+        
+        if ((newName != null) && (newName.compareTo(name) != 0)) {
+            setIsChanging(true);
+            project.setName(newName + "...");
+            ((DefaultTreeModel) RSMTree.getTree().getModel()).nodeChanged(this);
+
+            final RSMProjectNode projectNode = this;
+            
+            AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
+
+                @Override
+                public boolean mustBeCalledInAWT() {
+                    return true;
+                }
+
+                @Override
+                public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
+                    setIsChanging(false);
+                    project.setName(newName);
+                    ((DefaultTreeModel) RSMTree.getTree().getModel()).nodeChanged(projectNode);
+                }
+            };
 
 
+            // ask asynchronous loading of data
+            DatabaseProjectTask task = new DatabaseProjectTask(callback);
+            task.initRenameProject(project.getId(), newName);
+            AccessDatabaseThread.getAccessDatabaseThread().addTask(task);
+        }
+    }
+
+    
 }

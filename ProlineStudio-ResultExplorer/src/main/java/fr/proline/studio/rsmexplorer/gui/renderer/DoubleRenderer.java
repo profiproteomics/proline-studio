0c3416b164ff0f04cc3586d6fcf439a01f483e97
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-03-22 10:31:05 +0000

    modifications of renderer : to correctly render different type of numerical values at right

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/DoubleRenderer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/DoubleRenderer.java
@@ -1,28 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package fr.proline.studio.rsmexplorer.gui.renderer;
 
 import fr.proline.studio.utils.DataFormat;
 import java.awt.Component;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
 /**
- * Renderer for a Float Value in a Table Cell which is displayed as 0.00
+ *
  * @author JM235353
  */
-public class FloatRenderer implements TableCellRenderer {
+public class DoubleRenderer implements TableCellRenderer  {
     
     TableCellRenderer defaultRenderer;
     
-    public FloatRenderer(TableCellRenderer defaultRenderer) {
+    public DoubleRenderer(TableCellRenderer defaultRenderer) {
         this.defaultRenderer = defaultRenderer;
     }
     
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
-        Float f = (Float) value;
-        String formatedValue = ((f == null) || (f.isNaN())) ? "" : DataFormat.format(f.floatValue(), 2);
+
+        Double d = (Double) value;
+        String formatedValue = (d != null) ? DataFormat.format(d.doubleValue(), 2) : "";
 
         
 

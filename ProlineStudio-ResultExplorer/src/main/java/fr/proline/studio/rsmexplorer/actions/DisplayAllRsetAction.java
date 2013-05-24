405d34b20fa2e42f48f96572791032bb844bf5ba
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-05-24 09:00:28 +0000

    modifications for TaskList window

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/DisplayAllRsetAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/DisplayAllRsetAction.java
@@ -6,7 +6,10 @@ import fr.proline.studio.pattern.WindowBoxFactory;
 import fr.proline.studio.rsmexplorer.DataBoxViewerTopComponent;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
 import fr.proline.studio.rsmexplorer.node.RSMProjectNode;
+import java.util.Set;
 import org.openide.util.NbBundle;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
 
 /**
  *
@@ -27,19 +30,40 @@ public class DisplayAllRsetAction extends AbstractRSMAction {
         
         Project p = projectNode.getProject();
         
-        // prepare window box
-        WindowBox wbox = WindowBoxFactory.getAllResultSetWindowBox("All Search Results");
-        wbox.setEntryData(p.getId(), p);
+        String windowName = p.getName()+" : All Imported";
+        
 
-        // open a window to display the window box
-        DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
-        win.open();
-        win.requestActive();
+        TopComponent tc = findTopComponent(windowName);
+        if (tc != null) {
+            tc.requestActive();
+        } else {
+        
+            // prepare window box
+            WindowBox wbox = WindowBoxFactory.getAllResultSetWindowBox(windowName);
+            wbox.setEntryData(p.getId(), p);
+
+            // open a window to display the window box
+            DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
+            win.open();
+            win.requestActive();
+        }
+
     }
     
     @Override
     public void updateEnabled(RSMNode[] selectedNodes) {
        setEnabled(true); //JPM.TODO
+    }
+    
+    
+    private TopComponent findTopComponent(String name) {
+        Set<TopComponent> openTopComponents = WindowManager.getDefault().getRegistry().getOpened();
+        for (TopComponent tc : openTopComponents) {
+            if (tc.getName().compareTo(name) == 0) {
+                return tc;
+            }
+        }
+        return null;
     }
     
 }

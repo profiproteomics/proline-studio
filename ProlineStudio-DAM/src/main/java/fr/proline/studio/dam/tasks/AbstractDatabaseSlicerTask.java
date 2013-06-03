6c480733d62b0f5447ae980ef5a8e9ad49f25c1c
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-06-03 09:16:31 +0000

    bug fix when reseting priority of a task

--- ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/AbstractDatabaseSlicerTask.java
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/AbstractDatabaseSlicerTask.java
@@ -54,7 +54,7 @@ public abstract class AbstractDatabaseSlicerTask extends AbstractDatabaseTask {
     
     @Override
     public void resetPriority() {
-        defaultPriority = currentPriority;
+        currentPriority = defaultPriority;
         subTaskManager.resetPriority();
     }
     

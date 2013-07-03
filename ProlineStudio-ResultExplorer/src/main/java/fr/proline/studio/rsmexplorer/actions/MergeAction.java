0b4d1822aae507f96920c2ce6025c274db74c12b
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-07-03 13:36:46 +0000

    merge on RSM

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/MergeAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/MergeAction.java
@@ -49,18 +49,26 @@ public class MergeAction extends AbstractRSMAction {
                 String error = null;
                 int nbChildren = node.getChildCount();
                 if (nbChildren == 1) {
-                    error = "Merge on a Aggregate with only one Child is not possible";
+                    error = "Merge on an Aggregate with only one Child is not possible";
                 }
 
                 ArrayList<Long> resultSetIdList = new ArrayList<>();
+                ArrayList<Long> resultSummaryIdList = new ArrayList<>();
                 for (int i = 0; i < nbChildren; i++) {
                     RSMDataSetNode childNode = (RSMDataSetNode) node.getChildAt(i);
-                    if (!childNode.hasResultSet()) {
-                        error = "Merge impossible : " + childNode.getDataset().getName() + " has no Result Set";
+                    if (childNode.isChanging()) {
+                        error = "Merge is not possible while a child Search Result is being imported or validated";
                         break;
                     }
-                    resultSetIdList.add(childNode.getDataset().getResultSetId());
+                    if (!childNode.hasResultSet()) {
+                        error = "Merge is not possible : " + childNode.getDataset().getName() + " has no Search Result";
+                        break;
+                    }
 
+                    resultSetIdList.add(childNode.getDataset().getResultSetId());
+                    if (childNode.hasResultSummary()) {
+                        resultSummaryIdList.add(childNode.getDataset().getResultSummaryId());
+                    }
                 }
 
                 if (error != null) {
@@ -68,7 +76,20 @@ public class MergeAction extends AbstractRSMAction {
                     return;
                 }
 
-                askMergeService((RSMDataSetNode) node, resultSetIdList);
+
+                if (resultSummaryIdList.size()==nbChildren) {
+                    // we do a merge on resultSummary
+                    askMergeService((RSMDataSetNode) node, resultSummaryIdList, true);
+                } else if (resultSummaryIdList.size()>0) {
+                    // not all children have a result summary
+                    error = "Merge is not possible : some Search Results are not validated ";
+                    JOptionPane.showMessageDialog(RSMTree.getTree(), error, "Warning", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    // merge on result set
+                    askMergeService((RSMDataSetNode) node, resultSetIdList, false);
+                }
+
+                
 
 
             }
@@ -78,7 +99,7 @@ public class MergeAction extends AbstractRSMAction {
 
     }
 
-    private void askMergeService(final RSMDataSetNode node, final List<Long> resultSetIdList) {
+    private void askMergeService(final RSMDataSetNode node, final List<Long> idList, final boolean mergeOnRsm) {
 
         final Dataset dataset = node.getDataset();
         long projectId = dataset.getProject().getId();
@@ -91,7 +112,7 @@ public class MergeAction extends AbstractRSMAction {
 
         // used as out parameter for the service
         final Long[] _resultSetId = new Long[1];
-
+        final Long[] _resultSummaryId = new Long[1];
 
         AbstractServiceCallback callback = new AbstractServiceCallback() {
 
@@ -103,7 +124,7 @@ public class MergeAction extends AbstractRSMAction {
             @Override
             public void run(boolean success) {
                 if (success) {
-                    updateDataset(node, dataset, _resultSetId[0], getTaskInfo());
+                    updateDataset(node, dataset, _resultSetId[0], _resultSummaryId[0], getTaskInfo());
 
                 } else {
                     //JPM.TODO : manage error with errorMessage
@@ -114,11 +135,17 @@ public class MergeAction extends AbstractRSMAction {
             }
         };
 
-        MergeTask task = new MergeTask(callback, projectId, resultSetIdList, datasetName, _resultSetId);
+        MergeTask task = new MergeTask(callback, projectId);
+        if (mergeOnRsm) {
+             task.initMergeRsm(idList, datasetName, _resultSetId, _resultSummaryId);
+        } else {
+            _resultSummaryId[0] = null;
+            task.initMergeRset(idList, datasetName, _resultSetId);
+        }
         AccessServiceThread.getAccessServiceThread().addTask(task);
     }
 
-    private void updateDataset(final RSMDataSetNode datasetNode, Dataset d, Long resultSetId, TaskInfo taskInfo) {
+    private void updateDataset(final RSMDataSetNode datasetNode, Dataset d, Long resultSetId, Long resultSummaryId, TaskInfo taskInfo) {
 
         AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
 
@@ -143,7 +170,7 @@ public class MergeAction extends AbstractRSMAction {
 
 
         DatabaseDataSetTask task = new DatabaseDataSetTask(callback);
-        task.initModifyDatasetForMerge(d, resultSetId, taskInfo);
+        task.initModifyDatasetForMerge(d, resultSetId, resultSummaryId, taskInfo);
         AccessDatabaseThread.getAccessDatabaseThread().addTask(task);
 
     }

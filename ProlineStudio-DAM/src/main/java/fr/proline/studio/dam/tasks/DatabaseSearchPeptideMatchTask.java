3a391345e3c3f1ea363298a224f24d8c730cf43b
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-08-12 12:05:36 +0000

    bug fix for abort task

--- ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/DatabaseSearchPeptideMatchTask.java
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/DatabaseSearchPeptideMatchTask.java
@@ -57,10 +57,6 @@ public class DatabaseSearchPeptideMatchTask extends AbstractDatabaseTask {
         return true;
     }
 
-    @Override
-    public void abortTask() {
-        // nothing to do for task which are not inherited from AbstractDatabaseSlicerTask 
-    }
     
     @Override
     public boolean needToFetch() {

6fbbfbe2a8c605716e34c5b534877986b9d99e60
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-09-06 07:05:48 +0000

    bug fix : avoid delete children of a merged aggregate

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/DeleteAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/DeleteAction.java
@@ -3,6 +3,10 @@ package fr.proline.studio.rsmexplorer.actions;
 import fr.proline.studio.rsmexplorer.node.RSMDataSetNode;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
 import fr.proline.studio.rsmexplorer.node.RSMTree;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import org.openide.util.NbBundle;
 
 /**
@@ -23,8 +27,13 @@ public class DeleteAction extends AbstractRSMAction {
     @Override
     public void updateEnabled(RSMNode[] selectedNodes) {
 
+        
+        // look if all the nodes can be separately deleted and 
+        // are in the same project
+        
         long commonProjectId = -1;
         int nbSelectedNodes = selectedNodes.length;
+        HashSet<RSMDataSetNode> selectedNodesHashSet = new HashSet<>();
         for (int i=0;i<nbSelectedNodes;i++) {
             RSMNode node = selectedNodes[i];
             
@@ -41,9 +50,41 @@ public class DeleteAction extends AbstractRSMAction {
                 setEnabled(false);
                 return;
             }
+            
+            selectedNodesHashSet.add(datasetNode);
+            addChildren(datasetNode, selectedNodesHashSet);
         }
+        
+        // Check if one selected node is a child of a non selected node with merged results
+        // in this case, it can not be suppressed alone
+        Iterator<RSMDataSetNode> it = selectedNodesHashSet.iterator();
+        while (it.hasNext()) {
+            RSMDataSetNode node = it.next();
+            RSMNode parent = (RSMNode) node.getParent();
+            if (parent instanceof RSMDataSetNode) {
+                
+                RSMDataSetNode parentDataset = (RSMDataSetNode) parent;
+                if (!selectedNodesHashSet.contains(parentDataset)) {
+                    if (parentDataset.hasResultSet() || parentDataset.hasResultSummary()) {
+                        // we have a merged parent dataset which is not selected to be deleted, so its son can not be deleted
+                        setEnabled(false);
+                        return;
+                    }
+                }
+            }
+        }
+        
         
         setEnabled(true);
 
+    }
+    
+    private void addChildren(RSMDataSetNode parentNode, HashSet<RSMDataSetNode> nodesHashSet) {
+        Enumeration e = parentNode.children();
+        while (e.hasMoreElements()) {
+            RSMDataSetNode child = (RSMDataSetNode) e.nextElement();
+            nodesHashSet.add(child);
+            addChildren(child, nodesHashSet);
+        }
     }
 }

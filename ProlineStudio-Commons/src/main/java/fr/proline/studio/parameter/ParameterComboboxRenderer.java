96c85b01a08d217034f7dce9fd73b2d5231846b1
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-09-20 09:29:08 +0000

    modified the class comment

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/parameter/ParameterComboboxRenderer.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/parameter/ParameterComboboxRenderer.java
@@ -6,34 +6,35 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 
 /**
- * Parameter to select an Object in a List
+ * Renderer for Paramters
+ *
  * @author JM235353
  */
 public class ParameterComboboxRenderer<E> extends DefaultListCellRenderer {
 
-        private AbstractParameterToString<E> m_paramToString = null;
-        
-        public ParameterComboboxRenderer(AbstractParameterToString<E> paramToString) {
-            m_paramToString = paramToString;
-        }
-        
-        @Override
-        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            JLabel l = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+    private AbstractParameterToString<E> m_paramToString = null;
 
-            if (value == null) {
-                l.setText("< Select >");
-                return l;
-            }
-            
-            String display;
-            if (m_paramToString != null) {
-                display = m_paramToString.toString((E) value);
-            } else {
-                display = value.toString();
-            }
-            l.setText(display);
+    public ParameterComboboxRenderer(AbstractParameterToString<E> paramToString) {
+        m_paramToString = paramToString;
+    }
 
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        JLabel l = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+        if (value == null) {
+            l.setText("< Select >");
             return l;
         }
+
+        String display;
+        if (m_paramToString != null) {
+            display = m_paramToString.toString((E) value);
+        } else {
+            display = value.toString();
+        }
+        l.setText(display);
+
+        return l;
     }
+}

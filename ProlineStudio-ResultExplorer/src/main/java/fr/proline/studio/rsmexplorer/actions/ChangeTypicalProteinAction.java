afc51be824d2defc1debd900512e02494bd2eb20
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-09-24 15:33:38 +0000

    used to change typical protein of a Protein Set

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/ChangeTypicalProteinAction.java
@@ -0,0 +1,120 @@
+package fr.proline.studio.rsmexplorer.actions;
+
+import fr.proline.core.orm.uds.Dataset;
+import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
+import fr.proline.studio.dpm.AccessServiceThread;
+import fr.proline.studio.dpm.task.AbstractServiceCallback;
+import fr.proline.studio.dpm.task.ChangeTypicalProteinTask;
+import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.rsmexplorer.gui.dialog.ChangeTypicalProteinDialog;
+import fr.proline.studio.rsmexplorer.node.RSMDataSetNode;
+import fr.proline.studio.rsmexplorer.node.RSMNode;
+import fr.proline.studio.rsmexplorer.node.RSMTree;
+import javax.swing.tree.DefaultTreeModel;
+import org.openide.util.NbBundle;
+import org.openide.windows.WindowManager;
+
+/**
+ *
+ * @author JM235353
+ */
+public class ChangeTypicalProteinAction extends AbstractRSMAction {
+
+    
+    public ChangeTypicalProteinAction() {
+        super(NbBundle.getMessage(ValidateAction.class, "CTL_ChangeTypicalProtein"));
+    }
+
+    @Override
+    public void actionPerformed(RSMNode[] selectedNodes, int x, int y) {
+
+        
+
+        ChangeTypicalProteinDialog dialog = ChangeTypicalProteinDialog.getDialog(WindowManager.getDefault().getMainWindow());
+        dialog.setLocation(x, y);
+        dialog.setVisible(true);
+
+
+        if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+
+            String regex = dialog.getRegex();
+            boolean regexOnAccession = dialog.regexOnAccession();
+
+            RSMTree tree = RSMTree.getTree();
+            DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+
+            // start validation for each selected Dataset
+            int nbNodes = selectedNodes.length;
+            for (int i = 0; i < nbNodes; i++) {
+                final RSMDataSetNode dataSetNode = (RSMDataSetNode) selectedNodes[i];
+
+                dataSetNode.setIsChanging(true);
+                treeModel.nodeChanged(dataSetNode);
+
+                final Dataset d = dataSetNode.getDataset();
+
+                AbstractServiceCallback callback = new AbstractServiceCallback() {
+
+                    @Override
+                    public boolean mustBeCalledInAWT() {
+                        return true;
+                    }
+
+                    @Override
+                    public void run(boolean success) {
+
+                        dataSetNode.setIsChanging(false);
+
+
+                        RSMTree tree = RSMTree.getTree();
+                        DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+                        treeModel.nodeChanged(dataSetNode);
+                    }
+                };
+
+
+                ChangeTypicalProteinTask task = new ChangeTypicalProteinTask(callback, d, regex, regexOnAccession);
+                AccessServiceThread.getAccessServiceThread().addTask(task);
+
+
+
+            }
+
+
+        }
+    }
+
+    @Override
+    public void updateEnabled(RSMNode[] selectedNodes) {
+
+        // note : we can ask for the validation of multiple ResultSet in one time
+
+        int nbSelectedNodes = selectedNodes.length;
+        for (int i = 0; i < nbSelectedNodes; i++) {
+            RSMNode node = selectedNodes[i];
+
+            // parent node is being created, we can not validate it (for the moment)
+            if (node.isChanging()) {
+                setEnabled(false);
+                return;
+            }
+
+            // parent node must be a dataset
+            if (node.getType() != RSMNode.NodeTypes.DATA_SET) {
+                setEnabled(false);
+                return;
+            }
+
+            // parent node must have a ResultSet
+            RSMDataSetNode dataSetNode = (RSMDataSetNode) node;
+            if (!dataSetNode.hasResultSummary()) {
+                setEnabled(false);
+                return;
+            }
+        }
+
+        setEnabled(true);
+
+    }
+    
+}
---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/ChangeTypicalProteinDialog.java
@@ -0,0 +1,126 @@
+package fr.proline.studio.rsmexplorer.gui.dialog;
+
+import fr.proline.studio.gui.DefaultDialog;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Window;
+import java.util.regex.Pattern;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author JM235353
+ */
+public class ChangeTypicalProteinDialog extends DefaultDialog {
+    
+    private static ChangeTypicalProteinDialog m_singletonDialog = null;
+
+    private static final String[] REGEX_TARGET_OPTIONS = { "Protein Accession", "Protein Description" };
+    
+    private JTextField m_regexTextField = null;
+    private JComboBox m_regexTargetCombobox = null;
+    
+    public static ChangeTypicalProteinDialog getDialog(Window parent) {
+        if (m_singletonDialog == null) {
+            m_singletonDialog = new ChangeTypicalProteinDialog(parent);
+        }
+
+
+        return m_singletonDialog;
+    }
+    
+    public ChangeTypicalProteinDialog(Window parent) {
+        super(parent, Dialog.ModalityType.APPLICATION_MODAL);
+
+        setTitle("Change Typical Protein");
+        setButtonVisible(DefaultDialog.BUTTON_DEFAULT, false);
+        setStatusVisible(false);
+
+        setInternalComponent(createInternalPanel());
+
+
+    }
+    
+    private JPanel createInternalPanel() {
+        JPanel internalPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        internalPanel.add(new JLabel("Typical Protein Match :"), c);
+
+        c.gridx++;
+        m_regexTextField = new JTextField(16);
+        m_regexTextField.setText("*");
+        internalPanel.add(m_regexTextField, c);
+
+        c.gridx++;
+        internalPanel.add(new JLabel("on"), c);
+        
+        m_regexTargetCombobox = new JComboBox(REGEX_TARGET_OPTIONS);
+        m_regexTargetCombobox.setSelectedIndex(0);
+        c.gridx++;
+        internalPanel.add(m_regexTargetCombobox, c);
+        
+        c.gridy++;
+        c.gridx = 1;
+        JLabel regexHelpLabel = new JLabel("( * = any string ; ? = any character )");
+        regexHelpLabel.setForeground(Color.gray);
+        internalPanel.add(regexHelpLabel, c);
+        
+        return internalPanel;
+    }
+    
+    public String getRegex() {
+        return wildcardToRegex(m_regexTextField.getText().trim());
+    }
+    
+    private String wildcardToRegex(String text) {
+        String escapedText = escapeRegex(text);
+        String wildcardsFilter = escapedText.replaceAll("\\*", ".*").replaceAll("\\?", ".");
+        return wildcardsFilter;
+    } 
+    private String escapeRegex(String s) {
+
+        int len = s.length();
+        if (len == 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(len * 2);
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if ("[](){}.+$^|#\\".indexOf(c) != -1) {
+                sb.append("\\");
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    
+    public boolean regexOnAccession() {
+        return (m_regexTargetCombobox.getSelectedIndex() == 0);
+    }
+    
+    @Override
+    protected boolean okCalled() {
+        
+        return true;
+    }
+
+    @Override
+    protected boolean cancelCalled() {
+        return true;
+    }
+
+    
+}

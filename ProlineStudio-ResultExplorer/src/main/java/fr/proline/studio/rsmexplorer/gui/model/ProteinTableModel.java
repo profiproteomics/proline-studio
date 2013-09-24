c687418fba63e5322fb00fa4a742930b8fb411f7
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-09-24 15:36:10 +0000

    added filtering

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/model/ProteinTableModel.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/model/ProteinTableModel.java
@@ -2,13 +2,14 @@ package fr.proline.studio.rsmexplorer.gui.model;
 
 import fr.proline.core.orm.msi.BioSequence;
 import fr.proline.core.orm.msi.dto.DProteinMatch;
-import javax.swing.table.AbstractTableModel;
+import fr.proline.studio.filter.*;
+import java.util.ArrayList;
 
 /**
  * Table Model for Proteins
  * @author JM235353
  */
-public class ProteinTableModel extends AbstractTableModel {
+public class ProteinTableModel extends FilterTableModel {
 
     public static final int COLTYPE_PROTEIN_NAME           = 0;
     public static final int COLTYPE_SAMESET_SUBSET         = 1;
@@ -20,13 +21,23 @@ public class ProteinTableModel extends AbstractTableModel {
     private DProteinMatch[] m_subSetMatches = null;
     private Long m_rsmId = null;
 
+    private ArrayList<Integer> m_filteredIds = null;
+    private boolean m_isFiltering = false;
+    private boolean m_filteringAsked = false;
+
+
     public DProteinMatch getProteinMatch(int row) {
 
-        int sameSetNb = m_sameSetMatches.length;
-        if (row < sameSetNb) {
-            return m_sameSetMatches[row];
+        int rowFiltered = row;
+        if ((!m_isFiltering) && (m_filteredIds != null)) {
+            rowFiltered = m_filteredIds.get(row).intValue();
         }
-        return m_subSetMatches[row - sameSetNb];
+        
+        int sameSetNb = m_sameSetMatches.length;
+        if (rowFiltered < sameSetNb) {
+            return m_sameSetMatches[rowFiltered];
+        }
+        return m_subSetMatches[rowFiltered - sameSetNb];
     }
 
     @Override
@@ -60,11 +71,17 @@ public class ProteinTableModel extends AbstractTableModel {
         if (m_sameSetMatches == null) {
             return 0;
         }
+        
+        if ((!m_isFiltering) && (m_filteredIds != null)) {
+            return m_filteredIds.size();
+        }
+        
         return m_sameSetMatches.length + m_subSetMatches.length;
     }
 
     @Override
     public Object getValueAt(int row, int col) {
+ 
         // Retrieve Protein Match
         DProteinMatch proteinMatch = getProteinMatch(row);
 
@@ -101,12 +118,21 @@ public class ProteinTableModel extends AbstractTableModel {
         m_rsmId = rsmId;
         m_sameSetMatches = sameSetMatches;
         m_subSetMatches = subSetMatches;
-        fireTableDataChanged();
+   
+        if (m_filteringAsked) {
+            m_filteringAsked = false;
+            filter();
+        } else {
+            fireTableDataChanged();
+        }
         
 
     }
     
     public int findRowToSelect(String searchedText) {
+        
+        //JPM.TODO ?
+        
         int rowToBeSelected = 0;
         if ((searchedText != null) && (searchedText.length() > 0)) {
 
@@ -122,5 +148,81 @@ public class ProteinTableModel extends AbstractTableModel {
 
         return rowToBeSelected;
     }
+    
+        @Override
+    public void filter() {
+        
+        if (m_sameSetMatches == null) {
+            // filtering not possible for the moment
+            m_filteringAsked = true;
+            return;
+        }
+        
+        m_isFiltering = true;
+        try {
+
+            int nbData = m_sameSetMatches.length+m_subSetMatches.length;
+            if (m_filteredIds == null) {
+                m_filteredIds = new ArrayList<>(nbData);
+            } else {
+                m_filteredIds.clear();
+            }
+
+            for (int i = 0; i < nbData; i++) {
+                if (!filter(i)) {
+                    continue;
+                }
+                m_filteredIds.add(Integer.valueOf(i));
+            }
+
+        } finally {
+            m_isFiltering = false;
+        }
+        fireTableDataChanged();
+    }
+        
+        @Override
+    public boolean filter(int row, int col) {
+        Filter filter = getColumnFilter(col);
+        if ((filter == null) || (!filter.isUsed())) {
+            return true;
+        }
+        
+        Object data = getValueAt(row, col);
+        if (data == null) {
+            return true; // should not happen
+        }
+        
+        switch (col) {
+            case COLTYPE_PROTEIN_NAME: {
+                return ((StringFilter) filter).filter((String)data);
+            }
+            case COLTYPE_PROTEIN_SCORE:
+            case COLTYPE_PROTEIN_MASS: {
+                return ((DoubleFilter) filter).filter((Float)data);
+            }
+            case COLTYPE_PROTEIN_PEPTIDES_COUNT: {
+                return ((IntegerFilter) filter).filter((Integer)data);
+            }
+    
+        }
+        
+        return true; // should never happen
+    }
+        
+        
+    @Override
+    public void initFilters() {
+        if (m_filters == null) {
+            int nbCol = getColumnCount();
+            m_filters = new Filter[nbCol];
+            m_filters[COLTYPE_PROTEIN_NAME] = new StringFilter(getColumnName(COLTYPE_PROTEIN_NAME));
+            m_filters[COLTYPE_SAMESET_SUBSET] = null; //JPM.TODO
+            m_filters[COLTYPE_PROTEIN_SCORE] = new DoubleFilter(getColumnName(COLTYPE_PROTEIN_SCORE));
+            m_filters[COLTYPE_PROTEIN_PEPTIDES_COUNT] = new IntegerFilter(getColumnName(COLTYPE_PROTEIN_PEPTIDES_COUNT));
+            m_filters[COLTYPE_PROTEIN_MASS] = new DoubleFilter(getColumnName(COLTYPE_PROTEIN_MASS));
+        } 
+    }
+
     
 }

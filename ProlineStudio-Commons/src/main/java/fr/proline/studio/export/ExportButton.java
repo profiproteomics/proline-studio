ed0ef5d92f64dc841dd8efdb26ea239c622639b3
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-11-20 07:55:19 +0000

    added possibility to export an image

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportButton.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportButton.java
@@ -6,19 +6,21 @@ import fr.proline.studio.utils.IconManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import org.jdesktop.swingx.JXTable;
 import org.openide.windows.WindowManager;
 
 /**
- * Button to export data of a table
+ * Button to export data of a table or image of a JPanel
  * @author JM235353
  */
 public class ExportButton extends JButton implements ActionListener {
 
     private String m_exportName;
-    private JXTable m_table;
+    private JXTable m_table = null;
+    private JPanel m_panel = null;
     
-    private ProgressInterface m_progressInterface;
+    private ProgressInterface m_progressInterface = null;
             
     public ExportButton(ProgressInterface progressInterface, String exportName, JXTable table) {
 
@@ -28,16 +30,29 @@ public class ExportButton extends JButton implements ActionListener {
         m_table = table;
         
         setIcon(IconManager.getIcon(IconManager.IconType.EXPORT));
-        setToolTipText("Export...");
+        setToolTipText("Export Data...");
 
         addActionListener(this);
     }
+    
+    public ExportButton(String exportName, JPanel panel) {
+
+
+        m_exportName = exportName;
+        m_panel = panel;
+
+        setIcon(IconManager.getIcon(IconManager.IconType.EXPORT_IMAGE));
+        setToolTipText("Export Image...");
+
+        addActionListener(this);
+    }
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
         
-        if (!m_progressInterface.isLoaded()) {
+        if ((m_progressInterface != null) && (!m_progressInterface.isLoaded())) {
 
             ProgressBarDialog dialog = ProgressBarDialog.getDialog(WindowManager.getDefault().getMainWindow(), m_progressInterface, "Data loading", "Export is not available while data is loading. Please Wait.");
             dialog.setLocation(getLocationOnScreen().x + getWidth() + 5, getLocationOnScreen().y + getHeight() + 5);
@@ -48,7 +63,14 @@ public class ExportButton extends JButton implements ActionListener {
             }
         }
 
-        ExportDialog dialog = ExportDialog.getDialog(WindowManager.getDefault().getMainWindow(), m_table, m_exportName);
+        ExportDialog dialog;
+        
+        if (m_table != null) {
+            dialog = ExportDialog.getDialog(WindowManager.getDefault().getMainWindow(), m_table, m_exportName);
+        } else {
+            dialog = ExportDialog.getDialog(WindowManager.getDefault().getMainWindow(), m_panel, m_exportName);
+        }
+        
         dialog.setLocation(getLocationOnScreen().x + getWidth() + 5, getLocationOnScreen().y + getHeight() + 5);
         dialog.setVisible(true);
 

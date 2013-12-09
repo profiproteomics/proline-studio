1c633b90c477e2f1e78f982471886cae98bd603a
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-12-09 15:28:39 +0000

    beta version

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
@@ -15,7 +15,7 @@ public class Installer extends ModuleInstall {
     public void restored() {
 
         // set the proline version for the application title
-        System.setProperty("netbeans.buildnumber", "0.1.2b"); 
+        System.setProperty("netbeans.buildnumber", "Beta"); 
         
         // initialize the connection to the server as soon as possible
         ServerConnectionManager.getServerConnectionManager();

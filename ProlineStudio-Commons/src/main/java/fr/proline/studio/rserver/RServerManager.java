66f14eb652db1b585a53025f05c2a83d9b4ffe3c
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2013-12-17 09:57:16 +0000

    Added methods for R Server evaluation (better handling of errors)
    Use of a specific Exception for all problems from R Server

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/rserver/RServerManager.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/rserver/RServerManager.java
@@ -3,14 +3,18 @@ package fr.proline.studio.rserver;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RFileInputStream;
 import org.rosuda.REngine.Rserve.RFileOutputStream;
 import org.rosuda.REngine.Rserve.RserveException;
+import org.slf4j.LoggerFactory;
 
 
 /**
- *
+ * RServer Manager to be able to connect, upload files, execute commands and download file results from a R Server
  * @author JM235353
  */
 public class RServerManager {
@@ -38,62 +42,141 @@ public class RServerManager {
 
     }
 
-    public RConnection connect() throws RserveException {
-        m_connection = new RConnection(m_host, m_port);
+    public RConnection connect() throws RServerException {
         
-        // for the moment, we need to load the library roline //JPM.TODO
-        m_connection.eval("library(Roline)");
-        
-        return m_connection;
+        try {
+            m_connection = new RConnection(m_host, m_port);
+
+            // for the moment, we need to load the library roline //JPM.TODO
+            m_connection.eval("library(Roline)");
+
+            return m_connection;
+
+        } catch (RserveException e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        }
     }
     
     public RConnection getConnection() {
         return m_connection;
     }
     
-    public void uploadFile(String userPathSrc, String serverFileName) throws Exception {
+    public void uploadFile(String userPathSrc, String serverFileName) throws RServerException {
 
-        //String pwd = m_connection.eval("getwd()").asString();
+        RFileOutputStream fout = null;
+        FileInputStream input = null;
 
-        //System.out.println("Pwd : " + pwd);
+        try {
 
-        RFileOutputStream fout = m_connection.createFile(/*pwd + "/" +*/ serverFileName);
+            fout = m_connection.createFile(serverFileName);
 
-        FileInputStream input = new FileInputStream(userPathSrc);
+            input = new FileInputStream(userPathSrc);
 
-        //System.out.println("upload to " + pwd + "/" + fileNameDestination);
+            byte[] buffer = new byte[256];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
 
-        byte[] buffer = new byte[256];
-        int bytesRead;
-        while ((bytesRead = input.read(buffer)) != -1) {
+                fout.write(buffer, 0, bytesRead);
+            }
 
-            fout.write(buffer, 0, bytesRead);
+        } catch (Exception e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                }
+            }
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                }
+            }
         }
 
-        fout.close();
-        input.close();
 
-        System.out.println("Upload Done");
+
     }
     
-    public void downloadFile(String serverFileName, String userPathDestination) throws IOException {
-        RFileInputStream fin = m_connection.openFile(serverFileName);
+    public void downloadFile(String serverFileName, String userPathDestination) throws RServerException {
         
-        FileOutputStream output = new FileOutputStream(userPathDestination);
-        
-        byte[] buffer = new byte[256];
-        int bytesRead;
-        while ((bytesRead = fin.read(buffer)) != -1) {
+        RFileInputStream fin = null;
+        FileOutputStream output = null;
 
-            output.write(buffer, 0, bytesRead);
+        try {
+            fin = m_connection.openFile(serverFileName);
+
+            output = new FileOutputStream(userPathDestination);
+
+            byte[] buffer = new byte[256];
+            int bytesRead;
+            while ((bytesRead = fin.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                }
+            }
+            if (fin != null) {
+                try {
+                    fin.close();
+                } catch (IOException e) {
+                }
+            }
         }
-
-        output.close();
-        fin.close();
+ 
     }
     
-    public void deleteFile(String fileNameDestination) throws RserveException {
-        m_connection.removeFile(fileNameDestination);
+    public void deleteFile(String fileNameDestination) throws RServerException {
+        try {
+            m_connection.removeFile(fileNameDestination);
+        } catch (RserveException e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        }
+    }
+    
+    public String getPwd() throws RServerException {
+        try {
+            return m_connection.eval("getwd()").asString();
+        } catch  (REXPMismatchException | REngineException e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        }
+    }
+    
+    public REXP eval(String code) throws RServerException {
+        
+        try {
+            REXP result = m_connection.parseAndEval("try(" + code + ",silent=TRUE)");
+            if (result.inherits("try-error")) {
+                RServerException e = new RServerException(result.asString());
+                LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+                throw e;
+            }
+        
+            return result;
+        } catch (REXPMismatchException | REngineException e) {
+            LoggerFactory.getLogger("ProlineStudio.Commons").error(getClass().getSimpleName() + " failed", e);
+            throw new RServerException(e.getMessage());
+        }
+    }
+    
+    public class RServerException extends Exception {
+        
+        public RServerException(String message) {
+            super(message);
+        }
     }
     
 }

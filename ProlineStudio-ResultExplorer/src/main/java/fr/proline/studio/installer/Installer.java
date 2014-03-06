32e6700fa996a16b5ecbfe593cf73795bbe46b74
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-03-06 10:26:35 +0000

    Indique quelle version beta dans l'IHM .... A voir par la suite comment on gère ça

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
@@ -23,7 +23,7 @@ public class Installer extends ModuleInstall {
     public void restored() {
 
         // set the proline version for the application title
-        System.setProperty("netbeans.buildnumber", "Beta"); 
+        System.setProperty("netbeans.buildnumber", "Beta 3c"); 
         
         // initialize the connection to the server as soon as possible
         ServerConnectionManager.getServerConnectionManager();

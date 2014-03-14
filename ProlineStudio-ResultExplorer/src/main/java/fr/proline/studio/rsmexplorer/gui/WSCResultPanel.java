5e6c8a60402c5f3e2f1bb3a8008a050fb3f971ba
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-03-14 08:37:52 +0000

    added toolbar with Export / Filter / Search

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/WSCResultPanel.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/WSCResultPanel.java
@@ -3,10 +3,13 @@ package fr.proline.studio.rsmexplorer.gui;
 
 import fr.proline.core.orm.msi.dto.DProteinMatch;
 import fr.proline.studio.dpm.task.SpectralCountTask;
+import fr.proline.studio.export.ExportButton;
+import fr.proline.studio.filter.FilterButton;
 import fr.proline.studio.gui.HourglassPanel;
 import fr.proline.studio.gui.SplittedPanelContainer;
 import fr.proline.studio.pattern.AbstractDataBox;
 import fr.proline.studio.pattern.DataBoxPanelInterface;
+import fr.proline.studio.progress.ProgressInterface;
 import fr.proline.studio.rsmexplorer.gui.renderer.DefaultRightAlignRenderer;
 import fr.proline.studio.rsmexplorer.gui.renderer.FloatRenderer;
 import fr.proline.studio.utils.DecoratedTable;
@@ -16,9 +19,15 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.table.TableColumn;
 import fr.proline.studio.rsmexplorer.gui.model.WSCProteinTableModel;
 import fr.proline.studio.rsmexplorer.gui.renderer.BooleanRenderer;
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import fr.proline.studio.search.AbstractSearch;
+import fr.proline.studio.search.SearchFloatingPanel;
+import fr.proline.studio.search.SearchToggleButton;
+import fr.proline.studio.utils.LazyTable;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.util.ArrayList;
+import javax.swing.*;
 
 /**
  * Panel for Protein Matches
@@ -29,11 +38,16 @@ public class WSCResultPanel extends HourglassPanel implements DataBoxPanelInterf
     private AbstractDataBox m_dataBox;
     private SpectralCountTask.WSCResultData m_weightedSCResult = null;
    
-    private javax.swing.JTextField rsmRefNameField;
-    private javax.swing.JTable proteinTable;
-    private javax.swing.JScrollPane scrollPane;
+    private JTextField m_rsmRefNameField;
+    private ProteinTable m_proteinTable;
+    private JScrollPane m_scrollPane;
     
+    private SearchFloatingPanel m_searchPanel;
+    private JToggleButton m_searchToggleButton;
+    private Search m_search = null;
     
+    private FilterButton m_filterButton;
+    private ExportButton m_exportButton;    
     
     /**
      * Creates new form RsmProteinsOfProteinSetPanel
@@ -41,37 +55,14 @@ public class WSCResultPanel extends HourglassPanel implements DataBoxPanelInterf
     public WSCResultPanel() {
         initComponents();
 
-        TableColumn accColumn = proteinTable.getColumnModel().getColumn(WSCProteinTableModel.COLTYPE_PROTEIN_NAME);
+        TableColumn accColumn = m_proteinTable.getColumnModel().getColumn(WSCProteinTableModel.COLTYPE_PROTEIN_NAME);
         URLCellRenderer renderer = new URLCellRenderer("URL_Template_Protein_Accession", "http://www.uniprot.org/uniprot/", WSCProteinTableModel.COLTYPE_PROTEIN_NAME);
         accColumn.setCellRenderer(renderer);
-        proteinTable.addMouseListener(renderer);
+        m_proteinTable.addMouseListener(renderer);
 
     }
 
-//    public ProteinMatch getSelectedProteinMatch() {
-//
-//       ProteinTable table = (ProteinTable) proteinTable;
-//
-//        // Retrieve Selected Row
-//        int selectedRow = table.getSelectedRow();
-//
-//
-//        // nothing selected
-//        if (selectedRow == -1) {
-//            return null;
-//
-//        }
-//
-//        // convert according to the sorting
-//        selectedRow = table.convertRowIndexToModel(selectedRow);
-//
-//
-//
-//        // Retrieve ProteinSet selected
-//        WSCProteinTableModel tableModel = (WSCProteinTableModel) table.getModel();
-//
-//        return tableModel.getProteinMatch(selectedRow);
-//    }
+
 
     public void setData(SpectralCountTask.WSCResultData scResult) {
 
@@ -87,19 +78,19 @@ public class WSCResultPanel extends HourglassPanel implements DataBoxPanelInterf
 
 
         // Modify protein description
-        rsmRefNameField.setText(scResult.getDataSetReference().getName());
+        m_rsmRefNameField.setText(scResult.getDataSetReference().getName());
 
 
         // Modify the Model
-        ((WSCProteinTableModel) proteinTable.getModel()).setData(scResult);
+        ((WSCProteinTableModel) m_proteinTable.getModel()).setData(scResult);
 
 
 
     }
 
     private void clearData() {
-        rsmRefNameField.setText("");
-        ((WSCProteinTableModel) proteinTable.getModel()).setData(null);
+        m_rsmRefNameField.setText("");
+        ((WSCProteinTableModel) m_proteinTable.getModel()).setData(null);
 
     }
 
@@ -121,45 +112,137 @@ public class WSCResultPanel extends HourglassPanel implements DataBoxPanelInterf
     /**
      * This method is called from within the constructor to initialize the form. 
      */
-    @SuppressWarnings("unchecked")
     private void initComponents() {
 
-        rsmRefNameField = new javax.swing.JTextField();
-        rsmRefNameField.setEditable(false);
-        rsmRefNameField.setBackground(Color.white);
-        
-        scrollPane = new javax.swing.JScrollPane();
-        proteinTable = new ProteinTable();
+        setLayout(new BorderLayout());
 
-        proteinTable.setModel(new WSCProteinTableModel());
-        scrollPane.setViewportView(proteinTable);
+        m_search = new Search();
+        m_searchPanel = new SearchFloatingPanel(m_search);
+        final JPanel spectralCountPanel = createSpectralCountPanel();
+        m_searchPanel.setToggleButton(m_searchToggleButton);
+
+        final JLayeredPane layeredPane = new JLayeredPane();
+
+        layeredPane.addComponentListener(new ComponentListener() {
+
+            @Override
+            public void componentResized(ComponentEvent e) {
+                final Component c = e.getComponent();
+
+                spectralCountPanel.setBounds(0, 0, c.getWidth(), c.getHeight());
+                layeredPane.revalidate();
+                layeredPane.repaint();
+
+            }
+
+            @Override
+            public void componentMoved(ComponentEvent e) {
+            }
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+            }
+        });
+        add(layeredPane, BorderLayout.CENTER);
+
+        layeredPane.add(spectralCountPanel, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(m_searchPanel, JLayeredPane.PALETTE_LAYER);
+
+    }
+           
+    private JPanel createSpectralCountPanel() {
+
+        JPanel spectralCountPanel = new JPanel();
+        spectralCountPanel.setBounds(0, 0, 500, 400);
+        spectralCountPanel.setLayout(new BorderLayout());
+
+        JPanel internalPanel = createInternalPanel();
+
+        JToolBar toolbar = initToolbar();
+        spectralCountPanel.add(toolbar, BorderLayout.WEST);
+        spectralCountPanel.add(internalPanel, BorderLayout.CENTER);
+
+
+        return spectralCountPanel;
+    }
+    
+    private JPanel createInternalPanel() {
+        JPanel internalPanel = new JPanel();
         
-        setLayout(new GridBagLayout());
+        internalPanel.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.NORTHWEST;
         c.fill = GridBagConstraints.BOTH;
         c.insets = new java.awt.Insets(5, 5, 5, 5);
         
+        m_rsmRefNameField = new javax.swing.JTextField();
+        m_rsmRefNameField.setEditable(false);
+        m_rsmRefNameField.setBackground(Color.white);
+        
+        m_scrollPane = new javax.swing.JScrollPane();
+        m_proteinTable = new ProteinTable();
+
+        m_proteinTable.setModel(new WSCProteinTableModel(m_proteinTable));
+        m_scrollPane.setViewportView(m_proteinTable);
+        
+                
         c.gridx = 0;
         c.gridy = 0;
-        add(rsmRefNameField, c);
+        internalPanel.add(m_rsmRefNameField, c);
         
         c.gridy++;
         c.weightx = 1;
         c.weighty = 1;
-        add(scrollPane, c);
-
+        internalPanel.add(m_scrollPane, c);
+        
+        return internalPanel;
     }
-               
+    
+    private JToolBar initToolbar() {
+        JToolBar toolbar = new JToolBar(JToolBar.VERTICAL);
+        toolbar.setFloatable(false);
+
+        // Search Button
+        m_searchToggleButton = new SearchToggleButton(m_searchPanel);
+        toolbar.add(m_searchToggleButton);
+
+        m_filterButton = new FilterButton(((WSCProteinTableModel) m_proteinTable.getModel()));
+
+        m_exportButton = new ExportButton(((WSCProteinTableModel) m_proteinTable.getModel()), "Spectral Counts", m_proteinTable);
+
+        toolbar.add(m_filterButton);
+        toolbar.add(m_exportButton);
+        
+        return toolbar;
+    }
 
 
-
-    private class ProteinTable extends DecoratedTable {
+    private class ProteinTable extends LazyTable {
 
         public ProteinTable() {
+            super(m_scrollPane.getVerticalScrollBar());
             setDefaultRenderer(Float.class, new FloatRenderer( new DefaultRightAlignRenderer(getDefaultRenderer(Float.class)) ) );
             setDefaultRenderer(Boolean.class, new BooleanRenderer());
 
+        }
+        
+        public boolean selectProtein(Integer row) {
+            WSCProteinTableModel tableModel = (WSCProteinTableModel) getModel();
+
+            // must convert row index if there is a sorting
+            row = convertRowIndexToView(row);
+            
+            // select the row
+            getSelectionModel().setSelectionInterval(row, row);
+            
+            // scroll to the row
+            scrollRowToVisible(row);
+
+            return true;
         }
         
         /**
@@ -175,6 +258,121 @@ public class WSCResultPanel extends HourglassPanel implements DataBoxPanelInterf
             m_dataBox.propagateDataChanged(DProteinMatch.class);
 
 
+        }
+
+        @Override
+        public void sortingChanged(int col) {
+            m_search.reinitSearch();
+        }
+
+        @Override
+        public boolean isLoaded() {
+            return true; // not used
+        }
+
+        @Override
+        public int getLoadingPercentage() {
+            return 0; // not used
+        }
+
+    }
+    
+    private class Search extends AbstractSearch {
+
+        private String previousSearch = "";
+        private int searchIndex = 0;
+        private ArrayList<Integer> proteinNamesRow = new ArrayList<>();
+
+        @Override
+        public void reinitSearch() {
+            if (proteinNamesRow.isEmpty()) {
+                return;
+            }
+            searchIndex = -1;
+            ((WSCProteinTableModel) m_proteinTable.getModel()).sortAccordingToModel(proteinNamesRow);
+        }
+
+        @Override
+        public void doSearch(String text) {
+            final String searchText = text.trim().toUpperCase();
+
+            if (searchText.compareTo(previousSearch) == 0) {
+                
+                int checkLoopIndex = -1;
+                while (true) {
+                    // search already done, display next result
+                    searchIndex++;
+                    if (searchIndex >= proteinNamesRow.size()) {
+                        searchIndex = 0;
+                    }
+
+                    if (checkLoopIndex == searchIndex) {
+                        break;
+                    }
+                    
+                    if (!proteinNamesRow.isEmpty()) {
+                        boolean found = m_proteinTable.selectProtein(proteinNamesRow.get(searchIndex));
+                        if (found) {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                    if (checkLoopIndex == -1) {
+                        checkLoopIndex =  searchIndex;
+                    }
+                }
+                
+            } else {
+                previousSearch = searchText;
+                searchIndex = -1;
+
+                String regex = wildcardToRegex(searchText);
+                
+                WSCProteinTableModel model = ((WSCProteinTableModel) m_proteinTable.getModel());
+                
+                proteinNamesRow.clear();
+                for (int i=0;i<model.getRowCount(); i++) {
+                    String name = (String) model.getValueAt(i, WSCProteinTableModel.COLTYPE_PROTEIN_NAME);
+                    if (name.matches(regex)) {
+                        proteinNamesRow.add(i);
+                    }
+                }
+
+                 if (!proteinNamesRow.isEmpty()) {
+                            
+                           model.sortAccordingToModel(proteinNamesRow);
+
+                             int checkLoopIndex = -1;
+                             while (true) {
+                                // search already done, display next result
+                                searchIndex++;
+                                if (searchIndex >= proteinNamesRow.size()) {
+                                    searchIndex = 0;
+                                }
+
+                                if (checkLoopIndex == searchIndex) {
+                                    break;
+                                }
+
+                                if (!proteinNamesRow.isEmpty()) {
+                                    boolean found = m_proteinTable.selectProtein(proteinNamesRow.get(searchIndex));
+                                    if (found) {
+                                        break;
+                                    }
+                                } else {
+                                    break;
+                                }
+                                if (checkLoopIndex == -1) {
+                                    checkLoopIndex = searchIndex;
+                                }
+                            }
+                            
+                        }
+
+
+            }
+        
         }
     }
 }
--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/model/WSCProteinTableModel.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/model/WSCProteinTableModel.java
@@ -2,36 +2,59 @@ package fr.proline.studio.rsmexplorer.gui.model;
 
 import fr.proline.core.orm.uds.dto.DDataset;
 import fr.proline.studio.dpm.task.SpectralCountTask;
+import fr.proline.studio.filter.DoubleFilter;
+import fr.proline.studio.filter.Filter;
+import fr.proline.studio.filter.FilterTableModel;
+import fr.proline.studio.filter.StringFilter;
+import fr.proline.studio.utils.DecoratedTable;
+import fr.proline.studio.utils.LazyTable;
+import fr.proline.studio.utils.LazyTableModel;
 import java.util.*;
-import javax.swing.table.AbstractTableModel;
+
 
 /**
  * Table Model for Proteins
  * @author JM235353
  */
-public class WSCProteinTableModel extends AbstractTableModel {
+public class WSCProteinTableModel extends LazyTableModel {
 
     public static final int COLTYPE_PROTEIN_NAME    = 0;
     public static final int COLTYPE_BSC             = 1;
     public static final int COLTYPE_SSC             = 2;
     public static final int COLTYPE_WSC             = 3;
     public static final int COLTYPE_STATUS          = 4;
-//    private static final String[] m_columnNames = {"Protein", "Basic SC", "Specific SC", "Weighted SC"};
-    private static final String[] m_columnNames = {"Protein", "Basic SC", "Specific SC", "Weighted SC","Status"};
-    private static final Class[] m_rsmColumnType = {Float.class, Float.class, Float.class,String.class};
+
+    private static final String[] m_columnNames = {"Protein", "Basic SC", "Specific SC", "Weighted SC", "Status"};
+    private static final Class[] m_columnTypes = {Float.class, Float.class, Float.class, String.class};
     private static final int COLNBR_PER_RSM = m_columnNames.length-1;
     
     private SpectralCountTask.WSCResultData m_wscResult = null;
     private List<String> m_protMatchNames = new ArrayList();
+
+    
+    private ArrayList<Integer> m_filteredIds = null;
+    private boolean m_isFiltering = false;
+    private boolean m_filteringAsked = false;
+    
+
+    public WSCProteinTableModel(LazyTable table) {
+        super(table);
+    }
     
     public String getProteinMatchAccession(int row) {
+        
+        if (m_filteredIds != null) {
+            row = m_filteredIds.get(row).intValue();
+        }
+
         return m_protMatchNames.get(row);
     }
 
     @Override
     public int getColumnCount() {
-        if (m_wscResult ==null)
+        if (m_wscResult == null) {
             return 1;
+        }
         return 1 + (m_wscResult.getComputedSCDatasets().size()*COLNBR_PER_RSM);        
     }
 
@@ -76,19 +99,30 @@ public class WSCProteinTableModel extends AbstractTableModel {
                     colSuffixIndex=modulo;
                     break;                
                 }
-                return m_rsmColumnType[colSuffixIndex-1];
+                return m_columnTypes[colSuffixIndex-1];
         }        
     }
 
     @Override
-    public int getRowCount() {      
+    public int getRowCount() {     
+
+        if ((!m_isFiltering) && (m_filteredIds != null)) {
+            return m_filteredIds.size();
+        }
+        
         return m_protMatchNames.size();
     }
 
     @Override
     public Object getValueAt(int row, int col) {
+        
+        int rowFiltered = row;
+        if ((!m_isFiltering) && (m_filteredIds != null)) {
+            rowFiltered = m_filteredIds.get(row).intValue();
+        }
+        
         // Retrieve Protein Match
-        String proteinMatchName = getProteinMatchAccession(row);
+        String proteinMatchName = m_protMatchNames.get(rowFiltered);
 
         switch (col) {
             case COLTYPE_PROTEIN_NAME:
@@ -96,7 +130,7 @@ public class WSCProteinTableModel extends AbstractTableModel {
                 
             default: {
               int currentRSMNbr = (col%COLNBR_PER_RSM==0) ? col/COLNBR_PER_RSM : (col/COLNBR_PER_RSM)+1;
-              Long rsmID =m_wscResult.getComputedSCDatasets().get(currentRSMNbr-1).getResultSummaryId();
+              Long rsmID = m_wscResult.getComputedSCDatasets().get(currentRSMNbr-1).getResultSummaryId();
               Map<String, SpectralCountTask.SpectralCountsStruct> rsmResult =  m_wscResult.getRsmSCResult(rsmID);
               SpectralCountTask.SpectralCountsStruct searchedProtSC = rsmResult.get(proteinMatchName);
               int modulo = col%COLNBR_PER_RSM;
@@ -120,7 +154,8 @@ public class WSCProteinTableModel extends AbstractTableModel {
                     if(searchedProtSC != null)
                         return searchedProtSC.getWsc();
                     else
-                        return Float.NaN;                }            
+                        return Float.NaN;                
+              }            
             }  
         }
         return null; // should never happen
@@ -135,8 +170,145 @@ public class WSCProteinTableModel extends AbstractTableModel {
         for (DDataset ds :dss) {
            names.addAll(m_wscResult.getRsmSCResult(ds.getResultSummaryId()).keySet());
         }
-        m_protMatchNames = new ArrayList(names);    
+        m_protMatchNames = new ArrayList(names);  
+        
+        
+        if (m_filteringAsked) {
+            m_filteringAsked = false;
+            filter();
+        }
+        
         fireTableStructureChanged();
+    }
+
+    @Override
+    public String getToolTipForHeader(int col) {
+        return getColumnName(col);
+    }
+
+    public void sortAccordingToModel(ArrayList<Integer> proteinNames) {
+        
+        if (m_protMatchNames.isEmpty()) {
+            // data not loaded 
+            return;
+        }
+        
+        HashSet<Integer> proteinNamesMap = new HashSet<>(proteinNames.size());
+        proteinNamesMap.addAll(proteinNames);
+        
+        int nb = getRowCount();
+        int iCur = 0;
+        for (int iView=0;iView<nb;iView++) {
+            int iModel = m_table.convertRowIndexToModel(iView);
+            // Retrieve Protein Set
+            String proteinName = (String) getValueAt(iModel, COLTYPE_PROTEIN_NAME);
+            if (  proteinNamesMap.contains(iModel)  ) {
+                proteinNames.set(iCur++, iModel);
+            }
+        }
+        
+        // need to refilter
+        if (m_filteredIds != null) { // NEEDED ????
+            filter();
+        }
+    }
+    
+    
+    @Override
+    public void initFilters() {
+        if (m_filters == null) {
+            int nbCol = getColumnCount();
+            m_filters = new Filter[nbCol];
+            m_filters[COLTYPE_PROTEIN_NAME] = new StringFilter(getColumnName(COLTYPE_PROTEIN_NAME));
+            for (int i=1;i<nbCol;i++) {
+                int modulo = i % COLNBR_PER_RSM;
+                switch (modulo){
+                case 0:
+                    m_filters[i] = new StringFilter(getColumnName(i));
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                    m_filters[i] = new DoubleFilter(getColumnName(i)); 
+                    break;
+              } 
+                
+                
+            }
+        }
+    }
+
+    @Override
+    public void filter() {
+    
+        if (m_wscResult == null) {
+            // filtering not possible for the moment
+            m_filteringAsked = true;
+            return;
+        }
+        
+        m_isFiltering = true;
+        try {
+
+            int nbData = m_protMatchNames.size();
+            if (m_filteredIds == null) {
+                m_filteredIds = new ArrayList<>(nbData);
+            } else {
+                m_filteredIds.clear();
+            }
+
+            for (int i = 0; i < nbData; i++) {
+                if (!filter(i)) {
+                    continue;
+                }
+                m_filteredIds.add(Integer.valueOf(i));
+            }
+
+        } finally {
+            m_isFiltering = false;
+        }
+        fireTableDataChanged();
+    }
+
+    @Override
+    public boolean filter(int row, int col) {
+        Filter filter = getColumnFilter(col);
+        if ((filter == null) || (!filter.isUsed())) {
+            return true;
+        }
+
+        Object data = getValueAt(row, col);
+        if (data == null) {
+            return true; // should not happen
+        }
+
+         int modulo = col % COLNBR_PER_RSM;
+        
+        if ((col == COLTYPE_PROTEIN_NAME) || (modulo == 0) /*COLTYPE_STATUS*/) {
+            return ((StringFilter) filter).filter((String) data);
+        }        
+
+        return ((DoubleFilter) filter).filter((Float) data);
+
+
+    }
+
+    @Override
+    public boolean isLoaded() {
+        return (m_protMatchNames.size() >0 );
+    }
+
+    @Override
+    public int getLoadingPercentage() {
+        if (m_protMatchNames.size() >0 ) {
+            return 100;
+        }
+        return 0;
+    }
+
+    @Override
+    public int getSubTaskId(int col) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 

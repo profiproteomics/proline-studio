f49397e7fb7991667c8906754d34299ddc2451e3
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-04-17 06:36:01 +0000

    enhancement of spectral count

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/SpectralCountAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/SpectralCountAction.java
@@ -10,6 +10,7 @@ import fr.proline.studio.pattern.WindowBox;
 import fr.proline.studio.pattern.WindowBoxFactory;
 import fr.proline.studio.rsmexplorer.DataBoxViewerTopComponent;
 import fr.proline.studio.rsmexplorer.gui.dialog.SetQuantitationDSNameDialog;
+import fr.proline.studio.rsmexplorer.gui.dialog.spectralcount.SpectralCountDialog;
 import fr.proline.studio.rsmexplorer.gui.dialog.TreeSelectionDialog;
 import fr.proline.studio.rsmexplorer.node.RSMDataSetNode;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
@@ -30,7 +31,8 @@ import org.slf4j.LoggerFactory;
 public class SpectralCountAction extends AbstractRSMAction {
  
     protected static final Logger m_logger = LoggerFactory.getLogger("ProlineStudio.ResultExplorer");
-    protected TreeSelectionDialog m_treeSelectionDialog;
+    //protected TreeSelectionDialog m_treeSelectionDialog;
+
   
     final static public String DS_NAME_PROPERTIES="dsName";
     final static public String DS_DESCRIPTION_PROPERTIES="dsDescription";
@@ -42,7 +44,95 @@ public class SpectralCountAction extends AbstractRSMAction {
     
     @Override
     public void actionPerformed(RSMNode[] selectedNodes, final int x, final int y) { 
+        
+        final RSMDataSetNode refDatasetNode = (RSMDataSetNode) selectedNodes[0];
+        
+        //Create Child Tree to select RSM to compute SC for
+        final IdentificationTree childTree = IdentificationTree.getCurrentTree().copyDataSetRootSubTree(refDatasetNode.getDataset(), refDatasetNode.getDataset().getProject().getId());
+        final SpectralCountDialog spectralCountDialog = new SpectralCountDialog(WindowManager.getDefault().getMainWindow(), childTree);
+
+        spectralCountDialog.setLocation(x, y);
+        
+        
+        
+        AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
+
+            @Override
+            public boolean mustBeCalledInAWT() {
+                return true;
+            }
+
+            @Override
+            public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
+                m_logger.debug(" Choose RSM for Weighted SC");
+
+                // check if we can compute SC
+                String error = null;
+                Map<String,Object> params = new HashMap();
+                ArrayList<DDataset> datasetList = new ArrayList<>();        
+                datasetList.add(refDatasetNode.getDataset()); //first entry is Reference Dataset in data box !
+                
+                if (spectralCountDialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+                   ArrayList<RSMDataSetNode> selectedDSNodes = spectralCountDialog.getSelectedRSMDSNodeList();
+                   //A Voir :) 
+//                   if(selectedDSNodes.contains(refDatasetNode) && selectedDSNodes.size()>1) 
+//                        error = " Spectral Count is not possible on reference Node and some of its child";
+                       
+                   for (RSMDataSetNode dsNode : selectedDSNodes) {
+                       //TODO : Verif pas père + fils sélectionnés : Que père ou que fils ?!? 
+                        if (!dsNode.hasResultSummary()) {
+                            error = " Spectral Count is not possible on Search result ("+ dsNode.getDataset().getName() +").  Identification Summary should be created first";
+                            break;                       
+                        }
+                        if(dsNode.isChanging()){
+                            error = " Spectral Count is not possible while import or validation is on going. (Search result "+ dsNode.getDataset().getName() +")";
+                            break;  
+                        }
+                        datasetList.add(dsNode.getDataset());
+                        params.put(DS_LIST_PROPERTIES,datasetList);
+                   }
+                   
+
+                    params.put(DS_NAME_PROPERTIES, spectralCountDialog.getSpectralCountName());
+                    params.put(DS_DESCRIPTION_PROPERTIES, spectralCountDialog.getSpectralCountDescription());
+
+                   
+                } else { //Cancel / Close was clicked
+                    return;                                                
+                }
+
+                if (error != null) {
+                    JOptionPane.showMessageDialog(IdentificationTree.getCurrentTree(), error, "Warning", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                
+                m_logger.debug(" Will Compute SC on "+(datasetList.size())+" RSMs : "+datasetList);
+                
+                WindowBox wbox = WindowBoxFactory.getRsmWSCWindowBox((String)params.get(DS_NAME_PROPERTIES), false) ;
+                wbox.setEntryData(refDatasetNode.getDataset().getProject().getId(), params);
+
+                // open a window to display the window box
+                DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
+                win.open();
+                win.requestActive(); 
+                
+            }
+        };
+        
+        spectralCountDialog.setVisible(true);
+
+
+        IdentificationTree.getCurrentTree().loadInBackground(refDatasetNode, callback);
+        
+        
+    }
+    
+    /*
+    public void actionPerformedOLD(RSMNode[] selectedNodes, final int x, final int y) { 
      
+        
+        
+        
         // Only Ref should be selected to compute SC         
         final RSMDataSetNode refDatasetNode = (RSMDataSetNode) selectedNodes[0];
         final SetQuantitationDSNameDialog qttDSDialog = SetQuantitationDSNameDialog.getDialog(WindowManager.getDefault().getMainWindow());
@@ -126,7 +216,7 @@ public class SpectralCountAction extends AbstractRSMAction {
         IdentificationTree.getCurrentTree().loadInBackground(refDatasetNode, callback);
 
     
-    }
+    }*/
 
     @Override
     public void updateEnabled(RSMNode[] selectedNodes) {

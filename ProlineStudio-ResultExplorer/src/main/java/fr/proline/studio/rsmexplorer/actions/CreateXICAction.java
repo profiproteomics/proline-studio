360f993d64e23a7c90ffcf5a38a565df791102a1
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-05-23 06:53:54 +0000

    #10340 ; Début de l'intégration du XIC

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/CreateXICAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/CreateXICAction.java
@@ -1,31 +1,126 @@
 package fr.proline.studio.rsmexplorer.actions;
 
+import fr.proline.core.orm.uds.dto.DDataset;
+import fr.proline.studio.dam.data.DataSetData;
+import fr.proline.studio.dpm.AccessServiceThread;
+import fr.proline.studio.dpm.task.AbstractServiceCallback;
+import fr.proline.studio.dpm.task.RunXICTask;
 import fr.proline.studio.gui.DefaultDialog;
 import fr.proline.studio.rsmexplorer.gui.dialog.xic.CreateXICDialog;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
 import org.openide.util.NbBundle;
 import org.openide.windows.WindowManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author JM235353
  */
 public class CreateXICAction extends AbstractRSMAction {
-
+    protected static final Logger m_logger = LoggerFactory.getLogger("ProlineStudio.ResultExplorer");
+    
     public CreateXICAction() {
         super(NbBundle.getMessage(ConnectAction.class, "CTL_CreateXIC"), false);
     }
     
      @Override
     public void actionPerformed(RSMNode[] selectedNodes, int x, int y) {
-         CreateXICDialog dialog = new CreateXICDialog(WindowManager.getDefault().getMainWindow());
+         
+         CreateXICDialog dialog =  CreateXICDialog.getDialog(WindowManager.getDefault().getMainWindow());
+         dialog.reinit();
          dialog.setLocation(x, y);
          dialog.setVisible(true);
-
-
-         if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+         final Long[] _xicQuantiDataSetId= new Long[1];
          
-         }
+         if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+            
+             //User specified experimental design
+            HashMap<String, ArrayList<String>> _samplesByGroup = new HashMap<>();
+            HashMap<String, ArrayList<String>> _samplesAnalysisBySample = new HashMap<>();
+            HashMap<String, Long> _rsmIdBySampleAnalysis = new HashMap<>();
+            DDataset _quantiDS  = null;
+            
+            String errorMsg = null;
+            if(dialog.getDesignRSMNode() == null){
+                errorMsg = "No experimental design defined";
+                
+            } else if(!DataSetData.class.isInstance(dialog.getDesignRSMNode().getData())){
+                errorMsg = "Invalide Quantitation Dataset specified ";                    
+                
+            } else {
+                //*** Get experimental design values                
+                _quantiDS = ((DataSetData)dialog.getDesignRSMNode().getData()).getDataset();
+                                
+                Enumeration xicGrps = dialog.getDesignRSMNode().children();                  
+                //Iterate over Groups
+                while(xicGrps.hasMoreElements()&& errorMsg==null){
+                    RSMNode grpNode = (RSMNode) xicGrps.nextElement();
+                    String grpName = grpNode.getData().getName();
+
+                    //Iterate over Samples
+                    Enumeration grpSpls  = grpNode.children();      
+                    ArrayList<String> splNames = new ArrayList<>();
+                    while(grpSpls.hasMoreElements() && errorMsg==null){
+
+                        RSMNode splNode = (RSMNode) grpSpls.nextElement();
+                        String sampleName = splNode.getData().getName();
+                        splNames.add(sampleName);
+                        
+                        //Iterate over SampleAnalysis
+                        Enumeration identRSMs  = splNode.children();      
+                        ArrayList<String> splAnalysisNames = new ArrayList<>();
+                        while(identRSMs.hasMoreElements()){
+                            //VD TODO TEST child type
+                            RSMNode qChannelNode = (RSMNode) identRSMs.nextElement();
+                            String spAnalysisName = qChannelNode.getData().getName();
+                            splAnalysisNames.add(spAnalysisName);
+                            if(!DataSetData.class.isInstance(qChannelNode.getData())){
+                                errorMsg = "Invalide Sample Analysis specified ";
+                                break;
+                            }
+                            _rsmIdBySampleAnalysis.put(spAnalysisName, ((DataSetData)qChannelNode.getData()).getDataset().getResultSummaryId());                            
+                        }
+                        _samplesAnalysisBySample.put(sampleName, splAnalysisNames);
+                    } //End go through group's sample
+                    _samplesByGroup.put(grpName,splNames);
+                }//End go through Grp
+            }
+            
+            if(errorMsg != null) {
+                JOptionPane.showMessageDialog(dialog, errorMsg, "Warning", JOptionPane.ERROR_MESSAGE);
+                return;                
+            }
+            
+            m_logger.debug(" Will Compute XIC Quanti with "+_samplesByGroup.size()+" groups regrouping "+_rsmIdBySampleAnalysis.size()+" identification summary. ");              
+            // CallBack for Xic Quantitation Service
+            AbstractServiceCallback xicCallback = new AbstractServiceCallback() {
+
+                @Override
+                public boolean mustBeCalledInAWT() {
+                    return true;
+                }
+
+                @Override
+                public void run(boolean success) {
+                    if (success) {
+                        m_logger.debug(" XIC SUCCESS : "+_xicQuantiDataSetId[0]);
+                        
+                    } else {
+                        m_logger.debug(" XIC ERROR ");
+
+                    }
+                }
+            };
+    
+            RunXICTask task = new RunXICTask(xicCallback, _quantiDS, _samplesByGroup, _samplesAnalysisBySample, _rsmIdBySampleAnalysis, _xicQuantiDataSetId);          
+            AccessServiceThread.getAccessServiceThread().addTask(task);
+            
+         } //End OK entered         
      }
     
     @Override
--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/xic/CreateXICDialog.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/xic/CreateXICDialog.java
@@ -1,10 +1,13 @@
 package fr.proline.studio.rsmexplorer.gui.dialog.xic;
 
 import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.rsmexplorer.node.RSMNode;
 import fr.proline.studio.utils.IconManager;
 import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.Window;
+import java.util.Enumeration;
+import java.util.Map;
 import javax.swing.JScrollPane;
 
 
@@ -17,8 +20,10 @@ public class CreateXICDialog extends DefaultDialog {
     private static final int STEP_PANEL_DEFINE_GROUPS = 0;
     private static final int STEP_PANEL_MODIFY_GROUPS = 1;
     private static final int STEP_PANEL_DEFINE_SAMPLE_ANALYSIS = 2;
+    private static final int STEP_PANEL_DEFINE_QUANT_PARAMS = 3;
     private int m_step = STEP_PANEL_DEFINE_GROUPS;
     private static CreateXICDialog m_singletonDialog = null;
+    private RSMNode finalXICDesignNode = null;
 
     public static CreateXICDialog getDialog(Window parent) {
         if (m_singletonDialog == null) {
@@ -29,7 +34,7 @@ public class CreateXICDialog extends DefaultDialog {
         return m_singletonDialog;
     }
 
-    public CreateXICDialog(Window parent) {
+    private CreateXICDialog(Window parent) {
         super(parent, Dialog.ModalityType.APPLICATION_MODAL);
 
         setTitle("XIC Quantitation Wizard");
@@ -38,14 +43,33 @@ public class CreateXICDialog extends DefaultDialog {
 
         setButtonVisible(DefaultDialog.BUTTON_DEFAULT, false);
 
-        setButtonName(DefaultDialog.BUTTON_OK, "Next");
-        setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.ARROW));
-
-        setInternalComponent(DefineBioGroupsPanel.getDefineBioGroupsPanel());
-
+        init();
 
     }
+    
+    private void init(){
+        
+       setButtonName(DefaultDialog.BUTTON_OK, "Next");
+       setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.ARROW));
+ 
+       setInternalComponent(DefineBioGroupsPanel.getDefineBioGroupsPanel());
+       //TODO: reinitialize wizard panels...
+        
+    }
 
+    public final void reinit(){
+        init();
+         m_step = STEP_PANEL_DEFINE_GROUPS; 
+    }
+    
+    public RSMNode getDesignRSMNode(){
+        return finalXICDesignNode;
+    }
+    
+    public Map<String, Object> getQuantiParameters(){
+        return DefineQuantParamsPanel.getDefineQuantPanel().getQuantParams();
+    }
+    
     @Override
     protected boolean okCalled() {
 
@@ -106,14 +130,11 @@ public class CreateXICDialog extends DefaultDialog {
 
             // check values
                  
-            // change to ok button
-            setButtonName(DefaultDialog.BUTTON_OK, "OK");
-            setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.OK));
-
             
             JScrollPane scrollPane = new JScrollPane();
             String quantitationName = DefineBioGroupsPanel.getDefineBioGroupsPanel().getQuantitationName();
-            SetSampleAnalysisPanel defineSampleAnalysisPanel = new SetSampleAnalysisPanel(ModifyBioGroupsPanel.getDefinePanel().generateTreeNodes(quantitationName));
+            finalXICDesignNode = ModifyBioGroupsPanel.getDefinePanel().generateTreeNodes(quantitationName);
+            SetSampleAnalysisPanel defineSampleAnalysisPanel = SetSampleAnalysisPanel.getDialog(finalXICDesignNode);
             scrollPane.setViewportView(defineSampleAnalysisPanel);
 
             replaceInternaleComponent(scrollPane);
@@ -125,7 +146,53 @@ public class CreateXICDialog extends DefaultDialog {
             m_step = STEP_PANEL_DEFINE_SAMPLE_ANALYSIS;
             
             return false;
-        } else {  // STEP_PANEL_DEFINE_SAMPLE_ANALYSIS
+        } else if (m_step == STEP_PANEL_DEFINE_SAMPLE_ANALYSIS)  {  // STEP_PANEL_DEFINE_SAMPLE_ANALYSIS
+             // check values
+            if (finalXICDesignNode == null) {
+
+                setStatus(true, "You must correctly define your experimental design");
+                highlight(SetSampleAnalysisPanel.getDialog());
+                return false;
+
+            }
+
+             Enumeration xicGrps = finalXICDesignNode.children();
+            //Iterate over Groups
+            while (xicGrps.hasMoreElements()) {
+                RSMNode grpNode = (RSMNode) xicGrps.nextElement();
+                //Iterate over Samples
+                Enumeration grpSpls = grpNode.children();
+                while (grpSpls.hasMoreElements()) {
+                    RSMNode splNode = (RSMNode) grpSpls.nextElement();
+                    if (splNode.getChildCount() < 1) {
+                        setStatus(true, "You must specify at least one identifications summary for each sample.");
+                        highlight(SetSampleAnalysisPanel.getDialog());
+                        return false;
+                    }
+                }
+            }
+
+            // change to ok button
+            setButtonName(DefaultDialog.BUTTON_OK, "OK");
+            setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.OK));
+ 
+            //Update panel
+            
+            JScrollPane scrollPane = new JScrollPane();            
+            DefineQuantParamsPanel quantPanel =  DefineQuantParamsPanel.getDefineQuantPanel();
+            scrollPane.setViewportView(quantPanel);
+            scrollPane.createVerticalScrollBar();
+            
+            replaceInternaleComponent(quantPanel);
+
+            revalidate();
+            repaint();
+            
+            m_step = STEP_PANEL_DEFINE_QUANT_PARAMS;
+            
+            return false;
+        } else { //STEP_PANEL_DEFINE_QUANT_PARAMS
+            // check values TODO    
             return true;
         }
 

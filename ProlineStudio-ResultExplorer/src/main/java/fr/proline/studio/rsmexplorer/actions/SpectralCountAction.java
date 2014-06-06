d76dd98afb06e0e7ad57fededfb0ac4b6195456e
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-06-06 07:36:24 +0000

    forbid spectralcount. to get it use Profi=true in Preferences.properties

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/SpectralCountAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/SpectralCountAction.java
@@ -9,17 +9,17 @@ import fr.proline.studio.gui.DefaultDialog;
 import fr.proline.studio.pattern.WindowBox;
 import fr.proline.studio.pattern.WindowBoxFactory;
 import fr.proline.studio.rsmexplorer.DataBoxViewerTopComponent;
-import fr.proline.studio.rsmexplorer.gui.dialog.SetQuantitationDSNameDialog;
 import fr.proline.studio.rsmexplorer.gui.dialog.spectralcount.SpectralCountDialog;
-import fr.proline.studio.rsmexplorer.gui.dialog.TreeSelectionDialog;
 import fr.proline.studio.rsmexplorer.node.RSMDataSetNode;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
 import fr.proline.studio.rsmexplorer.node.IdentificationTree;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.prefs.Preferences;
 import javax.swing.JOptionPane;
 import org.openide.util.NbBundle;
+import org.openide.util.NbPreferences;
 import org.openide.windows.WindowManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -127,99 +127,17 @@ public class SpectralCountAction extends AbstractRSMAction {
         
     }
     
-    /*
-    public void actionPerformedOLD(RSMNode[] selectedNodes, final int x, final int y) { 
-     
-        
-        
-        
-        // Only Ref should be selected to compute SC         
-        final RSMDataSetNode refDatasetNode = (RSMDataSetNode) selectedNodes[0];
-        final SetQuantitationDSNameDialog qttDSDialog = SetQuantitationDSNameDialog.getDialog(WindowManager.getDefault().getMainWindow());
-        qttDSDialog.setLocation(x, y);
-
-        AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
-
-            @Override
-            public boolean mustBeCalledInAWT() {
-                return true;
-            }
-
-            @Override
-            public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
-                m_logger.debug(" Choose RSM for Weighted SC");
-
-                // check if we can compute SC
-                String error = null;
-                Map<String,Object> params = new HashMap();
-                ArrayList<DDataset> datasetList = new ArrayList<>();        
-                datasetList.add(refDatasetNode.getDataset()); //first entry is Reference Dataset in data box !
-                
-                if (m_treeSelectionDialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
-                   ArrayList<RSMDataSetNode> selectedDSNodes = m_treeSelectionDialog.getSelectedRSMDSNodeList();
-                   //A Voir :) 
-//                   if(selectedDSNodes.contains(refDatasetNode) && selectedDSNodes.size()>1) 
-//                        error = " Spectral Count is not possible on reference Node and some of its child";
-                       
-                   for (RSMDataSetNode dsNode : selectedDSNodes) {
-                       //TODO : Verif pas père + fils sélectionnés : Que père ou que fils ?!? 
-                        if (!dsNode.hasResultSummary()) {
-                            error = " Spectral Count is not possible on Search result ("+ dsNode.getDataset().getName() +").  Identification Summary should be created first";
-                            break;                       
-                        }
-                        if(dsNode.isChanging()){
-                            error = " Spectral Count is not possible while import or validation is on going. (Search result "+ dsNode.getDataset().getName() +")";
-                            break;  
-                        }
-                        datasetList.add(dsNode.getDataset());
-                        params.put(DS_LIST_PROPERTIES,datasetList);
-                   }
-                   
-                      
-                   qttDSDialog.setVisible(true);
-                    if (qttDSDialog.getButtonClicked() == SetQuantitationDSNameDialog.BUTTON_OK) {
-                        params.put(DS_NAME_PROPERTIES, qttDSDialog.getQuantiDSName());
-                        params.put(DS_DESCRIPTION_PROPERTIES, qttDSDialog.getQuantiDSDescriptionName());
-                    }  else { //Cancel / Close was clicked
-                        return;                                                
-                    }
-                   
-                } else { //Cancel / Close was clicked
-                    return;                                                
-                }
-
-                if (error != null) {
-                    JOptionPane.showMessageDialog(IdentificationTree.getCurrentTree(), error, "Warning", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-                
-                m_logger.debug(" Will Compute SC on "+(datasetList.size())+" RSMs : "+datasetList);
-                
-                WindowBox wbox = WindowBoxFactory.getRsmWSCWindowBox((String)params.get(DS_NAME_PROPERTIES), false) ;
-                wbox.setEntryData(refDatasetNode.getDataset().getProject().getId(), params);
-
-                // open a window to display the window box
-                DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
-                win.open();
-                win.requestActive(); 
-                
-            }
-        };
-        
-        //Create Child Tree to select RSM to compute SC for
-        final IdentificationTree childTree = IdentificationTree.getCurrentTree().copyDataSetRootSubTree(refDatasetNode.getDataset(),  refDatasetNode.getDataset().getProject().getId());
-        m_treeSelectionDialog = new TreeSelectionDialog(WindowManager.getDefault().getMainWindow(), childTree, "Select Identification Summaries for Spectral Count", 380, 500);
-        m_treeSelectionDialog.setButtonName(TreeSelectionDialog.BUTTON_OK, "Next");
-        m_treeSelectionDialog.setLocation(x, y);   
-        m_treeSelectionDialog.setVisible(true);  
-
-        IdentificationTree.getCurrentTree().loadInBackground(refDatasetNode, callback);
-
-    
-    }*/
 
     @Override
     public void updateEnabled(RSMNode[] selectedNodes) {
+        
+        // forbid this action for the moment  JPM.LATER
+        Preferences preferences = NbPreferences.root();
+        boolean showSpectralCount = preferences.getBoolean("Profi", false);
+        if (!showSpectralCount) {
+            setEnabled(false);
+            return;
+        }
         
          if (selectedNodes.length != 1) {
             setEnabled(false);

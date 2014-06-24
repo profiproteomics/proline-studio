9d0b964ad33b0ea9b8a43702b2bff283bf2f1641
Alexandre Walter <alexandre.walter@unistra.fr>
2014-06-24 16:20:29 +0000

    New feature: used for exporting spectrum graph to svg outpout format.

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportPictureWrapper.java
@@ -0,0 +1,20 @@
+package fr.proline.studio.export;
+
+
+import java.io.File;
+
+
+public class ExportPictureWrapper {
+
+	File m_graphicFile = null;
+
+	
+	public void setFile(File svgFile) {
+		m_graphicFile = svgFile;
+	}
+	
+	
+
+	
+	
+}

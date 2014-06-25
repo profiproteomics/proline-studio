09c50bf67c1d1b3af5e3ae4f1fdc8e1c1eef03b2
Alexandre Walter <alexandre.walter@unistra.fr>
2014-06-25 14:26:44 +0000

    update: allows storing 2 files description instead of 1. (to allow png output in 2 sizes)

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportPictureWrapper.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportPictureWrapper.java
@@ -7,10 +7,14 @@ import java.io.File;
 public class ExportPictureWrapper {
 
 	File m_graphicFile = null;
+	File m_graphicFile2 = null;
 
 	
 	public void setFile(File svgFile) {
 		m_graphicFile = svgFile;
+	}
+	public void setFile2(File svgFile) {
+		m_graphicFile2 = svgFile;
 	}
 	
 	

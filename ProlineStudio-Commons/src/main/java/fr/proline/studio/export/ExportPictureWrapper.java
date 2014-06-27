fe16db76aec71c5ae19469e46f4d5c0ff1ea6e77
Alexandre Walter <alexandre.walter@unistra.fr>
2014-06-27 10:14:13 +0000

    added both jfreesvg and batik svg export

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportPictureWrapper.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportPictureWrapper.java
@@ -6,14 +6,19 @@ import java.io.File;
 
 public class ExportPictureWrapper {
 
-	File m_graphicFile = null;
-	File m_graphicFile2 = null;
+	File m_graphicFile = null; // jfreesvg
+	File m_graphicFile2 = null; // "spectrum_tmp_3000x2000.png");
+	File m_graphicFile3 = null; // batik svg
 
 	
 	public void setFile(File svgFile) {
 		m_graphicFile = svgFile;
 	}
 	public void setFile2(File svgFile) {
+		m_graphicFile2 = svgFile;
+	}
+	
+	public void setFile3(File svgFile) {
 		m_graphicFile2 = svgFile;
 	}
 	

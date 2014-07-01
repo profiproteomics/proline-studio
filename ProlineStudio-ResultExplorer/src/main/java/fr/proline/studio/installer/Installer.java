2b018d3261c67c6fa8742be90190433299b309e1
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-07-01 06:14:53 +0000

    Renamme to Beta RC (not the same as our alpha tester)

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
@@ -23,7 +23,7 @@ public class Installer extends ModuleInstall {
     public void restored() {
 
         // set the proline version for the application title
-        System.setProperty("netbeans.buildnumber", "Beta 2 rc1"); 
+        System.setProperty("netbeans.buildnumber", "Beta 2 rc2"); 
         
         // initialize the connection to the server as soon as possible
         ServerConnectionManager.getServerConnectionManager();

5a4ea22b39eda747d65529275e1fe76c43004514
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-08-18 10:02:08 +0000

    modifications to take in charge different types of tree

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/AbstractRSMAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/AbstractRSMAction.java
@@ -1,8 +1,10 @@
 package fr.proline.studio.rsmexplorer.actions;
 
+import fr.proline.studio.rsmexplorer.gui.dialog.xic.DesignTree;
 import fr.proline.studio.rsmexplorer.node.RSMNode;
 import fr.proline.studio.rsmexplorer.node.IdentificationTree;
 import fr.proline.studio.rsmexplorer.node.QuantitationTree;
+import fr.proline.studio.rsmexplorer.node.RSMTree;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
@@ -16,42 +18,42 @@ import javax.swing.tree.TreePath;
  */
 public abstract class AbstractRSMAction extends AbstractAction {
 
-    private boolean m_onIdentificationTree;
-    
-    public AbstractRSMAction(String name) {
-        this(name, true);
-    }
-    public AbstractRSMAction(String name, boolean onIdentificationTree) {
+    private RSMTree.TreeType m_treeType;
+ 
+    public AbstractRSMAction(String name, RSMTree.TreeType treeType) {
         super(name);
         
-        m_onIdentificationTree = onIdentificationTree;
+        m_treeType = treeType;
     }
     
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if (m_onIdentificationTree) {
-            IdentificationTree tree = IdentificationTree.getCurrentTree();
-            TreePath treePath = tree.getSelectionPath();
-        
-            tree.getPathBounds(treePath);
-        
-            Rectangle r = tree.getPathBounds(treePath);
-            Point p = tree.getLocationOnScreen();
 
-            actionPerformed(tree.getSelectedNodes(), p.x+r.x+r.width/2, p.y+r.y+r.height/2);
-        } else {
-            QuantitationTree tree = QuantitationTree.getCurrentTree();
-            TreePath treePath = tree.getSelectionPath();
-        
-            tree.getPathBounds(treePath);
-        
-            Rectangle r = tree.getPathBounds(treePath);
-            Point p = tree.getLocationOnScreen();
-
-            actionPerformed(tree.getSelectedNodes(), p.x+r.x+r.width/2, p.y+r.y+r.height/2);
+        RSMTree tree = null;
+        switch (m_treeType) {
+            case TREE_IDENTIFICATION:
+                tree = IdentificationTree.getCurrentTree(); 
+                break;
+            case TREE_QUANTITATION:
+                tree = QuantitationTree.getCurrentTree(); 
+                break;
+            case TREE_XIC_DESIGN:
+                tree = DesignTree.getDesignTree();
+                break;
+            default:
+                return; // should not happen
         }
+    
+        TreePath treePath = tree.getSelectionPath();
+
+        tree.getPathBounds(treePath);
+
+        Rectangle r = tree.getPathBounds(treePath);
+        Point p = tree.getLocationOnScreen();
+
+        actionPerformed(tree.getSelectedNodes(), p.x + r.x + r.width / 2, p.y + r.y + r.height / 2);
+
     }
     
     public void actionPerformed(RSMNode[] selectedNodes, int x, int y) { 

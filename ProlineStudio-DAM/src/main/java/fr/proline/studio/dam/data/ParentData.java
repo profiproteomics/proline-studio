c423d86a494f438b3d610f503b808ca516b24bc1
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-09-12 13:27:08 +0000

    changed the name of a class used

--- ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/data/ParentData.java
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/data/ParentData.java
@@ -1,7 +1,7 @@
 package fr.proline.studio.dam.data;
 
 import fr.proline.studio.dam.AccessDatabaseThread;
-import fr.proline.studio.dam.UDSDataManager;
+import fr.proline.studio.dam.DatabaseDataManager;
 import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
 import fr.proline.studio.dam.tasks.AbstractDatabaseTask;
 import fr.proline.studio.dam.tasks.DatabaseProjectTask;
@@ -23,7 +23,7 @@ public class ParentData extends AbstractData {
     @Override
     public void load(AbstractDatabaseCallback callback, List<AbstractData> list, AbstractDatabaseTask.Priority priority) {
         DatabaseProjectTask task = new DatabaseProjectTask(callback);
-        task.initLoadProject(UDSDataManager.getUDSDataManager().getProjectUserName(), list);
+        task.initLoadProject(DatabaseDataManager.getDatabaseDataManager().getProjectUserName(), list);
         if (priority != null) {
             task.setPriority(priority);
         }

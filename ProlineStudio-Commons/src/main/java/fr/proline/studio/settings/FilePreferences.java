4c4f406d26eabc7d01fabf2997ca150883457eab
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-10-08 14:38:51 +0000

    added classes to save a part of settings in a file

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/settings/FilePreferences.java
@@ -0,0 +1,198 @@
+package fr.proline.studio.settings;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.prefs.AbstractPreferences;
+import java.util.prefs.BackingStoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Preferences implementation that stores to a user-defined file.
+ *
+ * @author Initial version : David Croft (<a href="http://www.davidc.net">www.davidc.net</a>)
+ * @version $Id: FilePreferences.java 283 2009-06-18 17:06:58Z david $
+ */
+public class FilePreferences extends AbstractPreferences {
+  
+  private static final Logger log = LoggerFactory.getLogger("ProlineStudio.Commons");
+  
+  private Map<String, String> root;
+  private Map<String, FilePreferences> children;
+  private boolean isRemoved = false;
+ 
+  private File m_file = null;
+  
+  public FilePreferences(File f, AbstractPreferences parent, String name)
+  {
+    super(parent, name);
+ 
+    m_file = f;
+    
+    root = new TreeMap<>();
+    children = new TreeMap<>();
+ 
+    try {
+      sync();
+    }
+    catch (BackingStoreException e) {
+      log.error("Unable to sync on creation of node " + name, e);
+    }
+  }
+ 
+    @Override
+  protected void putSpi(String key, String value)
+  {
+    root.put(key, value);
+    try {
+      flush();
+    }
+    catch (BackingStoreException e) {
+      log.error("Unable to flush after putting " + key, e);
+    }
+  }
+ 
+    @Override
+  protected String getSpi(String key)
+  {
+    return root.get(key);
+  }
+ 
+    @Override
+  protected void removeSpi(String key)
+  {
+    root.remove(key);
+    try {
+      flush();
+    }
+    catch (BackingStoreException e) {
+      log.error("Unable to flush after removing " + key, e);
+    }
+  }
+ 
+    @Override
+  protected void removeNodeSpi() throws BackingStoreException
+  {
+    isRemoved = true;
+    flush();
+  }
+ 
+    @Override
+  protected String[] keysSpi() throws BackingStoreException
+  {
+    return root.keySet().toArray(new String[root.keySet().size()]);
+  }
+ 
+    @Override
+  protected String[] childrenNamesSpi() throws BackingStoreException
+  {
+    return children.keySet().toArray(new String[children.keySet().size()]);
+  }
+ 
+    @Override
+  protected FilePreferences childSpi(String name)
+  {
+    FilePreferences child = children.get(name);
+    if (child == null || child.isRemoved()) {
+      child = new FilePreferences(m_file, this, name);
+      children.put(name, child);
+    }
+    return child;
+  }
+ 
+ 
+    @Override
+  protected void syncSpi() throws BackingStoreException
+  {
+    if (isRemoved()) return;
+ 
+
+    if (!m_file.exists()) return;
+ 
+    synchronized (m_file) {
+      Properties p = new Properties();
+      try {
+        p.load(new FileInputStream(m_file));
+ 
+        StringBuilder sb = new StringBuilder();
+        getPath(sb);
+        String path = sb.toString();
+ 
+        final Enumeration<?> pnen = p.propertyNames();
+        while (pnen.hasMoreElements()) {
+          String propKey = (String) pnen.nextElement();
+          if (propKey.startsWith(path)) {
+            String subKey = propKey.substring(path.length());
+            root.put(subKey, p.getProperty(propKey));
+          }
+        }
+      }
+      catch (IOException e) {
+        throw new BackingStoreException(e);
+      }
+    }
+  }
+ 
+  private void getPath(StringBuilder sb)
+  {
+    final FilePreferences parent = (FilePreferences) parent();
+    if (parent == null) return;
+ 
+    parent.getPath(sb);
+    sb.append(name()).append('.');
+  }
+ 
+  @Override
+  protected void flushSpi() throws BackingStoreException
+  {
+
+    synchronized (m_file) {
+      Properties p = new Properties();
+      try {
+ 
+        StringBuilder sb = new StringBuilder();
+        getPath(sb);
+        String path = sb.toString();
+ 
+        if (m_file.exists()) {
+          p.load(new FileInputStream(m_file));
+ 
+          List<String> toRemove = new ArrayList<>();
+ 
+          // Make a list of all direct children of this node to be removed
+          final Enumeration<?> pnen = p.propertyNames();
+          while (pnen.hasMoreElements()) {
+            String propKey = (String) pnen.nextElement();
+            if (propKey.startsWith(path)) {
+              String subKey = propKey.substring(path.length());
+              // Only do immediate descendants
+              if (subKey.indexOf('.') == -1) {
+                toRemove.add(propKey);
+              }
+            }
+          }
+ 
+          // Remove them now that the enumeration is done with
+          for (String propKey : toRemove) {
+            p.remove(propKey);
+          }
+        }
+ 
+        // If this node hasn't been removed, add back in any values
+        if (!isRemoved) {
+          for (String s : root.keySet()) {
+            p.setProperty(path + s, root.get(s));
+          }
+        }
+ 
+        p.store(new FileOutputStream(m_file), "FilePreferences");
+      }
+      catch (IOException e) {
+        throw new BackingStoreException(e);
+      }
+    }
+  }
+}
---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/settings/SettingsDialog.java
@@ -0,0 +1,248 @@
+package fr.proline.studio.settings;
+
+import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.utils.IconManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.prefs.Preferences;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.openide.util.NbPreferences;
+
+/**
+ *
+ * @author JM235353
+ */
+public class SettingsDialog extends DefaultDialog {
+    
+    private static final String DEFAULT_SETTINGS = "Default Settings";
+    
+    private JTextField m_fileTextField;
+    private JList<String> m_filesList;
+    private JButton m_openButton;
+    
+    private boolean m_defaultSettings = false;
+    private File m_selectedFile = null;
+    
+    private JFileChooser m_fileChooser = null;
+    private String m_chooserDefaultDirectory = null;
+    private ArrayList<String> m_potentialSettingsFile = null;
+    private String m_settingsKey = null;
+    
+    
+    public SettingsDialog(Window parent, String settingsKey) {
+        super(parent, Dialog.ModalityType.APPLICATION_MODAL);
+        
+        m_settingsKey = settingsKey;
+        
+        setTitle("Settings");
+        
+        setButtonVisible(BUTTON_HELP, false);
+        
+        m_potentialSettingsFile = readSettingsPaths(settingsKey);
+        setInternalComponent(createInternalPanel(m_potentialSettingsFile));
+    }
+    
+    private JPanel createInternalPanel(ArrayList<String> potentialSettingsFile) {
+        JPanel internalPanel = new JPanel(new GridBagLayout());
+        internalPanel.setBorder(BorderFactory.createTitledBorder(" Settings File Selection "));
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
+        // create graphical objects
+        m_openButton = new JButton(IconManager.getIcon(IconManager.IconType.OPEN_FILE));
+        m_fileTextField = new JTextField(30);
+        
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        listModel.addElement(DEFAULT_SETTINGS);
+        for (int i=0;i<potentialSettingsFile.size();i++) {
+            listModel.addElement(potentialSettingsFile.get(i));
+        }
+        m_filesList = new JList<>(listModel);
+        
+        JScrollPane filesListScrollPane = new JScrollPane(m_filesList) {
+
+            private Dimension preferredSize = new Dimension(280, 140);
+
+            @Override
+            public Dimension getPreferredSize() {
+                return preferredSize;
+            }
+        };
+ 
+        // actions on object
+        m_openButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (m_fileChooser == null) {
+                    m_fileChooser = new JFileChooser(m_chooserDefaultDirectory);
+                    m_fileChooser.setMultiSelectionEnabled(false);
+                    FileNameExtensionFilter filter = new FileNameExtensionFilter("Proline Settings File", "settings");
+                    m_fileChooser.addChoosableFileFilter(filter);
+                    m_fileChooser.setFileFilter(filter);
+                }
+                int result = m_fileChooser.showOpenDialog(m_openButton);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    m_fileTextField.setText(m_fileChooser.getSelectedFile().getAbsolutePath());
+                }
+            }
+            
+        });
+        
+        m_filesList.addListSelectionListener(new ListSelectionListener() {
+
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                String value = m_filesList.getSelectedValue();
+                if (value != null) {
+                    m_fileTextField.setText(m_filesList.getSelectedValue());
+                }
+            }
+        });
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1.0;
+        internalPanel.add(m_fileTextField, c);
+        
+        c.gridx++;
+        c.weightx = 0;
+        internalPanel.add(m_openButton, c);
+
+        c.gridy++;
+        c.gridx = 0;
+        c.gridwidth = 2;
+        internalPanel.add(filesListScrollPane, c);
+
+        return internalPanel;
+    }
+    
+    @Override
+    protected boolean okCalled() {
+        
+        String filePath = m_fileTextField.getText().trim();
+        
+        if (filePath.isEmpty()) {
+            setStatus(true, "You must choose a Settings File");
+            highlight(m_fileTextField);
+            return false;
+        }
+        
+        if (filePath.compareToIgnoreCase(DEFAULT_SETTINGS) == 0) {
+            m_defaultSettings = true;
+            return true;
+        }
+        
+        
+        File f = new File(filePath);
+        if (!f.exists()) {
+            setStatus(true, "This file does not exist");
+            highlight(m_fileTextField);
+            return false;
+        }
+        if (f.isDirectory()) {
+            setStatus(true, "You must select a File, not a Directory");
+            highlight(m_fileTextField);
+            return false;
+        }
+        if (!f.canRead()) {
+            setStatus(true, "The file is not readable");
+            highlight(m_fileTextField);
+            return false;
+        }
+
+        m_selectedFile = f;
+        
+        String path = f.getAbsolutePath();
+        boolean pathFound = false;
+        for (int i=0;i<m_potentialSettingsFile.size();i++) {
+            String pathCur = m_potentialSettingsFile.get(i);
+            if (pathCur.compareTo(path) == 0) {
+                pathFound = true;
+                break;
+            }
+        }
+        if (!pathFound) {
+            m_potentialSettingsFile.add(0, f.getAbsolutePath());
+        }
+        writeSettingsPath(m_settingsKey, m_potentialSettingsFile);
+        
+        return true;
+    }
+    
+    
+    
+    @Override
+    protected boolean cancelCalled() {
+        return true;
+    }
+    
+    public boolean isDefaultSettingsSelected() {
+        return m_defaultSettings;
+    }
+    
+    public File getSelectedFile() {
+        return m_selectedFile;
+    }
+    
+    
+    
+    private ArrayList<String> readSettingsPaths(String settingsKey) {
+        
+        Preferences preferences = NbPreferences.root();
+        
+        String settingsDirectoryKey = settingsKey+".filePath_";
+        
+        
+        m_chooserDefaultDirectory = preferences.get(settingsDirectoryKey, null);
+        
+        ArrayList<String> filePathList = new ArrayList();
+        int i = 1;
+        while (i<10) {
+            String filePath = preferences.get(settingsDirectoryKey+i, null);
+            if (filePath == null) {
+                break;
+            }
+            filePathList.add(filePath);
+            i++;
+        } 
+
+        
+        return filePathList;
+    }
+    private void writeSettingsPath(String settingsKey, ArrayList<String> filePathList) {
+
+        Preferences preferences = NbPreferences.root();
+        
+        String settingsDirectoryKey = settingsKey+".filePath_";
+        
+        // remove previous file path
+        int i = 1;
+        while (i<10) {
+            String key = settingsDirectoryKey+i;
+        
+            String regex = preferences.get(key, null);
+            if (regex == null) {
+                break;
+            }
+            preferences.remove(key);
+            i++;
+        }
+        
+        // put new file path
+        for (i=0;i<filePathList.size();i++) {
+            String key = settingsDirectoryKey+(i+1);
+            preferences.put(key, filePathList.get(i));
+        }
+    }
+
+}

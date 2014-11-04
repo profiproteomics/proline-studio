581f28e7172f9cdf1659192279d4dc06478a60bf
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2014-11-04 08:04:41 +0000

    version changed

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
@@ -24,7 +24,7 @@ public class Installer extends ModuleInstall {
     public void restored() {
 
         // set the proline version for the application title
-        System.setProperty("netbeans.buildnumber", "Beta 2 Release"); 
+        System.setProperty("netbeans.buildnumber", "Beta 3 M1"); 
         
         // initialize the connection to the server as soon as possible
         ServerConnectionManager.getServerConnectionManager();

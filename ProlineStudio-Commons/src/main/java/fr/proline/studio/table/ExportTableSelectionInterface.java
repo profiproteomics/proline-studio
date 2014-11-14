2bd77655e78d91be75e28fe5a0301c14d1fb2683
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-11-14 08:28:12 +0000

    added interfaces to export/import table selection

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/table/ExportTableSelectionInterface.java
@@ -0,0 +1,13 @@
+package fr.proline.studio.table;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author JM235353
+ */
+public interface ExportTableSelectionInterface {
+    
+    public HashSet exportSelection(int[] rows);
+    
+}
---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/table/ImportTableSelectionInterface.java
@@ -0,0 +1,11 @@
+package fr.proline.studio.table;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author JM235353
+ */
+public interface ImportTableSelectionInterface {
+    public void importSelection(HashSet selectedData);
+}

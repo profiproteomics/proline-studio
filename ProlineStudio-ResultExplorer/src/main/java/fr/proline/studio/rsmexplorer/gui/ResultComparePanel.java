fa4d88bf7b9328af1268cb54d19e2bbe5ec7f408
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-11-18 09:26:47 +0000

    panels for data mixing

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/ResultComparePanel.java
@@ -0,0 +1,154 @@
+package fr.proline.studio.rsmexplorer.gui;
+
+import fr.proline.studio.comparedata.CompareDataInterface;
+import fr.proline.studio.comparedata.CompareTableModel;
+import fr.proline.studio.export.ExportButton;
+import fr.proline.studio.filter.FilterButton;
+import fr.proline.studio.gui.SplittedPanelContainer;
+import fr.proline.studio.markerbar.MarkerContainerPanel;
+import fr.proline.studio.pattern.AbstractDataBox;
+import fr.proline.studio.pattern.DataBoxPanelInterface;
+import fr.proline.studio.table.DecoratedMarkerTable;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+
+/**
+ *
+ * @author JM235353
+ */
+public class ResultComparePanel extends JPanel implements DataBoxPanelInterface {
+
+    private AbstractDataBox m_dataBox;
+
+
+    private FilterButton m_filterButton;
+    private ExportButton m_exportButton;
+    
+    private JScrollPane m_dataScrollPane = new JScrollPane();
+    private DataTable m_dataTable;
+    private MarkerContainerPanel m_markerContainerPanel;
+    
+    public ResultComparePanel() {
+        
+        setLayout(new BorderLayout());
+        setBounds(0, 0, 500, 400);
+
+
+        JPanel internalPanel = initComponents();
+        add(internalPanel, BorderLayout.CENTER);
+
+        JToolBar toolbar = initToolbar();
+        add(toolbar, BorderLayout.WEST);
+    }
+    
+    private JPanel initComponents() {
+
+
+        JPanel internalPanel = new JPanel();
+
+        internalPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
+
+        // create objects
+        m_dataScrollPane = new JScrollPane();
+
+        m_dataTable = new DataTable();
+        m_dataTable.setModel(new CompareTableModel(null));
+        
+
+
+        m_markerContainerPanel = new MarkerContainerPanel(m_dataScrollPane, m_dataTable);
+
+        m_dataScrollPane.setViewportView(m_dataTable);
+        m_dataTable.setFillsViewportHeight(true);
+        m_dataTable.setViewport(m_dataScrollPane.getViewport());
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 3;
+        internalPanel.add(m_markerContainerPanel, c);
+
+
+        return internalPanel;
+
+    }
+    
+    private JToolBar initToolbar() {
+        JToolBar toolbar = new JToolBar(JToolBar.VERTICAL);
+        toolbar.setFloatable(false);
+
+
+        //m_filterButton = new FilterButton(((CompareTableModel) m_dataTable.getModel()));
+        m_exportButton = new ExportButton(((CompareTableModel) m_dataTable.getModel()), "Data", m_dataTable);
+
+
+        //toolbar.add(m_filterButton); //JPM.TODO : model not finished for filtering
+        toolbar.add(m_exportButton);
+
+        return toolbar;
+    }
+    
+    
+    public void setData(CompareDataInterface dataInterface) {
+        ((CompareTableModel) m_dataTable.getModel()).setDataInterface(dataInterface);
+        m_markerContainerPanel.setMaxLineNumber(dataInterface.getRowCount());
+    }
+ 
+
+    @Override
+    public void setDataBox(AbstractDataBox dataBox) {
+        m_dataBox = dataBox;
+    }
+    @Override
+    public AbstractDataBox getDataBox() {
+        return m_dataBox;
+    }
+    
+    @Override
+    public ActionListener getRemoveAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getRemoveAction(splittedPanel);
+    }
+
+    @Override
+    public ActionListener getAddAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getAddAction(splittedPanel);
+    }
+
+    @Override
+    public ActionListener getSaveAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getSaveAction(splittedPanel);
+    }
+
+    @Override
+    public void setLoading(int id) {}
+
+    @Override
+    public void setLoading(int id, boolean calculating) {}
+
+    @Override
+    public void setLoaded(int id) {}
+
+    
+    private class DataTable extends DecoratedMarkerTable {
+
+        public DataTable() {
+
+        }
+
+        public void dataUpdated() {
+
+        }
+
+    }
+}
---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/SelectComparePanel.java
@@ -0,0 +1,196 @@
+package fr.proline.studio.rsmexplorer.gui;
+
+import fr.proline.studio.comparedata.DiffDataModel;
+import fr.proline.studio.gui.SplittedPanelContainer;
+import fr.proline.studio.pattern.AbstractDataBox;
+import fr.proline.studio.pattern.DataBoxPanelInterface;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import fr.proline.studio.comparedata.CompareDataInterface;
+import fr.proline.studio.comparedata.JoinDataModel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+
+/**
+ *
+ * @author JM235353
+ */
+public class SelectComparePanel extends JPanel implements DataBoxPanelInterface {
+
+    private AbstractDataBox m_dataBox;
+
+    private CompareDataInterface m_result = null;
+    private CompareDataInterface m_compareDataInterface1;
+    private CompareDataInterface m_compareDataInterface2;
+    
+    private JScrollPane m_dataScrollPane;
+    private JLabel m_label1;
+    private JLabel m_label2;
+    private JComboBox m_algorithmCombobox;
+    
+    private final static String[] ALGOS = { "Difference", "Join" };
+    private final static int ALGO_DIFF = 0;
+    private final static int ALGO_JOIN = 1;
+    
+    public SelectComparePanel() {
+                setLayout(new BorderLayout());
+        setBounds(0, 0, 500, 400);
+
+
+        JPanel internalPanel = initComponents();
+        add(internalPanel, BorderLayout.CENTER);
+
+        JToolBar toolbar = initToolbar();
+        add(toolbar, BorderLayout.WEST);
+    }
+    
+    
+    private JPanel initComponents() {
+
+
+        JPanel internalPanel = new JPanel();
+
+        internalPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
+
+        // create objects
+        m_dataScrollPane = new JScrollPane();
+
+        JPanel graphPanel = createGraphPanel();
+
+        m_dataScrollPane.setViewportView(graphPanel);
+
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 3;
+        internalPanel.add(m_dataScrollPane, c);
+
+
+        return internalPanel;
+
+    }
+    
+    private JPanel createGraphPanel() {
+        JPanel p = new JPanel();
+        
+        p.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+        
+        m_label1 = new JLabel();
+        m_label2 = new JLabel();
+        
+        m_algorithmCombobox = new JComboBox(ALGOS);
+        m_algorithmCombobox.setSelectedIndex(0);
+        m_algorithmCombobox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                applyAlgorithm();
+            }
+            
+        });
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        p.add(m_label1, c);
+        
+        c.gridy++;
+        p.add(m_label2, c);
+        
+        c.gridy++;
+        p.add(m_algorithmCombobox, c);
+        
+        return p;
+
+    }
+    
+    private JToolBar initToolbar() {
+        JToolBar toolbar = new JToolBar(JToolBar.VERTICAL);
+        toolbar.setFloatable(false);
+
+        //JPM.TODO
+
+        return toolbar;
+    }
+    
+    public void applyAlgorithm() {
+        int algo = m_algorithmCombobox.getSelectedIndex();
+        if (algo == ALGO_DIFF) {
+            DiffDataModel diffModel = new DiffDataModel();
+            diffModel.setData(m_compareDataInterface1, m_compareDataInterface2);
+
+            m_result = diffModel;
+        } else {
+            JoinDataModel joinDataModel = new JoinDataModel();
+            joinDataModel.setData(m_compareDataInterface1, m_compareDataInterface2);
+            
+            m_result = joinDataModel;
+        }
+    }
+    
+    public void setData(CompareDataInterface compareDataInterface1, CompareDataInterface compareDataInterface2) {
+        m_compareDataInterface1 = compareDataInterface1;
+        m_compareDataInterface2 = compareDataInterface2;
+
+        m_label1.setText(compareDataInterface1.getName());
+        m_label2.setText(compareDataInterface2.getName());
+
+        applyAlgorithm();
+    }
+           
+    public CompareDataInterface getResultDataInterface() {
+        return m_result;
+    }
+
+    @Override
+    public void setDataBox(AbstractDataBox dataBox) {
+        m_dataBox = dataBox;
+    }
+    @Override
+    public AbstractDataBox getDataBox() {
+        return m_dataBox;
+    }
+    
+    @Override
+    public ActionListener getRemoveAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getRemoveAction(splittedPanel);
+    }
+
+    @Override
+    public ActionListener getAddAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getAddAction(splittedPanel);
+    }
+
+    @Override
+    public ActionListener getSaveAction(SplittedPanelContainer splittedPanel) {
+        return m_dataBox.getSaveAction(splittedPanel);
+    }
+
+    @Override
+    public void setLoading(int id) {}
+
+    @Override
+    public void setLoading(int id, boolean calculating) {}
+
+    @Override
+    public void setLoaded(int id) {}
+
+    
+}

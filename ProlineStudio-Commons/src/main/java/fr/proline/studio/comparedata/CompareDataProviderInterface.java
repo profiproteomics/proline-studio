4d9c7193626b5c2180633cc69185bd4aa29227fb
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2014-11-25 12:32:51 +0000

    interface to provide data

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/comparedata/CompareDataProviderInterface.java
@@ -0,0 +1,9 @@
+package fr.proline.studio.comparedata;
+
+/**
+ *
+ * @author JM235353
+ */
+public interface CompareDataProviderInterface {
+    public CompareDataInterface getCompareDataInterface();
+}

089532bae7d8fd59245177af7a7756429f92aaea
Alexandre Walter <alexandre.walter@unistra.fr>
2014-12-02 10:44:34 +0000

    Added code to deal with msdiag settings change from user input

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/GenerateMSDiagReportAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/GenerateMSDiagReportAction.java
@@ -2,6 +2,7 @@ package fr.proline.studio.rsmexplorer.actions.identification;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import fr.proline.core.orm.msi.ResultSet;
 import fr.proline.core.orm.uds.dto.DDataset;
 import fr.proline.studio.dam.AccessDatabaseThread;
@@ -9,11 +10,11 @@ import fr.proline.studio.dam.data.DataSetData;
 import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
 import fr.proline.studio.dam.tasks.DatabaseDataSetTask;
 import fr.proline.studio.dam.tasks.SubTask;
-import fr.proline.studio.dpm.AccessServiceThread;
-import fr.proline.studio.dpm.task.AbstractServiceCallback;
+import fr.proline.studio.gui.DefaultDialog;
 import fr.proline.studio.pattern.WindowBox;
 import fr.proline.studio.pattern.WindowBoxFactory;
 import fr.proline.studio.rsmexplorer.DataBoxViewerTopComponent;
+import fr.proline.studio.rsmexplorer.gui.dialog.MSDiagDialog;
 import fr.proline.studio.rsmexplorer.tree.DataSetNode;
 import fr.proline.studio.rsmexplorer.tree.AbstractNode;
 import fr.proline.studio.rsmexplorer.tree.AbstractTree;
@@ -22,6 +23,7 @@ import fr.proline.studio.rsmexplorer.tree.identification.IdentificationTree;
 import javax.swing.tree.DefaultTreeModel;
 
 import org.openide.util.NbBundle;
+import org.openide.windows.WindowManager;
 
 /**
  * Action to launch the msdiag generation of the report. action
@@ -47,22 +49,39 @@ public class GenerateMSDiagReportAction extends AbstractRSMAction {
             final DataSetNode dataSetNode = (DataSetNode) selectedNodes[i];
 
            // actionImpl(dataSetNode);
-            AbstractServiceCallback callback = new AbstractServiceCallback() {
+         //   AbstractServiceCallback callback = new AbstractServiceCallback() {
 
-                @Override
-                public boolean mustBeCalledInAWT() {
-                    return true;
-                }
+//                @Override
+//                public boolean mustBeCalledInAWT() {
+//                    return true;
+//                }
+//
+//                @Override
+//                public void run(boolean success) {
+//                	dataSetNode.setIsChanging(false);
+//                    treeModel.nodeChanged(dataSetNode);
+//                }
+//            };
 
-                @Override
-                public void run(boolean success) {
-                	dataSetNode.setIsChanging(false);
-                    treeModel.nodeChanged(dataSetNode);
-                }
-            };
-
+            MSDiagDialog dialog = MSDiagDialog.getDialog(WindowManager.getDefault().getMainWindow());
+            dialog.setLocation(x, y);
+            dialog.setVisible(true);
+            
+            HashMap<String,String> msdiagParams = null;
+            if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+                
+                 // retrieve parameters
+            	
+               msdiagParams = dialog.getMSDiagSettings();
+              
+            } else
+            {
+            	return;
+            }
+            
             ArrayList resultMessages = new ArrayList(0);
-            resultMessages.add("message initialized in GenerateMSDiag Action");
+            resultMessages.add(msdiagParams);
+            
            
             actionImpl(dataSetNode,resultMessages);
             
@@ -83,11 +102,13 @@ public class GenerateMSDiagReportAction extends AbstractRSMAction {
        // final boolean hasResultSummary = dataSetNode.hasResultSummary();
         
         
+       
         
         if (rset != null) {
         
             // prepare window box
-            WindowBox wbox = WindowBoxFactory.getMSDiagWindowBox(dataSet.getName()+" MSDiag" , resultMessages.get(0).toString()); 
+            //WindowBox wbox = WindowBoxFactory.getMSDiagWindowBox(dataSet.getName()+" MSDiag" , resultMessages.get(0).toString());
+        	WindowBox wbox = WindowBoxFactory.getMSDiagWindowBox(dataSet.getName()+" MSDiag" , (HashMap<String,String>) resultMessages.get(0));
             wbox.setEntryData(dataSet.getProject().getId(), rset); 
             
             // open a window to display the window box
@@ -96,7 +117,7 @@ public class GenerateMSDiagReportAction extends AbstractRSMAction {
             win.requestActive();
         } else {
             
-            final WindowBox wbox = WindowBoxFactory.getMSDiagWindowBox(dataSet.getName()+" MSDiag" , resultMessages.get(0).toString()); 
+            final WindowBox wbox = WindowBoxFactory.getMSDiagWindowBox(dataSet.getName()+" MSDiag" , (HashMap<String,String>) resultMessages.get(0)); 
             // open a window to display the window box
             DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
             win.open();

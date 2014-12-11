0b5d4ce16b291238d77b084af2356050fa1fc056
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2014-12-11 10:07:38 +0000

    check computeProfile added

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/ExportXICAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/ExportXICAction.java
@@ -1,10 +1,13 @@
 package fr.proline.studio.rsmexplorer.actions.identification;
 
-
 import fr.proline.core.orm.uds.Dataset;
 import fr.proline.core.orm.uds.QuantitationMethod;
 import fr.proline.core.orm.uds.dto.DDataset;
+import fr.proline.studio.dam.AccessDatabaseThread;
 import fr.proline.studio.dam.data.DataSetData;
+import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
+import fr.proline.studio.dam.tasks.DatabaseDataSetTask;
+import fr.proline.studio.dam.tasks.SubTask;
 import fr.proline.studio.dpm.AccessServiceThread;
 import fr.proline.studio.dpm.task.AbstractServiceCallback;
 import fr.proline.studio.dpm.task.DownloadFileTask;
@@ -12,6 +15,8 @@ import fr.proline.studio.dpm.task.ExportXICTask;
 import fr.proline.studio.export.ExportDialog;
 import fr.proline.studio.export.ExporterFactory;
 import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.gui.OptionDialog;
+import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
 import fr.proline.studio.rsmexplorer.tree.DataSetNode;
 import fr.proline.studio.rsmexplorer.tree.AbstractNode;
 import fr.proline.studio.rsmexplorer.tree.AbstractTree;
@@ -22,148 +27,175 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Action to retrieve previously computed spectral count and display it
+ *
  * @author VD225637
- * 
+ *
  */
 public class ExportXICAction extends AbstractRSMAction {
- 
-  public enum ExportType {
-        MASTER_QPEP_IONS("master_quant_peptide_ions", "Quant Peptides ions"),MASTER_QPEPS("master_quant_peptides", "Quant Peptides"), BASIC_MASTER_QPROT_SETS("basic_master_quant_protein_sets", "Quant Proteins Sets"), MASTER_QPROT_SETS("master_quant_protein_sets", "Quant Proteins Sets Profiles");
-        
+
+    public enum ExportType {
+
+        MASTER_QPEP_IONS("master_quant_peptide_ions", "Quant Peptides ions"), MASTER_QPEPS("master_quant_peptides", "Quant Peptides"), BASIC_MASTER_QPROT_SETS("basic_master_quant_protein_sets", "Quant Proteins Sets"), MASTER_QPROT_SETS("master_quant_protein_sets", "Quant Proteins Sets Profiles");
+
         private String m_value;
         private String m_label;
 
         public String getAsParameter() {
             return m_value;
         }
-        
+
         public String getAsLabel() {
             return m_label;
         }
 
         private ExportType(String value, String label) {
             this.m_value = value;
-            this.m_label = label;    
+            this.m_label = label;
         }
-        
+
     };
-    
+
     protected static final Logger m_logger = LoggerFactory.getLogger("ProlineStudio.ResultExplorer");
     private ExportType m_exportType = null;
-    
+
     public ExportXICAction(ExportType type) {
-        super(NbBundle.getMessage(ExportXICAction.class, "CTL_PrefixExportXICAction")+type.getAsLabel()+"...", AbstractTree.TreeType.TREE_QUANTITATION);
-        m_exportType = type; 
+        super(NbBundle.getMessage(ExportXICAction.class, "CTL_PrefixExportXICAction") + type.getAsLabel() + "...", AbstractTree.TreeType.TREE_QUANTITATION);
+        m_exportType = type;
     }
-    
+
     @Override
-    public void actionPerformed(AbstractNode[] selectedNodes, int x, int y) { 
-        
+    public void actionPerformed(AbstractNode[] selectedNodes, int x, int y) {
+        final int posx = x;
+        final int posy = y;
         // Only Ref should be selected to compute SC         
-
         final DataSetNode xicDatasetNode = (DataSetNode) selectedNodes[0];
-        
+        //dataset
+        final DDataset dataSet = xicDatasetNode.getDataset();
 
-        final ExportDialog dialog = ExportDialog.getDialog(WindowManager.getDefault().getMainWindow(),false, ExporterFactory.EXPORT_XIC);
-        
-        DefaultDialog.ProgressTask task = new DefaultDialog.ProgressTask() {
+        // call back for loading masterQuantChannel
+        AbstractDatabaseCallback masterQuantChannelCallback = new AbstractDatabaseCallback() {
 
             @Override
-            public int getMinValue() {
-                return 0;
+            public boolean mustBeCalledInAWT() {
+                return true;
             }
 
             @Override
-            public int getMaxValue() {
-                return 100;
-            }
-
-            @Override
-            protected Object doInBackground() throws Exception {
-
-
-                final AbstractServiceCallback downloadCallback = new AbstractServiceCallback() {
-
-                    @Override
-                    public boolean mustBeCalledInAWT() {
-                        return true;
-                    }
-
-                    @Override
-                    public void run(boolean success) {
-                        if (success) {
-
-                            setProgress(100);
-
-                        } else {
-                            // nothing to do
-                            // failed
-                            setProgress(100);
+            public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
+                if (success) {
+                    // check if profilizer has already been launched
+                    if ((m_exportType.equals(ExportType.MASTER_QPROT_SETS)) && dataSet.getPostQuantProcessingConfig() == null) {
+                        String message = "No Compute Quantitative Profiles has been launched on this XIC.\nSome data may be empty.\nDo you want to continue?";
+                        OptionDialog yesNoDialog = new OptionDialog(WindowManager.getDefault().getMainWindow(), "Export profile", message);
+                        yesNoDialog.setLocation(posx, posy);
+                        yesNoDialog.setVisible(true);
+                        if (yesNoDialog.getButtonClicked() != DefaultDialog.BUTTON_OK) {
+                            return;
                         }
                     }
-                };
+                    final ExportDialog dialog = ExportDialog.getDialog(WindowManager.getDefault().getMainWindow(), false, ExporterFactory.EXPORT_XIC);
 
-                // used as out parameter for the service
-                final String[] _filePath = new String[1];
+                    DefaultDialog.ProgressTask task = new DefaultDialog.ProgressTask() {
 
-                AbstractServiceCallback exportCallback = new AbstractServiceCallback() {
+                        @Override
+                        public int getMinValue() {
+                            return 0;
+                        }
 
-                    @Override
-                    public boolean mustBeCalledInAWT() {
-                        return true;
-                    }
+                        @Override
+                        public int getMaxValue() {
+                            return 100;
+                        }
 
-                    @Override
-                    public void run(boolean success) {
-                        if (success) {
+                        @Override
+                        protected Object doInBackground() throws Exception {
 
+                            final AbstractServiceCallback downloadCallback = new AbstractServiceCallback() {
+
+                                @Override
+                                public boolean mustBeCalledInAWT() {
+                                    return true;
+                                }
+
+                                @Override
+                                public void run(boolean success) {
+                                    if (success) {
+
+                                        setProgress(100);
+
+                                    } else {
+                                        // nothing to do
+                                        // failed
+                                        setProgress(100);
+                                    }
+                                }
+                            };
+
+                            // used as out parameter for the service
+                            final String[] _filePath = new String[1];
+
+                            AbstractServiceCallback exportCallback = new AbstractServiceCallback() {
+
+                                @Override
+                                public boolean mustBeCalledInAWT() {
+                                    return true;
+                                }
+
+                                @Override
+                                public void run(boolean success) {
+                                    if (success) {
+
+                                        String fileName = dialog.getFileName();
+                                        if (!fileName.endsWith(".xls") && !fileName.endsWith(".xlsx")) {
+                                            fileName += ".xls";
+                                        }
+                                        DownloadFileTask task = new DownloadFileTask(downloadCallback, fileName, _filePath[0]);
+                                        AccessServiceThread.getAccessServiceThread().addTask(task);
+
+                                    } else {
+                                        // nothing to do
+                                        // failed
+                                        setProgress(100);
+                                    }
+                                }
+                            };
+
+                            String templateName = "";
                             String fileName = dialog.getFileName();
-                            if (!fileName.endsWith(".xls") && !fileName.endsWith(".xlsx")) {
-                                fileName += ".xls";
+                            boolean isXLSX = fileName.endsWith(".xlsx");
+                            if (m_exportType.equals(ExportType.BASIC_MASTER_QPROT_SETS)) {
+                                templateName = isXLSX ? "BASIC_MASTER_QUANT_PROTEIN_SETS_XLSX" : "BASIC_MASTER_QUANT_PROTEIN_SETS_TSV";
+                            } else if (m_exportType.equals(ExportType.MASTER_QPROT_SETS)) {
+                                templateName = isXLSX ? "MASTER_QUANT_PROTEIN_SETS_XLSX" : "MASTER_QUANT_PROTEIN_SETS_TSV";
+                            } else if (m_exportType.equals(ExportType.MASTER_QPEPS)) {
+                                templateName = isXLSX ? "MASTER_QUANT_PEPTIDES_XLSX" : "MASTER_QUANT_PEPTIDES_TSV";
+                            } else if (m_exportType.equals(ExportType.MASTER_QPEP_IONS)) {
+                                templateName = isXLSX ? "MASTER_QUANT_PEPTIDE_IONS_XLSX" : "MASTER_QUANT_PEPTIDE_IONS_TSV";
                             }
-                            DownloadFileTask task = new DownloadFileTask(downloadCallback, fileName, _filePath[0]);
+                            ExportXICTask task = new ExportXICTask(exportCallback, xicDatasetNode.getDataset(), m_exportType.getAsParameter(), _filePath, templateName);
                             AccessServiceThread.getAccessServiceThread().addTask(task);
 
-                        } else {
-                            // nothing to do
-                            // failed
-                            setProgress(100);
+                            return null;
                         }
-                    }
-                };
+                    };
 
-                String templateName = "";
-                String fileName = dialog.getFileName();
-                boolean isXLSX = fileName.endsWith(".xlsx");
-                if (m_exportType.equals(ExportType.BASIC_MASTER_QPROT_SETS)) {
-                    templateName = isXLSX ? "BASIC_MASTER_QUANT_PROTEIN_SETS_XLSX" : "BASIC_MASTER_QUANT_PROTEIN_SETS_TSV";
-                } else if (m_exportType.equals(ExportType.MASTER_QPROT_SETS)) {
-                    templateName = isXLSX ? "MASTER_QUANT_PROTEIN_SETS_XLSX" : "MASTER_QUANT_PROTEIN_SETS_TSV";
-                }else if (m_exportType.equals(ExportType.MASTER_QPEPS)) {
-                    templateName = isXLSX ? "MASTER_QUANT_PEPTIDES_XLSX" : "MASTER_QUANT_PEPTIDES_TSV";
-                }else if (m_exportType.equals(ExportType.MASTER_QPEP_IONS)) {
-                    templateName = isXLSX ? "MASTER_QUANT_PEPTIDE_IONS_XLSX" : "MASTER_QUANT_PEPTIDE_IONS_TSV";
+                    dialog.setTask(task);
+                    dialog.setLocation(posx, posy);
+                    dialog.setVisible(true);
                 }
-                ExportXICTask task = new ExportXICTask(exportCallback, xicDatasetNode.getDataset(), m_exportType.getAsParameter(), _filePath, templateName);
-                AccessServiceThread.getAccessServiceThread().addTask(task);
-
-                
-
-                return null;
             }
+
         };
-
-
-        dialog.setTask(task);
-        dialog.setLocation(x, y);
-        dialog.setVisible(true);
+        DatabaseDataSetTask loadTask = new DatabaseDataSetTask(masterQuantChannelCallback);
+        loadTask.initLoadQuantitation(ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject(), dataSet);
+        AccessDatabaseThread.getAccessDatabaseThread().addTask(loadTask);
 
     }
 
     @Override
     public void updateEnabled(AbstractNode[] selectedNodes) {
-        
-         if (selectedNodes.length != 1) {
+
+        if (selectedNodes.length != 1) {
             setEnabled(false);
             return;
         }
@@ -181,13 +213,13 @@ public class ExportXICAction extends AbstractRSMAction {
         }
 
         DataSetNode datasetNode = (DataSetNode) node;
-        
+
         Dataset.DatasetType datasetType = ((DataSetData) datasetNode.getData()).getDatasetType();
         if (datasetType != Dataset.DatasetType.QUANTITATION) {
             setEnabled(false);
             return;
         }
-        
+
         DDataset d = ((DataSetData) datasetNode.getData()).getDataset();
         QuantitationMethod quantitationMethod = d.getQuantitationMethod();
         if (quantitationMethod == null) {
@@ -199,8 +231,8 @@ public class ExportXICAction extends AbstractRSMAction {
             setEnabled(false);
             return;
         }
-        
-        setEnabled(true);        
+
+        setEnabled(true);
     }
 
 }
--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/xic/ComputeQuantitationProfileAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/xic/ComputeQuantitationProfileAction.java
@@ -9,6 +9,7 @@ import fr.proline.core.orm.uds.dto.DDataset;
 import fr.proline.core.orm.uds.dto.DMasterQuantitationChannel;
 import fr.proline.studio.dam.AccessDatabaseThread;
 import fr.proline.studio.dam.DatabaseDataManager;
+import fr.proline.studio.dam.data.DataSetData;
 import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
 import fr.proline.studio.dam.tasks.DatabaseDataSetTask;
 import fr.proline.studio.dam.tasks.SubTask;
@@ -16,6 +17,7 @@ import fr.proline.studio.dpm.AccessServiceThread;
 import fr.proline.studio.dpm.task.AbstractServiceCallback;
 import fr.proline.studio.dpm.task.ComputeQuantProfileTask;
 import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.gui.OptionDialog;
 import fr.proline.studio.rsmexplorer.actions.identification.AbstractRSMAction;
 import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
 import fr.proline.studio.rsmexplorer.gui.dialog.xic.QuantProfileXICDialog;
@@ -53,48 +55,63 @@ public class ComputeQuantitationProfileAction extends AbstractRSMAction {
         }
         //project id
         final Long pID = ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject().getId();
-         // dialog with the parameters for quantitation profiler
-        QuantProfileXICDialog dialog = QuantProfileXICDialog.getDialog(WindowManager.getDefault().getMainWindow());
-        dialog.setLocation(x, y);
-        dialog.setVisible(true);
+        final int posx = x;
+        final int posy = y;
+        // only on one node
+        final AbstractNode node = selectedNodes[0];
+        final DataSetNode datasetNode = (DataSetNode) selectedNodes[0];
+        //dataset
+        final DDataset dataSet = datasetNode.getDataset();
+        
+        // call back for loading masterQuantChannel
+        AbstractDatabaseCallback masterQuantChannelCallback = new AbstractDatabaseCallback() {
 
-        // retreive parameters
-        if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
-
-            //User specified parameters
-
-            String errorMsg = null;
-
-            final Map<String, Object> quantParams = dialog.getQuantParams();
-            if (quantParams == null) {
-                errorMsg = "Null Quantitation parameters !";
+            @Override
+            public boolean mustBeCalledInAWT() {
+                return true;
             }
 
-            if (errorMsg != null) {
-                JOptionPane.showMessageDialog(dialog, errorMsg, "Warning", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            // only on one node
-            final AbstractNode node = selectedNodes[0];
-            node.setIsChanging(true);
-            QuantitationTree tree = QuantitationTree.getCurrentTree();
-            final DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
-            treeModel.nodeChanged(node);
-            final DataSetNode datasetNode = (DataSetNode) selectedNodes[0];
-            //dataset
-            DDataset dataSet = datasetNode.getDataset();
-            final String xicName = dataSet.getName() ;
-            // call back for loading masterQuantChannel
-            AbstractDatabaseCallback masterQuantChannelCallback = new AbstractDatabaseCallback() {
+            @Override
+            public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
+                if (success) {
+                    // check if profilizer has already been launched
+                    if (dataSet.getPostQuantProcessingConfig() != null) {
+                        String message = "Compute Quantitation Profile has already been launched.\nDo you want to relaunch it?";
+                        OptionDialog yesNoDialog = new OptionDialog(WindowManager.getDefault().getMainWindow(), "Compute Quant. Profile", message);
+                        yesNoDialog.setLocation(posx, posy);
+                        yesNoDialog.setVisible(true);
+                        if (yesNoDialog.getButtonClicked() != DefaultDialog.BUTTON_OK) {
+                            return;
+                        }
+                    }    
+                    
+                    // dialog with the parameters for quantitation profiler
+                    QuantProfileXICDialog dialog = QuantProfileXICDialog.getDialog(WindowManager.getDefault().getMainWindow());
+                    dialog.setLocation(posx, posy);
+                    dialog.setVisible(true);
+                    
+                    // retreive parameters
+                    if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+                        //User specified parameters
+                        String errorMsg = null;
 
-                @Override
-                public boolean mustBeCalledInAWT() {
-                    return true;
-                }
+                        final Map<String, Object> quantParams = dialog.getQuantParams();
+                        if (quantParams == null) {
+                            errorMsg = "Null Quantitation parameters !";
+                        }
 
-                @Override
-                public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
-                    if (success) {
+                        if (errorMsg != null) {
+                            JOptionPane.showMessageDialog(dialog, errorMsg, "Warning", JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                        // only on one node
+                        node.setIsChanging(true);
+                        QuantitationTree tree = QuantitationTree.getCurrentTree();
+                        final DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+                        treeModel.nodeChanged(node);
+                        //dataset
+                        final String xicName = dataSet.getName() ;
+                        
                         List<DMasterQuantitationChannel> listMasterQuantChannels = datasetNode.getDataset().getMasterQuantitationChannels();
                         if (listMasterQuantChannels != null && !listMasterQuantChannels.isEmpty()) {
                             Long masterQuantChannelId = new Long(listMasterQuantChannels.get(0).getId());
@@ -118,14 +135,15 @@ public class ComputeQuantitationProfileAction extends AbstractRSMAction {
                             ComputeQuantProfileTask task = new ComputeQuantProfileTask(xicCallback, pID, masterQuantChannelId, quantParams, xicName);
                             AccessServiceThread.getAccessServiceThread().addTask(task);
                         }
-                    }
+                    } //End OK entered   
                 }
-            };
-            DatabaseDataSetTask loadTask = new DatabaseDataSetTask(masterQuantChannelCallback);
-            loadTask.initLoadQuantitation(ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject(), dataSet);
-            AccessDatabaseThread.getAccessDatabaseThread().addTask(loadTask);
-
-        } //End OK entered         
+            }
+        };
+                
+        DatabaseDataSetTask loadTask = new DatabaseDataSetTask(masterQuantChannelCallback);
+        loadTask.initLoadQuantitation(ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject(), dataSet);
+        AccessDatabaseThread.getAccessDatabaseThread().addTask(loadTask);
+          
     }
 
     @Override

c993b6705e29f08e451d3b31c10cafd3c9e17132
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-01-23 13:22:39 +0000

    string added as y values

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/graphics/PlotType.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/graphics/PlotType.java
@@ -90,6 +90,7 @@ public enum PlotType {
                 acceptedValues.add(Double.class);
                 acceptedValues.add(Float.class);
                 acceptedValues.add(Integer.class);
+                acceptedValues.add(String.class);
                 break;
         }
         return acceptedValues;

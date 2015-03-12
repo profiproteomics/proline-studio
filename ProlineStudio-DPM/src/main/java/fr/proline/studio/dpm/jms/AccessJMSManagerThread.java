d471caf53f1b7587f88571ee3e400558908475ae
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-03-12 09:12:51 +0000

    Rename AccessJMSManager to AccessJMSManagerThread

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/jms/AccessJMSManagerThread.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/jms/AccessJMSManagerThread.java
@@ -1,31 +1,48 @@
-package fr.proline.studio.dpm;
+package fr.proline.studio.dpm.jms;
 
 import fr.proline.studio.dam.taskinfo.TaskInfoManager;
-import fr.proline.studio.dpm.task.AbstractServiceTask;
+import fr.proline.studio.dpm.task.jms.AbstractJMSTask;
+import fr.proline.studio.dpm.task.util.JMSConstants;
 import java.util.LinkedList;
+import javax.jms.*;
 import org.slf4j.LoggerFactory;
 
+
 /**
- * Thread in charge to ask asynchronously the execution of services to Web-Core
- * @author jm235353
+ * Manager used to add Tasks to the JMS Queue
+ * @author JM235353
  */
-public class AccessServiceThread extends Thread {
-    private static AccessServiceThread m_instance;
+public class AccessJMSManagerThread extends Thread {
     
-    private LinkedList<AbstractServiceTask> m_taskList = new LinkedList<>();
+    private static AccessJMSManagerThread m_instance;
+    private Connection m_connection;
+    private Session m_session;
+    private LinkedList<AbstractJMSTask> m_taskList = new LinkedList<>();
     
-    public static AccessServiceThread getAccessServiceThread() {
+    public static AccessJMSManagerThread getAccessJMSManagerThread() {
         if (m_instance == null) {
-            m_instance = new AccessServiceThread();
+            m_instance = new AccessJMSManagerThread();
             m_instance.start();
         }
         return m_instance;
     }
     
-    private AccessServiceThread() {
-        super("AccessServiceThread"); // useful for debugging
+    private AccessJMSManagerThread() {
+        super("AccessJMSManagerThread"); // useful for debugging
+        try {
+            // Get JMS Connection
+            m_connection = JMSConstants.getJMSConnection();
+            m_connection.start(); // Explicitely start connection to begin Consumer reception 
+            m_session = m_connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        } catch (JMSException e) {
+            LoggerFactory.getLogger("ProlineStudio.DPM").error("Unexpected exception when initializing JMS Connection", e);
+        }
     }
     
+    public Session getSession(){
+        return m_session;
+    }
+
     /**
      * Main loop of the thread
      */
@@ -33,7 +50,7 @@ public class AccessServiceThread extends Thread {
     public void run() {
         try {
             while (true) {
-                AbstractServiceTask task = null;
+                AbstractJMSTask task = null;
                 synchronized (this) {
 
                     while (true) {
@@ -49,48 +66,29 @@ public class AccessServiceThread extends Thread {
                 }
 
                 //Thread.sleep(500);
-
                 //System.out.println("Action : "+action.getClass().toString()+" "+System.currentTimeMillis());
 
                 // fetch data
-                boolean success = task.askService();
-
-                if (success) {
-                    // we check that the service is done
-                    if (task.isSynchronous()) {
-                        // call callback code at once
-                        task.callback(success);
-                    } else {
-                        // for an asynchronous task, we will do a polling
-                        // to check when it is finished
-                        ServiceStatusThread.getServiceStatusThread().addTask(task);
-                    }
-                    
-                } else {
-                    // call callback code
-                    task.callback(success);
-                }
-
+                task.askJMS(JMSConstants.getJMSConnection());
 
             }
 
 
-        } catch (Throwable t) {
+        } catch (InterruptedException | JMSException t) {
             LoggerFactory.getLogger("ProlineStudio.DPM").debug("Unexpected exception in main loop of AccessServiceThread", t);
             m_instance = null; // reset thread
         }
-
     }
     
-     /**
+    
+    /**
      * Add a task to be done
      *
      * @param task
      */
-    public final void addTask(AbstractServiceTask task) {
+    public final void addTask(AbstractJMSTask task) {
 
         TaskInfoManager.getTaskInfoManager().add(task.getTaskInfo());
-        
         // task is queued
         synchronized (this) {
             m_taskList.add(task);

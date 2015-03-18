f5f9f593332d015e58f166baa53799ff75ec8790
Christophe Bruley <christophe.bruley@cea.fr>
2015-03-18 09:34:42 +0000

    - Copy cell action added on each table

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/table/TablePopupMenu.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/table/TablePopupMenu.java
@@ -1,10 +1,17 @@
 package fr.proline.studio.table;
 
-
+import fr.proline.studio.export.ExportTextInterface;
+import java.awt.Component;
 import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
+import javax.swing.AbstractButton;
+import javax.swing.JLabel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
 /**
  *
@@ -12,73 +19,113 @@ import javax.swing.JTable;
  */
 public class TablePopupMenu extends JPopupMenu {
 
-    private ArrayList<AbstractTableAction> m_actions = new ArrayList<>();
-    
-    private int m_row;
-    private int m_col;
-    private int[] m_selectedRows;
-    private JTable m_table;
+   public static class CopyCellAction extends AbstractTableAction {
 
-    
-    public void addAction(AbstractTableAction action) {
-        m_actions.add(action);
-        if (action != null) {
-            action.setTablePopupMenu(this);
-        }
-    }
-    
-    public int getRow() {
-        return m_row;
-    }
-    
-    public int getCol() {
-        return m_col;
-    }
-    
-    public int[] getSelectedRows() {
-        return m_selectedRows;
-    }
-    
-    public JTable getTable() {
-        return m_table;
-    }
-    
-    public void preparePopup() {
+      public CopyCellAction() {
+         super("Copy cell");
+      }
 
-        int nbActions = m_actions.size();
-        for (int i = 0; i < nbActions; i++) {
-            AbstractTableAction action = m_actions.get(i);
-            if (action == null) {
-                addSeparator();
-            } else {
-                add(action.getPopupPresenter());
-            }
-        }
+      @Override
+      public void actionPerformed(int col, int row, int[] selectedRows, JTable table) {
+         TableCellRenderer renderer = table.getCellRenderer(row, col);
+         Component c = table.prepareRenderer(renderer, row, col);
+         String text = componentToText(c);
+         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+         clipboard.setContents(new StringSelection(text), null);
+      }
 
-    }
-    
-    public void show(int mouseX, int mouseY, JTable table) {
-        
-        Point mousePoint = new Point(mouseX, mouseY);
-        
-        m_table = table;
-        m_selectedRows = table.getSelectedRows();
-        m_row = table.rowAtPoint(mousePoint);
-        m_col = table.columnAtPoint(mousePoint);  
+   @Override
+   public void updateEnabled(int row, int col, int[] selectedRows, JTable table) {
 
-        // update actions
-        int nbActions = m_actions.size();
-        for (int i = 0; i < nbActions; i++) {
-            AbstractTableAction action = m_actions.get(i);
-            if (action != null) {
-                action.updateEnabled(m_row, m_col, m_selectedRows, table);
-            }
-        }
+   }
 
-        show(table, mouseX, mouseY);
+   private String componentToText(Component c) {
+      if (c instanceof ExportTextInterface) {
+         return ((ExportTextInterface) c).getExportText();
+      } else if (c instanceof JLabel) {
+         return ((JLabel) c).getText();
+      } else if (c instanceof AbstractButton) {
+         return ((AbstractButton) c).getText();
+      }
+      return "";
+   }
+}
 
-    }
-    
-    
-    
+   private ArrayList<AbstractTableAction> m_actions = new ArrayList<>();
+
+   private int m_row;
+   private int m_col;
+   private int[] m_selectedRows;
+   private JTable m_table;
+
+   public TablePopupMenu(boolean setDefaultActions) {
+      if (setDefaultActions) {
+         addAction(new CopyCellAction());
+         addAction(null);
+      }
+   }
+
+   public TablePopupMenu() {
+      this(true);
+   }
+
+   public void addAction(AbstractTableAction action) {
+      m_actions.add(action);
+      if (action != null) {
+         action.setTablePopupMenu(this);
+      }
+   }
+
+   public int getRow() {
+      return m_row;
+   }
+
+   public int getCol() {
+      return m_col;
+   }
+
+   public int[] getSelectedRows() {
+      return m_selectedRows;
+   }
+
+   public JTable getTable() {
+      return m_table;
+   }
+
+   public void preparePopup() {
+
+      int nbActions = m_actions.size();
+      for (int i = 0; i < nbActions; i++) {
+         AbstractTableAction action = m_actions.get(i);
+         if (action == null) {
+            addSeparator();
+         } else {
+            add(action.getPopupPresenter());
+         }
+      }
+
+   }
+
+   public void show(int mouseX, int mouseY, JTable table) {
+
+      Point mousePoint = new Point(mouseX, mouseY);
+
+      m_table = table;
+      m_selectedRows = table.getSelectedRows();
+      m_row = table.rowAtPoint(mousePoint);
+      m_col = table.columnAtPoint(mousePoint);
+
+      // update actions
+      int nbActions = m_actions.size();
+      for (int i = 0; i < nbActions; i++) {
+         AbstractTableAction action = m_actions.get(i);
+         if (action != null) {
+            action.updateEnabled(m_row, m_col, m_selectedRows, table);
+         }
+      }
+
+      show(table, mouseX, mouseY);
+
+   }
+
 }

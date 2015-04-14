619fc2eb52ca26f5dcda450324a8f7affd7609ba
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-04-14 09:10:25 +0000

    renamed interface

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/comparedata/GlobalTabelModelProviderInterface.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/comparedata/GlobalTabelModelProviderInterface.java
@@ -2,12 +2,14 @@ package fr.proline.studio.comparedata;
 
 import fr.proline.studio.graphics.CrossSelectionInterface;
 import fr.proline.studio.table.GlobalTableModelInterface;
+import org.jdesktop.swingx.JXTable;
 
 /**
  *
  * @author JM235353
  */
-public interface CompareDataProviderInterface {
-    public GlobalTableModelInterface getCompareDataInterface();
+public interface GlobalTabelModelProviderInterface {
+    public GlobalTableModelInterface getGlobalTableModelInterface();
+    public JXTable getGlobalAssociatedTable();
     public CrossSelectionInterface getCrossSelectionInterface();
 }

0ae40a609257b99ee9aa86dbe7e3fa85d8b6a756
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-05-05 13:49:06 +0000

    Appel des actions (WebCore ou JMS) en fonction de la définition ou nom du serveur JMS

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/AddAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/AddAction.java
@@ -1,9 +1,7 @@
 package fr.proline.studio.rsmexplorer.actions.identification;
 
 
-import fr.proline.core.orm.uds.Project;
-import fr.proline.studio.dam.DatabaseDataManager;
-import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
+import fr.proline.studio.dpm.task.util.JMSConnectionManager;
 import fr.proline.studio.rsmexplorer.tree.AbstractNode;
 import fr.proline.studio.rsmexplorer.tree.AbstractTree;
 import javax.swing.JMenu;
@@ -16,8 +14,7 @@ import org.openide.util.NbBundle;
  */
 public class AddAction extends AbstractRSMAction {
 
-    private ImportSearchResultAsDatasetAction m_identificationAction;
-    private ImportSearchResultAsDatasetJMSAction m_identificationJmsAction;
+    private AbstractRSMAction m_identificationAction; // Could be ImportSearchResultAsDatasetAction or ImportSearchResultAsDatasetJMSAction
     private AggregateAction m_aggregateAction;
     
     private JMenu m_menu;
@@ -28,18 +25,19 @@ public class AddAction extends AbstractRSMAction {
 
     @Override
     public JMenuItem getPopupPresenter() {
-        m_menu = new JMenu((String) getValue(NAME));
-        
-        m_identificationAction = new ImportSearchResultAsDatasetAction();
-        m_identificationJmsAction = new ImportSearchResultAsDatasetJMSAction();
+        m_menu = new JMenu((String) getValue(NAME));              
+    
         m_aggregateAction = new AggregateAction();
-        
+        if (JMSConnectionManager.getJMSConnectionManager().isJMSDefined()) {
+            m_identificationAction = new ImportSearchResultAsDatasetJMSAction();
+        } else {
+            m_identificationAction = new ImportSearchResultAsDatasetAction();
+        }
+
         JMenuItem identificationItem = new JMenuItem(m_identificationAction);
-        JMenuItem identificationJmsItem = new JMenuItem(m_identificationJmsAction);
         JMenuItem aggregateItem = new JMenuItem(m_aggregateAction);
         
         m_menu.add(identificationItem);
-        m_menu.add(identificationJmsItem);
         m_menu.add(aggregateItem);
 
         return m_menu;
@@ -48,13 +46,11 @@ public class AddAction extends AbstractRSMAction {
     @Override
     public void updateEnabled(AbstractNode[] selectedNodes) {
 
-        
         m_identificationAction.updateEnabled(selectedNodes);
-        m_identificationJmsAction.updateEnabled(selectedNodes);
         m_aggregateAction.updateEnabled(selectedNodes);
         
         
-        boolean isEnabled = m_identificationAction.isEnabled() || m_identificationJmsAction.isEnabled() || m_aggregateAction.isEnabled();
+        boolean isEnabled = m_identificationAction.isEnabled() ||  m_aggregateAction.isEnabled();
         setEnabled(isEnabled);
         m_menu.setEnabled(isEnabled);
     }

9b5a8e3828cf49019366f1bc32cbd579410f5ebe
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-05-11 06:59:43 +0000

    new class used to get the default renderers for table

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/table/TableDefaultRendererManager.java
@@ -0,0 +1,20 @@
+package fr.proline.studio.table;
+
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author JM235353
+ */
+public class TableDefaultRendererManager {
+    
+    private static JTable m_t = null;
+    
+    public static TableCellRenderer getDefaultRenderer(Class c) {
+        if (m_t == null) {
+            m_t = new JTable();
+        }
+        return m_t.getDefaultRenderer(c);
+    }
+}

1f9f635d2732cd572a3bbd04365abf182ff2d5b1
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-05-12 09:44:45 +0000

    adapted diff function to work as join function

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/functions/DiffFunction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/functions/DiffFunction.java
@@ -1,9 +1,14 @@
 package fr.proline.studio.rsmexplorer.gui.calc.functions;
 
+import fr.proline.studio.comparedata.AbstractJoinDataModel;
+import fr.proline.studio.parameter.ObjectParameter;
 import fr.proline.studio.parameter.ParameterError;
+import fr.proline.studio.parameter.ParameterList;
 import fr.proline.studio.python.data.Table;
 import fr.proline.studio.rsmexplorer.gui.calc.graph.AbstractGraphObject;
 import fr.proline.studio.rsmexplorer.gui.calc.graph.GraphNode;
+import fr.proline.studio.table.GlobalTableModelInterface;
+import javax.swing.JComboBox;
 
 /**
  *
@@ -11,6 +16,13 @@ import fr.proline.studio.rsmexplorer.gui.calc.graph.GraphNode;
  */
 public class DiffFunction extends AbstractFunction {
 
+    private static final String JOIN_COL1 = "JOIN_COL1";
+    private static final String JOIN_COL2 = "JOIN_COL2";
+
+    private ParameterList m_parameterList;
+    private ObjectParameter m_paramColumn1;
+    private ObjectParameter m_paramColumn2;
+    
     @Override
     public String getName() {
         return "Diff";
@@ -39,25 +51,103 @@ public class DiffFunction extends AbstractFunction {
         
         Table t1 = new Table(graphObjects[0].getGlobalTableModelInterface());
         Table t2 = new Table(graphObjects[1].getGlobalTableModelInterface());
-        Table diffTable = Table.diff(t1, t2);
+        
+        Table diffTable;
+        if ((m_paramColumn1 != null) && (m_paramColumn2 != null)) {
+            Integer key1 = (Integer) m_paramColumn1.getAssociatedObjectValue();
+            Integer key2 = (Integer) m_paramColumn2.getAssociatedObjectValue();
+            diffTable = Table.diff(t1, t2, key1, key2);
+        } else {
+            diffTable = Table.diff(t1, t2);
+        }
         m_globalTableModelInterface = diffTable.getModel();
         m_state = GraphNode.NodeState.READY;
     }
     
     @Override
     public void generateDefaultParameters(AbstractGraphObject[] graphObjects) {
-        //JPM.TODO
+
+        if (m_globalTableModelInterface == null) {
+            Table t1 = new Table(graphObjects[0].getGlobalTableModelInterface());
+            Table t2 = new Table(graphObjects[1].getGlobalTableModelInterface());
+            Table diffTable = Table.diff(t1, t2);
+            m_globalTableModelInterface = diffTable.getModel();
+        }
+        
+        GlobalTableModelInterface model1 = graphObjects[0].getGlobalTableModelInterface();
+        int nbColumns = model1.getColumnCount();
+        int nbColumnsKept = 0;
+        for (int i = 0; i < nbColumns; i++) {
+            Class c = model1.getDataColumnClass(i);
+            if (c.equals(String.class) || c.equals(Integer.class) || c.equals(Long.class)) {
+                nbColumnsKept++;
+            }
+        }
+        Object[] objectArray1 = new Object[nbColumnsKept];
+        Object[] associatedObjectArray1 = new Object[nbColumnsKept];
+        int iKept = 0;
+        for (int i = 0; i < nbColumns; i++) {
+            Class c = model1.getDataColumnClass(i);
+            if (c.equals(String.class) || c.equals(Integer.class) || c.equals(Long.class)) {
+                objectArray1[iKept] = model1.getColumnName(i);
+                associatedObjectArray1[iKept] = i;
+                iKept++;
+            }
+        }
+
+        GlobalTableModelInterface model2 = graphObjects[1].getGlobalTableModelInterface();
+        nbColumns = model2.getColumnCount();
+        nbColumnsKept = 0;
+        for (int i = 0; i < nbColumns; i++) {
+            Class c = model2.getDataColumnClass(i);
+            if (c.equals(String.class) || c.equals(Integer.class) || c.equals(Long.class)) {
+                nbColumnsKept++;
+            }
+        }
+        Object[] objectArray2 = new Object[nbColumnsKept];
+        Object[] associatedObjectArray2 = new Object[nbColumnsKept];
+        iKept = 0;
+        for (int i = 0; i < nbColumns; i++) {
+            Class c = model2.getDataColumnClass(i);
+            if (c.equals(String.class) || c.equals(Integer.class) || c.equals(Long.class)) {
+                objectArray2[iKept] = model2.getColumnName(i);
+                associatedObjectArray2[iKept] = i;
+                iKept++;
+            }
+        }
+        
+        
+        
+        m_paramColumn1 = new ObjectParameter(JOIN_COL1, graphObjects[0].getName() +" Join Column Key", new JComboBox(objectArray1), objectArray1, associatedObjectArray1, ((AbstractJoinDataModel)m_globalTableModelInterface).getSelectedKey1(), null);
+        m_paramColumn2 = new ObjectParameter(JOIN_COL2, graphObjects[1].getName() +" Join Column Key", new JComboBox(objectArray2), objectArray2, associatedObjectArray2, ((AbstractJoinDataModel)m_globalTableModelInterface).getSelectedKey2(), null);
+        m_parameterList = new ParameterList("Diff");
+        m_parameters = new ParameterList[1];
+        m_parameters[0] = m_parameterList;
+        
+        m_parameterList.add(m_paramColumn1);
+        m_parameterList.add(m_paramColumn2);
+        
     }
     
     @Override
     public ParameterError checkParameters() {
-        //JPM.TODO
-        return null;
+        Integer key1 = (Integer) m_paramColumn1.getAssociatedObjectValue();
+        Integer key2 = (Integer) m_paramColumn2.getAssociatedObjectValue();
+        boolean checkKeys = ((AbstractJoinDataModel)m_globalTableModelInterface).checkKeys(key1, key2);
+        
+        ParameterError error = null;
+        if (!checkKeys) {
+            error = new ParameterError("Selected Keys are not compatible", m_parameterList.getPanel() );
+        }
+        return error;
     }
     
     @Override
     public void userParametersChanged() {
-        //JPM.TODO
+        Integer key1 = (Integer) m_paramColumn1.getAssociatedObjectValue();
+        Integer key2 = (Integer) m_paramColumn2.getAssociatedObjectValue();
+        ((AbstractJoinDataModel)m_globalTableModelInterface).setKeys(key1, key2);
+        m_state = GraphNode.NodeState.UNSET;
     }
     
 }

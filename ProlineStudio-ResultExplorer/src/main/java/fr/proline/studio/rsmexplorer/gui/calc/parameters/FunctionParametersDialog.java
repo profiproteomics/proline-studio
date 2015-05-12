03cc666453e18c6d1978c81a55120977c487b572
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-05-12 09:43:56 +0000

    changed window title + added frame to inner generated panel

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/parameters/FunctionParametersDialog.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/parameters/FunctionParametersDialog.java
@@ -5,7 +5,10 @@ import fr.proline.studio.parameter.ParameterError;
 import fr.proline.studio.parameter.ParameterList;
 import fr.proline.studio.rsmexplorer.gui.calc.functions.AbstractFunction;
 import java.awt.Dialog;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Window;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 /**
@@ -17,13 +20,13 @@ public class FunctionParametersDialog extends DefaultDialog {
     private final ParameterList[] m_parameterList;
     private final AbstractFunction m_function;
     
-    public FunctionParametersDialog(Window parent, ParameterList[] parameterList, AbstractFunction function) {
+    public FunctionParametersDialog(String name, Window parent, ParameterList[] parameterList, AbstractFunction function) {
         super(parent, Dialog.ModalityType.APPLICATION_MODAL);
 
         m_parameterList = parameterList;
         m_function = function;
         
-        setTitle("Function Parameters");
+        setTitle(name+" Settings");
 
         setButtonVisible(BUTTON_HELP, false);
 
@@ -32,8 +35,27 @@ public class FunctionParametersDialog extends DefaultDialog {
     }
     
     public final JPanel createInternalPanel() {
+        
+        JPanel containerPanel = new JPanel(new GridBagLayout());
+        
+        JPanel framedPanel = new JPanel(new GridBagLayout());
+        framedPanel.setBorder(BorderFactory.createTitledBorder(""));
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
         ParameterList parameterList = m_parameterList[0];
-        return parameterList.getPanel();
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 0;
+        c.weighty = 0;
+        framedPanel.add(parameterList.getPanel(), c);
+        containerPanel.add(framedPanel, c);
+        
+        
+        return containerPanel;
     }
     
     @Override

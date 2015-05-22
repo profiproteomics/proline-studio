4184cdb94fdeb980abb342e283a4ebe9b2ba6ae0
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-05-22 11:44:06 +0000

    auto generated dialog for parameters

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/parameters/FunctionParametersDialog.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/calc/parameters/FunctionParametersDialog.java
@@ -4,6 +4,7 @@ import fr.proline.studio.gui.DefaultDialog;
 import fr.proline.studio.parameter.ParameterError;
 import fr.proline.studio.parameter.ParameterList;
 import fr.proline.studio.rsmexplorer.gui.calc.functions.AbstractFunction;
+import fr.proline.studio.utils.IconManager;
 import java.awt.Dialog;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -20,6 +21,8 @@ public class FunctionParametersDialog extends DefaultDialog {
     private final ParameterList[] m_parameterList;
     private final AbstractFunction m_function;
     
+    private int m_currentPanelNumber = 0;
+    
     public FunctionParametersDialog(String name, Window parent, ParameterList[] parameterList, AbstractFunction function) {
         super(parent, Dialog.ModalityType.APPLICATION_MODAL);
 
@@ -30,38 +33,56 @@ public class FunctionParametersDialog extends DefaultDialog {
 
         setButtonVisible(BUTTON_HELP, false);
 
-        setInternalComponent(createInternalPanel());
+        setInternalComponent(createInternalPanel(m_currentPanelNumber));
 
+        initOkButton(m_currentPanelNumber, m_parameterList.length);
     }
     
-    public final JPanel createInternalPanel() {
+    private void initOkButton(int panelNumber, int nbPanels) {
+        if (nbPanels == 1) {
+            // nothing to do
+            return;
+        }
+        
+        if (panelNumber < nbPanels - 1) {
+            // we are not on the last panel
+            setButtonName(DefaultDialog.BUTTON_OK, "Next");
+            setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.ARROW));
+        } else {
+            setButtonName(DefaultDialog.BUTTON_OK, "OK");
+            setButtonIcon(DefaultDialog.BUTTON_OK, IconManager.getIcon(IconManager.IconType.OK));
+        }
+        
+    }
+    
+    private JPanel createInternalPanel(int panelNumber) {
         
         JPanel containerPanel = new JPanel(new GridBagLayout());
-        
-        JPanel framedPanel = new JPanel(new GridBagLayout());
-        framedPanel.setBorder(BorderFactory.createTitledBorder(""));
+
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.NORTHWEST;
         c.fill = GridBagConstraints.BOTH;
         c.insets = new java.awt.Insets(5, 5, 5, 5);
 
-        ParameterList parameterList = m_parameterList[0];
+        ParameterList parameterList = m_parameterList[panelNumber];
         
         c.gridx = 0;
         c.gridy = 0;
-        c.weightx = 0;
-        c.weighty = 0;
-        framedPanel.add(parameterList.getPanel(), c);
-        containerPanel.add(framedPanel, c);
+        c.weightx = 1;
+        c.weighty = 1;
+        containerPanel.add(parameterList.getPanel(), c);
         
         
         return containerPanel;
     }
     
+    
     @Override
     protected boolean okCalled() {
         
-        ParameterError error = m_parameterList[0].checkParameters();
+
+        
+        ParameterError error = m_parameterList[m_currentPanelNumber].checkParameters();
         // report error
         if (error == null) {
             error = m_function.checkParameters();
@@ -74,8 +95,20 @@ public class FunctionParametersDialog extends DefaultDialog {
         }
         
         
+        if (m_currentPanelNumber == m_parameterList.length-1) {
+            // we were on the last panel : ok is called and all parameters are correctly set 
+            return true;
+        } else {
+            
+            m_currentPanelNumber++;
+            replaceInternaleComponent(createInternalPanel(m_currentPanelNumber));
+            initOkButton(m_currentPanelNumber,  m_parameterList.length);
+            revalidate();
+            repaint();
 
-        return true;
+            return false;
+        }
+        
     }
     
     

9acb62d2c49d64411507cb299f3f082f1bfaa8a3
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-05-22 11:47:27 +0000

    possibility to add vars to interpreter

--- ProlineStudio-R/src/main/java/fr/proline/studio/python/interpreter/CalcInterpreterTask.java
+++ ProlineStudio-R/src/main/java/fr/proline/studio/python/interpreter/CalcInterpreterTask.java
@@ -8,11 +8,17 @@ public class CalcInterpreterTask {
     
     private CalcCallback m_callback = null;
     private String m_code = null;
+    private ResultVariable[] m_parameters = null;
     
     public CalcInterpreterTask(String code, CalcCallback callback) {
+        this(code, null, callback);
+    }
+    
+    public CalcInterpreterTask(String code, ResultVariable[] parameters, CalcCallback callback) {
         m_code = code;
         m_callback = callback;
-    }
+        m_parameters = parameters;
+    } 
     
     public String getCode() {
         return m_code;
@@ -21,4 +27,10 @@ public class CalcInterpreterTask {
     public CalcCallback getCallback() {
         return m_callback;
     }
+    
+    public ResultVariable[] getParameters() {
+        return m_parameters;
+    }
+    
+    
 }

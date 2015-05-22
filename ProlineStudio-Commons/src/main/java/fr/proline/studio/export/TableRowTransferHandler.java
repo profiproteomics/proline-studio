f5140cdc30b52cfe03c2f0ef568f6e7451618866
Alexandre Walter <alexandre.walter@unistra.fr>
2015-05-22 14:03:08 +0000

    allows reordering of elements in custom param file table

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/export/TableRowTransferHandler.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/TableRowTransferHandler.java
@@ -4,73 +4,88 @@ import java.awt.Cursor;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.dnd.DragSource;
+import java.util.ArrayList;
+import java.util.Vector;
 
-import javax.activation.ActivationDataFlavor;
 import javax.activation.DataHandler;
 import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.TransferHandler;
-import javax.swing.table.DefaultTableModel;
 
 public class TableRowTransferHandler extends TransferHandler {
-	   //private final DataFlavor localObjectFlavor = new ActivationDataFlavor(Integer.class, DataFlavor.javaJVMLocalObjectMimeType, "Integer Row Index");
-	   
-	   private final DataFlavor localObjectFlavor = new DataFlavor(Integer.class, "Integer Row Index");
-	   
-	   
-	   private JTable        table             = null;
 
-	   public TableRowTransferHandler(JTable table) {
-	      this.table = table;
-	   }
+    private final DataFlavor localObjectFlavor = new DataFlavor(Integer.class, "Integer Row Index");
+    private JTable table = null;
 
-	   @Override
-	   protected Transferable createTransferable(JComponent c) {
-	      assert (c == table);
-	      return new DataHandler(new Integer(table.getSelectedRow()), localObjectFlavor.getMimeType());
-	   }
+    public TableRowTransferHandler(JTable table) {
+        this.table = table;
+    }
 
-	   @Override
-	   public boolean canImport(TransferHandler.TransferSupport info) {
-	      boolean b = info.getComponent() == table && info.isDrop() && info.isDataFlavorSupported(localObjectFlavor);
-	      table.setCursor(b ? DragSource.DefaultMoveDrop : DragSource.DefaultMoveNoDrop);
-	      return b;
-	   }
+    @Override
+    protected Transferable createTransferable(JComponent c) {
+        assert (c == table);
+        return new DataHandler(new Integer(table.getSelectedRow()), localObjectFlavor.getMimeType());
+    }
 
-	   @Override
-	   public int getSourceActions(JComponent c) {
-	      return TransferHandler.COPY_OR_MOVE;
-	   }
+    @Override
+    public boolean canImport(TransferHandler.TransferSupport info) {
+        boolean b = info.getComponent() == table && info.isDrop() && info.isDataFlavorSupported(localObjectFlavor);
+        table.setCursor(b ? DragSource.DefaultMoveDrop : DragSource.DefaultMoveNoDrop);
+        return b;
+    }
 
-	   @Override
-	   public boolean importData(TransferHandler.TransferSupport info) {
-	      JTable target = (JTable) info.getComponent();
-	      JTable.DropLocation dl = (JTable.DropLocation) info.getDropLocation();
-	      int index = dl.getRow();
-	      int max = table.getModel().getRowCount();
-	      if (index < 0 || index > max)
-	         index = max;
-	      target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-	      try {
-	         Integer rowFrom = (Integer) info.getTransferable().getTransferData(localObjectFlavor);
-	         if (rowFrom != -1 && rowFrom != index) {
-	            ( (Reorderable) table.getModel()).reorder(rowFrom, index);
-	            if (index > rowFrom)
-	               index--;
-	            target.getSelectionModel().addSelectionInterval(index, index);
-	            return true;
-	         }
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-	      return false;
-	   }
+    @Override
+    public int getSourceActions(JComponent c) {
+        return TransferHandler.COPY_OR_MOVE;
+    }
 
-	   @Override
-	   protected void exportDone(JComponent c, Transferable t, int act) {
-	      if (act == TransferHandler.MOVE) {
-	         table.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-	      }
-	   }
+    @Override
+    public boolean importData(TransferHandler.TransferSupport info) {
+        JTable target = (JTable) info.getComponent();
+        JTable.DropLocation dl = (JTable.DropLocation) info.getDropLocation();
+        int index = dl.getRow();
+        int max = table.getModel().getRowCount();
+        if (index < 0 || index > max) {
+            index = max;
+        }
+        target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 
-	}
+        try {
+            Integer rowFrom = (Integer) info.getTransferable().getTransferData(localObjectFlavor);
+            if (rowFrom != -1 && rowFrom != index) {
+
+                int[] rows = table.getSelectedRows();
+                int dist = 0;
+                for (int row : rows) {
+                    if (index > row) {
+                        dist++;
+                    }
+                }
+                index -= dist;
+
+                ArrayList<Vector> list = TableUtil.getSelectedList(table);
+                TableUtil.removeSelected(table);
+                ArrayList<Integer> sels = new ArrayList<Integer>();
+                for (Vector obj : list) {
+                    sels.add(index);
+                    TableUtil.addRowAt(table, obj, index++);
+                }
+                TableUtil.selectMultipleRow(table, sels);
+
+
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    protected void exportDone(JComponent c, Transferable t, int act) {
+        if (act == TransferHandler.MOVE) {
+            table.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        }
+    }
+}
+

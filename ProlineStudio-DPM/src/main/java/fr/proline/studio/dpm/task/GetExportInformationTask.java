7e8c1ba7ff1d355fddb8811c3765bef8fccb33e0
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-06-10 14:09:33 +0000

    getExportInformation changed (request name and synchrone service)

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/GetExportInformationTask.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/GetExportInformationTask.java
@@ -13,8 +13,6 @@ import fr.proline.core.orm.uds.dto.DDataset;
 import fr.proline.studio.dam.taskinfo.TaskError;
 import fr.proline.studio.dam.taskinfo.TaskInfo;
 import static fr.proline.studio.dpm.task.AbstractServiceTask.TASK_LIST_INFO;
-import static fr.proline.studio.dpm.task.AbstractServiceTask.m_idIncrement;
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +22,7 @@ import java.util.Map;
  */
 public class GetExportInformationTask extends AbstractServiceTask {
 
-    private static String m_request = "dps.uds/get_export_information/";
+    private static String m_request = "das.uds/get_export_information/";
 
     private DDataset m_dataset;
     private String m_mode;
@@ -52,14 +50,12 @@ public class GetExportInformationTask extends AbstractServiceTask {
 
     @Override
     public boolean askService() {
-
+        
         try {
             // create the request
             JsonRpcRequest request = new JsonRpcRequest();
-
             request.setId(m_id);
-            request.setMethod("run_job");
-
+            request.setMethod("register");
             Map<String, Object> params = new HashMap<>();
             Map<String, Object> extraParams = new HashMap<>();
             if (m_dataset != null) {
@@ -69,22 +65,22 @@ public class GetExportInformationTask extends AbstractServiceTask {
                 extraParams.put("export_mode", m_mode);
             }
             params.put("extra_params", extraParams);
-
             request.setParameters(params);
 
-            HttpResponse response = postRequest(m_request + request.getMethod() + getIdString(), request);
-
+            HttpResponse response = postRequest(m_request+request.getMethod()+getIdString(), request);
+            
             GenericJson jsonResult = response.parseAs(GenericJson.class);
 
             ArrayMap errorMap = (ArrayMap) jsonResult.get("error");
-
+            
+            m_taskError = null;
             if (errorMap != null) {
                 String message = (String) errorMap.get("message");
-
+                
                 if (message != null) {
                     m_taskError = new TaskError(message);
                 }
-
+                
                 String data = (String) errorMap.get("data");
                 if (data != null) {
                     if (m_taskError == null) {
@@ -93,127 +89,37 @@ public class GetExportInformationTask extends AbstractServiceTask {
                         m_taskError.setErrorText(data);
                     }
                 }
+                
+                
+                 //JPM.WART : Web core returns an error and the project id !!!
+                m_loggerWebcore.error(getClass().getSimpleName()+m_taskError.toString());
+               // return false; 
+            }
 
-                if (m_taskError != null) {
-                    m_loggerWebcore.error(getClass().getSimpleName() + " failed : " + m_taskError.toString());
-                }
-
+            // retrieve configuration 
+            String configStr = (String) jsonResult.get("result");
+            if (configStr == null || configStr.isEmpty()) {
+                m_loggerProline.error(getClass().getSimpleName() + " failed : No configuration returned.");
                 return false;
             }
+            m_customizableExport.add(configStr);
 
-            BigDecimal jobId = (BigDecimal) jsonResult.get("result");
-            if (jobId != null) {
-                m_id = jobId.intValue();
-            } else {
-                m_loggerProline.error(getClass().getSimpleName() + " failed : id not defined");
-            }
-
+            
+            
         } catch (Exception e) {
             m_taskError = new TaskError(e);
-            m_loggerProline.error(getClass().getSimpleName() + " failed", e);
+            m_loggerProline.error(getClass().getSimpleName()+" failed", e);
             return false;
         }
-
+        
         return true;
     }
+    
 
     @Override
     public ServiceState getServiceState() {
-
-        try {
-
-            // create the request
-            JsonRpcRequest request = new JsonRpcRequest();
-
-            request.setId(m_idIncrement++);
-            request.setMethod("get_job_status");
-
-            Map<String, Object> params = new HashMap<>();
-            params.put("job_id", m_id);
-
-            request.setParameters(params);
-
-            HttpResponse response = postRequest(m_request + request.getMethod() + getIdString(), request);
-
-            GenericJson jsonResult = response.parseAs(GenericJson.class);
-
-            ArrayMap errorMap = (ArrayMap) jsonResult.get("error");
-
-            if (errorMap != null) {
-
-                String message = (String) errorMap.get("message");
-
-                if (message != null) {
-                    m_taskError = new TaskError(message);
-                }
-
-                String data = (String) errorMap.get("data");
-                if (data != null) {
-                    if (m_taskError == null) {
-                        m_taskError = new TaskError(data);
-                    } else {
-                        m_taskError.setErrorText(data);
-                    }
-                }
-
-                if (m_taskError != null) {
-                    m_loggerWebcore.error(getClass().getSimpleName() + " failed : " + m_taskError.toString());
-                }
-
-                return ServiceState.STATE_FAILED; // should not happen !
-            }
-
-            ArrayMap resultMap = (ArrayMap) jsonResult.get("result");
-
-            if (resultMap != null) {
-                Boolean success = (Boolean) resultMap.get("success");  //JPM.TODO : get ResultSummary created
-
-                if (success == null) {
-
-                    String message = (String) resultMap.get("message");
-                    if ((message != null) && message.startsWith("Running")) {
-                        getTaskInfo().setRunning(false);
-                    }
-
-                    return ServiceState.STATE_WAITING;
-                }
-
-                if (success) {
-
-                    BigDecimal duration = (BigDecimal) resultMap.get("duration");
-                    if (duration != null) {
-                        getTaskInfo().setDuration(duration.longValue());
-                    }
-
-                    // retrieve configuration 
-                    String configStr = (String) resultMap.get("result");
-                    if (configStr == null || configStr.isEmpty()) {
-                        m_loggerProline.error(getClass().getSimpleName() + " failed : No configuration returned.");
-                        return ServiceState.STATE_FAILED;
-                    }
-                    m_customizableExport.add(configStr);
-
-                    return ServiceState.STATE_DONE;
-                } else {
-                    String errorMessage = (String) resultMap.get("message");
-
-                    if (errorMessage != null) {
-                        m_taskError = new TaskError(errorMessage);
-                        m_loggerWebcore.error(getClass().getSimpleName() + " failed : " + errorMessage);
-                    }
-
-                    return ServiceState.STATE_FAILED;
-                }
-
-            }
-
-        } catch (Exception e) {
-            m_taskError = new TaskError(e);
-            m_loggerProline.error(getClass().getSimpleName() + " failed", e);
-            return ServiceState.STATE_FAILED; // should not happen !
-        }
-
-        return ServiceState.STATE_WAITING;
-
+        // always returns STATE_DONE because to create a project
+        // is a synchronous service
+        return ServiceState.STATE_DONE;
     }
 }

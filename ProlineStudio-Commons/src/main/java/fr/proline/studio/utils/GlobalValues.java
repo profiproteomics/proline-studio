84c0e262d5567fc43a41a509693afdfbdeffe581
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-06-30 14:36:12 +0000

    version fixed for release

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/utils/GlobalValues.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/utils/GlobalValues.java
@@ -11,7 +11,7 @@ public class GlobalValues {
     public final static String HTML_COLOR_VIOLET = "#ce85fd;";
     
     /*if true the help text will be redirected to proline.profiproteomics.fr , otherwise on biodev*/
-    public final static  Boolean PUBLIC_RELEASE_VERSION = false;
+    public final static  Boolean PUBLIC_RELEASE_VERSION = true;
     
 
     

de6a3d86015351e39f395814db18925f71d01d18
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-07-27 13:20:56 +0000

    Go to SNAPSHOT for eventual further fix version

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/Installer.java
@@ -23,8 +23,8 @@ public class Installer extends VersionInstaller {
     @Override
     public void restored() {
 
-        //String buildnumber = "1.0.1  alpha ("+moduleBuildDate+")"; //specify if alpha (or nothing = release)
-        String buildnumber = "1.1.1";
+        String buildnumber = "1.1.2  alpha ("+moduleBuildDate+")"; //specify if alpha (or nothing = release)
+//        String buildnumber = "1.1.1";
         
         // set the proline version for the application title
         System.setProperty("netbeans.buildnumber", buildnumber);  //"1.0.1 (alpha : build date @build.date@)"

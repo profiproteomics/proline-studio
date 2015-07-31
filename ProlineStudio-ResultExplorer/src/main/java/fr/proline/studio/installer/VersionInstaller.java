3fe7b69cdc57325c5cae7b73b1e075d28804340b
Veronique Dupierris <veronique.dupierris@cea.fr>
2015-07-31 09:41:39 +0000

    En route pour la 1.2

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/installer/VersionInstaller.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.installer;
+
+import org.openide.modules.ModuleInstall;
+
+//Do not commit java file (only template!)
+/**
+ * Class generated from template. Do not edit !
+ *
+ * @author VD225637
+ */
+public class VersionInstaller extends ModuleInstall{
+    
+    public final String moduleVersion = "0.5.0-SNAPSHOT";
+    public final String moduleBuildDate = "20150731-1033_VD225637";
+}

a57709096fbbaea7f6b7b07273e68d1f3870555c
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-08-10 10:33:47 +0000

    - label marker without attachment line - possibility to move this new sort of label marker

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/graphics/marker/LabelMarker.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/graphics/marker/LabelMarker.java
@@ -1,8 +1,7 @@
 package fr.proline.studio.graphics.marker;
 
 import fr.proline.studio.graphics.BasePlotPanel;
-import fr.proline.studio.graphics.XAxis;
-import fr.proline.studio.graphics.YAxis;
+import fr.proline.studio.graphics.MoveableInterface;
 import java.awt.*;
 
 /**
@@ -10,41 +9,44 @@ import java.awt.*;
  *
  * @author JM235353
  */
-public class LabelMarker extends AbstractMarker {
+public class LabelMarker extends AbstractMarker implements MoveableInterface {
 
     public static final int ORIENTATION_X_LEFT = 0;
     public static final int ORIENTATION_X_RIGHT = 1;
-    public static final int ORIENTATION_Y_TOP = 2;
-    public static final int ORIENTATION_Y_BOTTOM = 3;
+    public static final int ORIENTATION_XY_MIDDLE = 2;
+    public static final int ORIENTATION_Y_TOP = 3;
+    public static final int ORIENTATION_Y_BOTTOM = 4;
     private static final Font MARKER_FONT = new Font("Arial", Font.PLAIN, 12);
     private String m_valueLabel = null;
-    private double m_x;
-    private double m_y;
-    private int m_orientationX;
-    private int m_orientationY;
+    private final AbstractCoordinates m_coordinates;
+    private final int m_orientationX;
+    private final int m_orientationY;
     private static final int LINE_DELTA = 18;
     private final static int COLOR_WIDTH = 5;
     private final static int DELTA_COLOR = 4;
 
+    private int m_xBox;
+    private int m_yBox;
+    private int m_heightBox;
+    private int m_widthBox;
+    
     // show a color point in the marker
     private Color m_labelColor = null;
 
-    public LabelMarker(BasePlotPanel plotPanel, double x, double y, String valueLabel, int orientationX, int orientationY) {
+    public LabelMarker(BasePlotPanel plotPanel, AbstractCoordinates coordinates, String valueLabel, int orientationX, int orientationY) {
         super(plotPanel);
 
         m_valueLabel = valueLabel;
-        m_x = x;
-        m_y = y;
+        m_coordinates = coordinates;
         m_orientationX = orientationX;
         m_orientationY = orientationY;
     }
 
-    public LabelMarker(BasePlotPanel plotPanel, double x, double y, String valueLabel, int orientationX, int orientationY, Color labelColor) {
+     public LabelMarker(BasePlotPanel plotPanel, AbstractCoordinates coordinates, String valueLabel, int orientationX, int orientationY, Color labelColor) {
         super(plotPanel);
 
         m_valueLabel = valueLabel;
-        m_x = x;
-        m_y = y;
+        m_coordinates = coordinates;
         m_orientationX = orientationX;
         m_orientationY = orientationY;
         this.m_labelColor = labelColor;
@@ -57,14 +59,36 @@ public class LabelMarker extends AbstractMarker {
     @Override
     public void paint(Graphics2D g) {
 
-        XAxis xAxis = m_plotPanel.getXAxis();
-        YAxis yAxis = m_plotPanel.getYAxis();
+        int pixelX = m_coordinates.getPixelX(m_plotPanel);
+        int pixelY = m_coordinates.getPixelY(m_plotPanel);
 
-        int pixelX = xAxis.valueToPixel(m_x);
-        int pixelY = yAxis.valueToPixel(m_y);
-
-        int deltaX = (m_orientationX == ORIENTATION_X_LEFT) ? -LINE_DELTA : (m_orientationX == ORIENTATION_X_RIGHT) ? LINE_DELTA : 0;
-        int deltaY = (m_orientationY == ORIENTATION_Y_TOP) ? -LINE_DELTA : (m_orientationY == ORIENTATION_Y_BOTTOM) ? LINE_DELTA : 0;
+        int deltaX;
+        switch (m_orientationX) {
+            case ORIENTATION_X_LEFT:
+                deltaX = -LINE_DELTA;
+                break;
+            case ORIENTATION_X_RIGHT:
+                deltaX = LINE_DELTA;
+                break;
+            case ORIENTATION_XY_MIDDLE:
+            default:
+                deltaX = 0;
+                break;
+        }
+        
+        int deltaY;
+        switch (m_orientationY) {
+            case ORIENTATION_Y_TOP:
+                deltaY = -LINE_DELTA;
+                break;
+            case ORIENTATION_Y_BOTTOM:
+                deltaY = LINE_DELTA;
+                break;
+            case ORIENTATION_XY_MIDDLE:
+            default:
+                deltaY = 0;
+                break;
+        }
 
         final int DELTA = 3;
         
@@ -81,74 +105,61 @@ public class LabelMarker extends AbstractMarker {
         g.drawLine(pixelX, pixelY, pixelX + deltaX, pixelY + deltaY);
 
 
-        int deltaC = 0;
+        int delta = 0;
 
         if (m_orientationX == ORIENTATION_X_RIGHT) {
-            int xBox = pixelX + deltaX;
-            int yBox = pixelY + deltaY - stringHeight / 2 - DELTA;
-            int heightBox = stringHeight + DELTA * 2;
-            int widthBox = stringWidth ;
+            m_xBox = pixelX + deltaX;
+            m_yBox = pixelY + deltaY - stringHeight / 2 - DELTA;
+            m_heightBox = stringHeight + DELTA * 2;
+            m_widthBox = stringWidth ;
             g.setColor(new Color(255, 255, 255, 196));
-            g.fillRect(xBox, yBox, widthBox, heightBox);
+            g.fillRect(m_xBox, m_yBox, m_widthBox, m_heightBox);
             g.setColor(Color.black);
-            g.drawRect(xBox, yBox, widthBox, heightBox);
+            g.drawRect(m_xBox, m_yBox, m_widthBox, m_heightBox);
             if (isPaintColor()) {
                 g.setColor(m_labelColor);
-                g.fillRect(xBox + DELTA_COLOR, yBox+((heightBox-COLOR_WIDTH) /2) , COLOR_WIDTH, COLOR_WIDTH);
-                deltaC = DELTA_COLOR;
+                g.fillRect(m_xBox + DELTA_COLOR, m_yBox+((m_heightBox-COLOR_WIDTH) /2) , COLOR_WIDTH, COLOR_WIDTH);
+                delta = 2 * DELTA_COLOR + COLOR_WIDTH;
                 g.setColor(Color.black);
+            } else {
+                delta = DELTA;
             }
-            g.drawString(m_valueLabel, xBox + DELTA + deltaC, yBox + metrics.getAscent());
-        } else if (m_orientationX == ORIENTATION_X_LEFT) {
-            int xBox = pixelX + deltaX - stringWidth ;
-            int yBox = pixelY + deltaY - stringHeight / 2 - DELTA;
-            int heightBox = stringHeight + DELTA * 2;
-            int widthBox = stringWidth  ;
+            g.drawString(m_valueLabel, m_xBox + delta, m_yBox + metrics.getAscent());
+        } else if ((m_orientationX == ORIENTATION_X_LEFT) || (m_orientationX == ORIENTATION_XY_MIDDLE)) {
+            m_xBox = pixelX + deltaX - stringWidth ;
+            m_yBox = pixelY + deltaY - stringHeight / 2 - DELTA;
+            m_heightBox = stringHeight + DELTA * 2;
+            m_widthBox = stringWidth  ;
             g.setColor(new Color(255, 255, 255, 128));
-            g.fillRect(xBox, yBox, widthBox, heightBox);
+            g.fillRect(m_xBox, m_yBox, m_widthBox, m_heightBox);
             g.setColor(Color.black);
-            g.drawRect(xBox, yBox, widthBox, heightBox);
+            g.drawRect(m_xBox, m_yBox, m_widthBox, m_heightBox);
             if (isPaintColor()) {
                 g.setColor(m_labelColor);
-                g.fillRect(xBox + DELTA_COLOR,  yBox+((heightBox-COLOR_WIDTH) /2), COLOR_WIDTH, COLOR_WIDTH);
-                deltaC = DELTA_COLOR;
+                g.fillRect(m_xBox + DELTA_COLOR,  m_yBox+((m_heightBox-COLOR_WIDTH) /2), COLOR_WIDTH, COLOR_WIDTH);
+                delta = 2 * DELTA_COLOR + COLOR_WIDTH;
                 g.setColor(Color.black);
+            } else {
+                delta = DELTA;
             }
-            g.drawString(m_valueLabel, xBox + DELTA + deltaC, yBox + metrics.getAscent());
-        } else if (m_orientationY == ORIENTATION_Y_TOP) {
-            int xBox = pixelX + deltaX - stringWidth / 2 - DELTA;
-            int yBox = pixelY + deltaY - stringHeight - DELTA * 2;
-            int heightBox = stringHeight + DELTA * 2;
-            int widthBox = stringWidth  ;
-            g.setColor(new Color(255, 255, 255, 128));
-            g.fillRect(xBox, yBox, widthBox, heightBox);
-            g.setColor(Color.black);
-            g.drawRect(xBox, yBox, widthBox, heightBox);
-            if (isPaintColor()) {
-                g.setColor(m_labelColor);
-                g.fillRect(xBox + DELTA_COLOR,  yBox+((heightBox-COLOR_WIDTH) /2), COLOR_WIDTH, COLOR_WIDTH);
-                deltaC = DELTA_COLOR;
-                g.setColor(Color.black);
-            }
-            g.drawString(m_valueLabel, xBox + DELTA + deltaC, yBox + metrics.getAscent());
-        } else if (m_orientationY == ORIENTATION_Y_BOTTOM) {
-            int xBox = pixelX + deltaX - stringWidth / 2 - DELTA;
-            int yBox = pixelY + deltaY;
-            int heightBox = stringHeight + DELTA * 2;
-            int widthBox = stringWidth ;
-            g.setColor(new Color(255, 255, 255, 128));
-            g.fillRect(xBox, yBox, widthBox, heightBox);
-            g.setColor(Color.black);
-            g.drawRect(xBox, yBox, widthBox, heightBox);
-            if (isPaintColor()) {
-                g.setColor(m_labelColor);
-                g.fillRect(xBox + DELTA_COLOR,  yBox+((heightBox-COLOR_WIDTH) /2), COLOR_WIDTH, COLOR_WIDTH);
-                deltaC = DELTA_COLOR;
-                g.setColor(Color.black);
-            }
-            g.drawString(m_valueLabel, xBox + DELTA + deltaC, yBox + metrics.getAscent());
+            g.drawString(m_valueLabel, m_xBox + delta, m_yBox + metrics.getAscent());
         }
 
+    }
+
+    @Override
+    public boolean inside(int x, int y) {
+        return (x>=m_xBox) && (x<=m_xBox+m_widthBox) && (y>=m_yBox) && (y<=m_yBox+m_heightBox);
+    }
+
+    @Override
+    public void move(int deltaX, int deltaY) {
+        m_coordinates.setPixelPosition(m_plotPanel, m_coordinates.getPixelX(m_plotPanel)+deltaX, m_coordinates.getPixelY(m_plotPanel)+deltaY);
+    }
+
+    @Override
+    public boolean isMoveable() {
+        return (m_orientationX == ORIENTATION_XY_MIDDLE);
     }
 
 }

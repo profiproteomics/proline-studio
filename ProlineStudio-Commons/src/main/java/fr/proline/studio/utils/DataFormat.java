de91eb8a5def63a2704d14a7e99f99bf87831b62
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-08-25 14:24:31 +0000

    scientific notation support

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/utils/DataFormat.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/utils/DataFormat.java
@@ -12,11 +12,16 @@ import java.text.DecimalFormatSymbols;
 public class DataFormat {
     
     private final static DecimalFormat m_decimalFormat = new DecimalFormat("###.##");
+    private final static DecimalFormat m_scientificFormat = new DecimalFormat("#.##E0");
     
     static {
         DecimalFormatSymbols dfs = m_decimalFormat.getDecimalFormatSymbols();
         dfs.setDecimalSeparator('.');
         m_decimalFormat.setDecimalFormatSymbols(dfs);
+        
+        dfs = m_scientificFormat.getDecimalFormatSymbols();
+        dfs.setDecimalSeparator('.');
+        m_scientificFormat.setDecimalFormatSymbols(dfs);
     }
     
     
@@ -36,7 +41,7 @@ public class DataFormat {
         return m_decimalFormat.format((double) f);
     }
     
-        public static String format(Double d, int nbFractionDigits) {
+    public static String format(Double d, int nbFractionDigits) {
         if (d == null) {
             return null;
         }
@@ -49,6 +54,36 @@ public class DataFormat {
         m_decimalFormat.setMinimumFractionDigits(nbFractionDigits);
         
         return m_decimalFormat.format((double) d);
+    }
+    
+    public static String formatScientific(Float f, int nbFractionDigits) {
+        if (f == null) {
+            return null;
+        }
+        return formatScientific(f.floatValue(), nbFractionDigits);
+    }
+    
+    public static String formatScientific(float f, int nbFractionDigits) {
+        
+        m_scientificFormat.setMaximumFractionDigits(nbFractionDigits);
+        m_scientificFormat.setMinimumFractionDigits(nbFractionDigits);
+        
+        return m_scientificFormat.format((double) f);
+    }
+    
+    public static String formatScientific(Double d, int nbFractionDigits) {
+        if (d == null) {
+            return null;
+        }
+        return formatScientific(d.doubleValue(), nbFractionDigits);
+    }
+    
+    public static String formatScientific(double d, int nbFractionDigits) {
+
+        m_scientificFormat.setMaximumFractionDigits(nbFractionDigits);
+        m_scientificFormat.setMinimumFractionDigits(nbFractionDigits);
+        
+        return m_scientificFormat.format((double) d);
     }
     
     public static String format(Integer i) {

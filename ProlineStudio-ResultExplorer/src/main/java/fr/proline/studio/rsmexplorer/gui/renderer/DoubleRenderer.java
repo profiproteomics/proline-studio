e03cbe63828090a53988d976e80237ccca4b09cc
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-08-25 14:22:21 +0000

    added scientific notation for floatrenderer and doublerenderer

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/DoubleRenderer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/DoubleRenderer.java
@@ -11,8 +11,17 @@ import javax.swing.table.TableCellRenderer;
  */
 public class DoubleRenderer implements TableCellRenderer  {
     
-    private TableCellRenderer m_defaultRenderer;
+    private final TableCellRenderer m_defaultRenderer;
     private int m_nbDigits = 2;
+    private boolean m_scientific = false;
+    private boolean m_showNaN = false;
+    
+    public DoubleRenderer(TableCellRenderer defaultRenderer, int digits, boolean scientific, boolean showNaN) {
+        m_defaultRenderer = defaultRenderer;
+        m_nbDigits = digits;
+        m_scientific = scientific;
+        m_showNaN = showNaN;
+    }
     
     public DoubleRenderer(TableCellRenderer defaultRenderer) {
         m_defaultRenderer = defaultRenderer;
@@ -29,9 +38,28 @@ public class DoubleRenderer implements TableCellRenderer  {
 
 
         Double d = (Double) value;
-        String formatedValue = (d != null) ? DataFormat.format(d.doubleValue(), m_nbDigits) : "";
-
-        
+        String formatedValue;
+        if ((d == null) || (d.isNaN())) {
+            if (m_showNaN) {
+                formatedValue = "NaN";
+            } else {
+                formatedValue = "";
+            }
+        } else if (m_scientific) {
+            double dAbs = Math.abs(d.doubleValue());
+ 
+            if ((dAbs!=0) && (dAbs*Math.pow(10, m_nbDigits-1)>=1)) {
+                formatedValue = DataFormat.format(d.doubleValue(), m_nbDigits);
+            } else {
+                int digits = m_nbDigits-2;
+                if (digits<2) {
+                    digits = 2;
+                }
+                formatedValue = DataFormat.formatScientific(d.doubleValue(), digits);
+            }
+        } else {
+            formatedValue = DataFormat.format(d.doubleValue(), m_nbDigits);
+        }
 
         return m_defaultRenderer.getTableCellRendererComponent(table, formatedValue, isSelected, hasFocus, row, column);
 
--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/FloatRenderer.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/FloatRenderer.java
@@ -14,6 +14,16 @@ public class FloatRenderer implements TableCellRenderer {
     private final TableCellRenderer m_defaultRenderer;
     
     private int m_digits = 2;
+    private boolean m_scientific = false;
+    private boolean m_showNaN = false;
+    
+    
+    public FloatRenderer(TableCellRenderer defaultRenderer, int digits, boolean scientific, boolean showNaN) {
+        m_defaultRenderer = defaultRenderer;
+        m_digits = digits;
+        m_scientific = scientific;
+        m_showNaN = showNaN;
+    }
     
     public FloatRenderer(TableCellRenderer defaultRenderer, int digits) {
         m_defaultRenderer = defaultRenderer;
@@ -28,7 +38,28 @@ public class FloatRenderer implements TableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 
         Float f = (Float) value;
-        String formatedValue = ((f == null) || (f.isNaN())) ? "" : DataFormat.format(f.floatValue(), m_digits);
+        String formatedValue;
+        
+        if ((f == null) || (f.isNaN())) {
+            if (m_showNaN) {
+                formatedValue = "NaN";
+            } else {
+                formatedValue = "";
+            }
+        } else if (m_scientific) {
+            double dAbs = Math.abs(f.floatValue());
+            if ((dAbs!=0) && (dAbs*Math.pow(10, m_digits-1)>=1)) {
+                formatedValue = DataFormat.format(f.floatValue(), m_digits);
+            } else {
+                int digits = m_digits - 2;
+                if (digits < 2) {
+                    digits = 2;
+                }
+                formatedValue = DataFormat.formatScientific(f.floatValue(), digits);
+            }
+        } else {
+            formatedValue = DataFormat.format(f.floatValue(), m_digits);
+        }
 
         
 

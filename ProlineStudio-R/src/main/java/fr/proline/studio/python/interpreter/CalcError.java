e8e96821e255d726f1343b12faaa0635bf1aa9cb
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2015-08-25 07:01:40 +0000

    better error management for DataMixer

---
+++ ProlineStudio-R/src/main/java/fr/proline/studio/python/interpreter/CalcError.java
@@ -0,0 +1,44 @@
+package fr.proline.studio.python.interpreter;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ *
+ * @author jm235353
+ */
+public class CalcError {
+    
+    private final Throwable m_t;
+    private final String m_errorMessage;
+    private final int m_lineError;
+
+    public CalcError(Throwable t, String errorMessage, int lineError) {
+        m_t = t;
+        if ((errorMessage == null) && (t!=null)) {
+            m_errorMessage = t.getMessage();
+        } else {
+            m_errorMessage = errorMessage;
+        }
+        m_lineError = lineError;
+    }
+    
+    public String getErrorMessage() {
+        return m_errorMessage;
+    }
+    
+    public int getLineError() {
+        return m_lineError;
+    }
+    
+    public String getFullErrorMessage() {
+        if (m_t != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            m_t.printStackTrace(pw);
+            return sw.toString();
+        } else {
+            return m_errorMessage;
+        }
+    }
+}

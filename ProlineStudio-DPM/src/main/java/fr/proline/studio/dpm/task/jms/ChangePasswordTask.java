ace17716b9fb2d0bda3b3f2d437e5f24841e087a
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-09-14 14:28:10 +0000

    Studio: changePasswordTask added for JMS

---
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/ChangePasswordTask.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.dpm.task.jms;
+
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Message;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Notification;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import fr.profi.util.security.SecurityUtils;
+import fr.proline.studio.dam.taskinfo.TaskError;
+import fr.proline.studio.dam.taskinfo.TaskInfo;
+import fr.proline.studio.dpm.jms.AccessJMSManagerThread;
+import static fr.proline.studio.dpm.task.jms.AbstractJMSTask.m_loggerProline;
+import fr.proline.studio.dpm.task.util.JMSConnectionManager;
+import java.util.HashMap;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+/**
+ * change password task through JMS
+ * @author MB243701
+ */
+public class ChangePasswordTask  extends AbstractJMSTask {
+    private static final String m_serviceName = "proline/admin/UserAccount";
+    private static final String m_methodName = "change_password";
+    //private static final String m_version = "2.0";
+    
+    private String m_userName;
+    private String m_password;
+    private String m_oldPassword;
+    
+    public ChangePasswordTask(AbstractJMSCallback callback,  String userName, String oldPassword, String newPassword) {
+        super(callback,  new TaskInfo("Change password for " + userName, false, TASK_LIST_INFO, TaskInfo.INFO_IMPORTANCE_HIGH));
+        m_userName = userName;
+        m_password = newPassword;
+        m_oldPassword = oldPassword;       
+    }
+    
+    @Override
+    public void taskRun() throws JMSException {
+        final JSONRPC2Request jsonRequest = new JSONRPC2Request(m_methodName, Integer.valueOf(m_id));
+        jsonRequest.setNamedParams(createParams());
+           
+        final TextMessage message = AccessJMSManagerThread.getAccessJMSManagerThread().getSession().createTextMessage(jsonRequest.toJSONString());
+
+        /* ReplyTo = Temporary Destination Queue for Server -> Client response */
+        message.setJMSReplyTo(m_replyQueue);
+        message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_NAME_KEY, m_serviceName);
+        //message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_VERSION_KEY, m_version);
+        
+        setTaskInfoRequest(message.getText());
+	
+        //  Send the Message
+        m_producer.send(message);
+        m_loggerProline.info("Message [{}] sent", message.getJMSMessageID());
+                    
+    }
+    
+    private HashMap<String, Object> createParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        //-- Global PARAMS
+        params.put("login", m_userName);
+        params.put("old_password_hash", SecurityUtils.sha256Hex(m_oldPassword));
+        params.put("new_password_hash", SecurityUtils.sha256Hex(m_password));
+        return params;
+    }
+    
+    @Override
+    public void taskDone(final Message jmsMessage) throws Exception {
+        
+        final TextMessage textMessage = (TextMessage) jmsMessage;
+        final String jsonString = textMessage.getText();
+
+        final JSONRPC2Message jsonMessage = JSONRPC2Message.parse(jsonString);
+        if(jsonMessage instanceof JSONRPC2Notification) {
+            m_loggerProline.warn("JSON Notification method: " + ((JSONRPC2Notification) jsonMessage).getMethod()+" instead of JSON Response");
+            throw new Exception("Invalid JSONRPC2Message type");
+            
+        } else if (jsonMessage instanceof JSONRPC2Response)  {
+            
+            final JSONRPC2Response jsonResponse = (JSONRPC2Response) jsonMessage;
+	    m_loggerProline.debug("JSON Response Id: " + jsonResponse.getID());
+            
+            final JSONRPC2Error jsonError = jsonResponse.getError();
+
+	    if (jsonError != null) {
+		m_loggerProline.error("JSON Error code {}, message : \"{}\"", jsonError.getCode(), jsonError.getMessage());
+		m_loggerProline.error("JSON Throwable", jsonError);
+                throw jsonError;
+	    }
+             
+            final Object rs = jsonResponse.getResult();
+            if (rs == null || ! Boolean.class.isInstance(rs) ) {
+		m_loggerProline.debug("Invalid result: No status returned");
+                throw new Exception("Invalid result "+rs);
+	    } else {
+		m_loggerProline.debug("Result :\n" + rs); 
+	    }
+            Boolean status = (Boolean) rs;
+            
+            if (!status) {
+                m_taskError = new TaskError("Change password failed");
+                return ;
+            }
+        }
+          m_currentState = JMSState.STATE_DONE;
+        
+    }
+}

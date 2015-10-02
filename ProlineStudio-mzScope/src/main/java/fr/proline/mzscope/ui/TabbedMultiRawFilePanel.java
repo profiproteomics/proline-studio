3bbdd2e0258bbb519f6a1fa0ffeb8295c7959de2
Marjolaine Bodin <Marjolaine.BODIN@cea.fr>
2015-10-02 09:45:13 +0000

    MzScope: multiRawFile display fixed

--- ProlineStudio-mzScope/src/main/java/fr/proline/mzscope/ui/TabbedMultiRawFilePanel.java
+++ ProlineStudio-mzScope/src/main/java/fr/proline/mzscope/ui/TabbedMultiRawFilePanel.java
@@ -26,7 +26,6 @@ import javax.swing.event.ChangeListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  *
  * @author CB205360
@@ -44,7 +43,6 @@ public class TabbedMultiRawFilePanel extends JPanel implements IRawFilePanel {
     private final List<IRawFile> rawfiles;
     private final Map<IRawFile, Chromatogram> mapChromatogramForRawFile;
 
-    
     public TabbedMultiRawFilePanel(List<IRawFile> rawfiles) {
         super();
         this.rawfiles = rawfiles;
@@ -54,13 +52,14 @@ public class TabbedMultiRawFilePanel extends JPanel implements IRawFilePanel {
         }
         initComponents();
         spectrumContainerPanel.initChart();
+        displayTIC();
     }
 
-       private void initComponents() {
+    private void initComponents() {
         this.setLayout(new BorderLayout());
         this.add(getSplitPane(), BorderLayout.CENTER);
     }
- 
+
     private JSplitPane getSplitPane() {
         if (this.splitPane == null) {
             splitPane = new JSplitPane();
@@ -75,59 +74,68 @@ public class TabbedMultiRawFilePanel extends JPanel implements IRawFilePanel {
         }
         return splitPane;
     }
-    
-    private JTabbedPane getChromatogramContainerPanel(){
-        if (this.chromatogramContainerPanel == null){
-           chromatogramContainerPanel = new JTabbedPane();
-           chromatogramContainerPanel.setName("chromatogramContainerPanel");
-           for (IRawFile rawFile : rawfiles) {
-               chromatogramContainerPanel.addTab(rawFile.getName(), getChromatogramPanel(rawFile));
-           }
+
+    private JTabbedPane getChromatogramContainerPanel() {
+        if (this.chromatogramContainerPanel == null) {
+            chromatogramContainerPanel = new JTabbedPane();
+            chromatogramContainerPanel.setName("chromatogramContainerPanel");
+            for (IRawFile rawFile : rawfiles) {
+                chromatogramContainerPanel.addTab(rawFile.getName(), getChromatogramPanel(rawFile));
+            }
         }
         chromatogramContainerPanel.addChangeListener(new ChangeListener() {
-           @Override
-           public void stateChanged(ChangeEvent e) {
-              currentChromatogramPanel = (ChromatogramPanel)chromatogramContainerPanel.getComponentAt(chromatogramContainerPanel.getSelectedIndex());
-           }
-        }) ;
-        if (chromatogramContainerPanel.getTabCount() >0 ){
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                currentChromatogramPanel = (ChromatogramPanel) chromatogramContainerPanel.getComponentAt(chromatogramContainerPanel.getSelectedIndex());
+            }
+        });
+        if (chromatogramContainerPanel.getTabCount() > 0) {
             chromatogramContainerPanel.setSelectedIndex(0);
         }
         return chromatogramContainerPanel;
     }
-    
+
     private ChromatogramPanel getChromatogramPanel(final IRawFile rawFile) {
-           ChromatogramPanel chromatogramPanel = new ChromatogramPanel();
-           chromatogramPanel.setName("chromatogramPanel");
-           chromatogramPanel.addPropertyChangeListener(new PropertyChangeListener() {
-              @Override
-              public void propertyChange(PropertyChangeEvent evt) {
-                 currentChromatogramPanel = (ChromatogramPanel)evt.getSource();
-                 displayScan(rawFile.getSpectrumId((Float)evt.getNewValue()));
-              }
-           });
+        ChromatogramPanel chromatogramPanel = new ChromatogramPanel();
+        chromatogramPanel.setName("chromatogramPanel");
+        chromatogramPanel.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                currentChromatogramPanel = (ChromatogramPanel) evt.getSource();
+                displayScan(rawFile.getSpectrumId((Float) evt.getNewValue()));
+            }
+        });
         return chromatogramPanel;
     }
-    
-    private JPanel getSpectrumContainerPanel(){
-        if (this.spectrumContainerPanel == null){
-           spectrumContainerPanel = new SpectrumPanel(this);
-           spectrumContainerPanel.setName("spectrumContainerPanel");
-           spectrumContainerPanel.setLayout(new BorderLayout());
+
+    private JPanel getSpectrumContainerPanel() {
+        if (this.spectrumContainerPanel == null) {
+            spectrumContainerPanel = new SpectrumPanel(this);
+            spectrumContainerPanel.setName("spectrumContainerPanel");
+            spectrumContainerPanel.setLayout(new BorderLayout());
         }
         return spectrumContainerPanel;
     }
-    
+
     @Override
     public IRawFile getCurrentRawfile() {
+        Chromatogram c = getCurrentChromatogram();
+        if (c != null) {
+            for (Map.Entry<IRawFile, Chromatogram> entrySet : mapChromatogramForRawFile.entrySet()) {
+                if (entrySet.getValue().equals(c)) {
+                    return entrySet.getKey();
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public void extractAndDisplayChromatogram(final Ms1ExtractionRequest params, DisplayMode mode, MzScopeCallback callback) {
-       // in this implementation displayMode is ignored : always REPLACE since we will extract one Chromatogram per RawFile
+        // in this implementation displayMode is ignored : always REPLACE since we will extract one Chromatogram per RawFile
         SwingWorker worker = new SwingWorker<Integer, Chromatogram>() {
             int count = 0;
+
             @Override
             protected Integer doInBackground() throws Exception {
 
@@ -163,31 +171,40 @@ public class TabbedMultiRawFilePanel extends JPanel implements IRawFilePanel {
 
         worker.execute();
     }
-    
+
     @Override
     public Color displayChromatogram(Chromatogram chromato, DisplayMode mode) {
-       // TODO : dispach to multiple Components
-       return null;
-       }
-    
-    
+        String rawFileName = chromato.rawFilename;
+        int nbTab = chromatogramContainerPanel.getTabCount();
+        Color c = null;
+        for (int t = 0; t < nbTab; t++) {
+            String tabTitle = chromatogramContainerPanel.getTitleAt(t);
+            if (tabTitle.equals(rawFileName)) {  // see how to better get the tabComponent linked to a rawFile
+                ChromatogramPanel chromatoPanel = (ChromatogramPanel) chromatogramContainerPanel.getComponentAt(t);
+                c = chromatoPanel.displayChromatogram(chromato, mode);
+                mapChromatogramForRawFile.put(getCurrentRawfile(), chromato);
+            }
+        }
+        return c;
+    }
+
     // override display feature to display all xic
     @Override
     public void displayFeature(final IFeature f) {
-       // TODO : how to handle this simple display of a unique feature ? In this case multiple Features must be displayed, on by RawFile
+        // TODO : how to handle this simple display of a unique feature ? In this case multiple Features must be displayed, on by RawFile
     }
-    
+
     @Override
     public void displayScan(long index) {
-        if ((currentScan == null) || (index != currentScan.getIndex())) {
-            currentScan = getCurrentRawfile().getSpectrum((int)index);
+        if ((currentScan == null) || (index != currentScan.getIndex()) && getCurrentRawfile() != null) {
+            currentScan = getCurrentRawfile().getSpectrum((int) index);
             if (currentScan != null) {
-                    spectrumContainerPanel.displayScan(currentScan);
+                spectrumContainerPanel.displayScan(currentScan);
             }
         }
     }
-    
-    private IRawFile getRawFile(String fileName){
+
+    private IRawFile getRawFile(String fileName) {
         for (IRawFile rawFile : rawfiles) {
             if (rawFile.getName().equals(fileName)) {
                 return rawFile;
@@ -196,15 +213,51 @@ public class TabbedMultiRawFilePanel extends JPanel implements IRawFilePanel {
         return null;
     }
 
-
     @Override
     public Color getPlotColor(String rawFilename) {
         return CyclicColorPalette.getColor(1);
     }
 
-   @Override
-   public Chromatogram getCurrentChromatogram() {
-      return (currentChromatogramPanel == null) ? null : currentChromatogramPanel.getCurrentChromatogram() ;
-   }
+    @Override
+    public Chromatogram getCurrentChromatogram() {
+        return (currentChromatogramPanel == null) ? null : currentChromatogramPanel.getCurrentChromatogram();
+    }
+
+    private void displayTIC() {
+
+        SwingWorker worker = new SwingWorker<Integer, Chromatogram>() {
+            int count = 0;
+
+            @Override
+            protected Integer doInBackground() throws Exception {
+
+                for (IRawFile rawFile : rawfiles) {
+                    Chromatogram c = rawFile.getTIC();
+                    count++;
+                    publish(c);
+                }
+                return count;
+            }
+
+            @Override
+            protected void process(List<Chromatogram> chunks) {
+                for (int k = 0; k < chunks.size(); k++) {
+                    logger.info("display  chromato");
+                    displayChromatogram(chunks.get(k), DisplayMode.REPLACE);
+                }
+            }
+
+            @Override
+            protected void done() {
+                try {
+                    logger.info("{} TIC chromatogram extracted", get());
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.error("Error while reading chromatogram");
+                }
+            }
+        };
+
+        worker.execute();
+    }
 
 }
--- ProlineStudio-mzScope/src/main/java/fr/proline/mzscope/ui/XICExtractionPanel.java
+++ ProlineStudio-mzScope/src/main/java/fr/proline/mzscope/ui/XICExtractionPanel.java
@@ -12,7 +12,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.event.EventListenerList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

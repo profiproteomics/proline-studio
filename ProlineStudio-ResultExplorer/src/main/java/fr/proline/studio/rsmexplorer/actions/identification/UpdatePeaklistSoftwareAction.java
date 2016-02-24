6767d1e7435d3edf7e04227eec664bb719b82ea1
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-02-24 14:28:19 +0000

    Permet la mise à jour des Spectrum title après import

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/UpdatePeaklistSoftwareAction.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.rsmexplorer.actions.identification;
+
+import fr.proline.core.orm.uds.Project;
+import fr.proline.studio.dam.DatabaseDataManager;
+import fr.proline.studio.dpm.jms.AccessJMSManagerThread;
+import fr.proline.studio.dpm.task.jms.AbstractJMSCallback;
+import fr.proline.studio.dpm.task.jms.UpdateSpectraParamsTask;
+import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
+import fr.proline.studio.rsmexplorer.gui.dialog.UpdatePeaklistSoftDialog;
+import fr.proline.studio.rsmexplorer.tree.AbstractNode;
+import fr.proline.studio.rsmexplorer.tree.AbstractTree;
+import fr.proline.studio.rsmexplorer.tree.DataSetNode;
+import fr.proline.studio.rsmexplorer.tree.identification.IdentificationTree;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.tree.DefaultTreeModel;
+import org.openide.util.NbBundle;
+import org.openide.windows.WindowManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author VD225637
+ */
+public class UpdatePeaklistSoftwareAction extends AbstractRSMAction {
+    protected static final Logger m_logger = LoggerFactory.getLogger("ProlineStudio.ResultExplorer");
+ 
+    public UpdatePeaklistSoftwareAction(){        
+        super(NbBundle.getMessage(GenerateSpectrumMatchesAction.class, "CTL_UpdatePeaklistSoft"), AbstractTree.TreeType.TREE_IDENTIFICATION);
+    }
+    
+    @Override
+    public void actionPerformed(final AbstractNode[] selectedNodes, int x, int y) {
+        UpdatePeaklistSoftDialog dialog = UpdatePeaklistSoftDialog.getDialog(WindowManager.getDefault().getMainWindow());
+        dialog.setLocation(x, y);
+        dialog.setVisible(true);
+        if (dialog.getButtonClicked() == DefaultDialog.BUTTON_OK) {
+            Long peaklistSoftID = dialog.getSelectedPeaklistSoftwareId();
+            if(peaklistSoftID != null){
+                //get Selected Node RS Ids
+                int nbrNodes = selectedNodes.length;
+                List<Long> rsIds = new ArrayList<>();
+                Long projectId = ((DataSetNode) selectedNodes[0]).getDataset().getProject().getId();
+                IdentificationTree tree = IdentificationTree.getCurrentTree();
+                DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+                List<DataSetNode> selectedDSNodes = new ArrayList<>();
+                
+                for(int i =0; i< nbrNodes; i++){
+                    DataSetNode dataSetNode = (DataSetNode) selectedNodes[i];                                      
+                    selectedDSNodes.add(dataSetNode);
+                    rsIds.add(dataSetNode.getResultSetId());
+                    dataSetNode.setIsChanging(true);
+                    treeModel.nodeChanged(dataSetNode);
+                }
+                                
+                
+                //CALL TASK WITH IDS  
+                AbstractJMSCallback callback = new AbstractJMSCallback() {
+
+                    @Override
+                    public boolean mustBeCalledInAWT() {
+                        return true;
+                    }
+
+                    @Override
+                    public void run(boolean success) {
+                        IdentificationTree tree = IdentificationTree.getCurrentTree();
+                        DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+                        
+                        for(DataSetNode node : selectedDSNodes){
+                            node.setIsChanging(false);                                
+                            treeModel.nodeChanged(node);
+                        }                  
+                    }
+                };
+                
+                UpdateSpectraParamsTask task = new UpdateSpectraParamsTask(callback, projectId, rsIds, peaklistSoftID);
+                AccessJMSManagerThread.getAccessJMSManagerThread().addTask(task);
+            }
+        }
+    }
+    
+    
+        @Override
+    public void updateEnabled(AbstractNode[] selectedNodes) {
+        
+        // to execute this action, the user must be the owner of the project
+        Project selectedProject = ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject();
+        if (!DatabaseDataManager.getDatabaseDataManager().ownProject(selectedProject)) {
+            setEnabled(false);
+            return;
+        }
+        
+        int nbSelectedNodes = selectedNodes.length;
+        for (int i = 0; i < nbSelectedNodes; i++) {
+            AbstractNode node = selectedNodes[i];
+
+            if (node.isChanging()) {
+                setEnabled(false);
+                return;
+            }
+
+            if (node.getType() != AbstractNode.NodeTypes.DATA_SET) {
+                setEnabled(false);
+                return;
+            }
+            
+            DataSetNode dataSetNode = (DataSetNode) node;
+            if (! dataSetNode.isLeaf() ||  ! dataSetNode.hasResultSet()) {
+                setEnabled(false);
+                return;
+            }
+
+
+        }
+        setEnabled(true);
+    }
+    
+}
---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/UpdatePeaklistSoftDialog.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.rsmexplorer.gui.dialog;
+
+
+import fr.proline.core.orm.uds.PeaklistSoftware;
+import fr.proline.studio.dam.DatabaseDataManager;
+import fr.proline.studio.gui.DefaultDialog;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Window;
+import java.util.HashMap;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+
+/**
+ *
+ * @author VD225637
+ */
+public class UpdatePeaklistSoftDialog extends DefaultDialog {
+    
+    private JComboBox m_peaklistSoftwaresComboBox = null;
+    private static UpdatePeaklistSoftDialog m_singletonDialog = null;
+    private HashMap<String, PeaklistSoftware> m_peaklistByIdent = null;
+          
+     public static UpdatePeaklistSoftDialog getDialog(Window parent/*, long projectId*/) {
+        if (m_singletonDialog == null) {
+            m_singletonDialog = new UpdatePeaklistSoftDialog(parent);
+        }
+
+        return m_singletonDialog;
+    }
+     
+     private UpdatePeaklistSoftDialog(Window parent) {
+        super(parent, Dialog.ModalityType.APPLICATION_MODAL);
+
+        setTitle("Update Spectrum Parameters");
+       
+//        setHelpURL("http://biodev.extra.cea.fr/docs/proline/doku.php?id=how_to:studio:importmascot");
+        
+//        setButtonVisible(BUTTON_LOAD, true);
+//        setButtonVisible(BUTTON_SAVE, true);
+        
+        setResizable(true);
+        setMinimumSize(new Dimension(200, 240));
+
+        setInternalComponent(createInternalPanel());   
+        pack();
+    }
+
+    private JPanel createInternalPanel() {
+         
+        JPanel panel = new JPanel();        
+        GridBagConstraints c = new GridBagConstraints();
+        panel.setLayout(new GridBagLayout());
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new java.awt.Insets(5, 5, 5, 5);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        JTextArea helpTxt = new JTextArea("Choose Peaklist Software to use to generate (new) spectrum parameters.");
+        helpTxt.setRows(1);
+        helpTxt.setForeground(Color.gray);
+        panel.add(helpTxt, c);
+        
+        
+        c.gridx++;
+        c.gridwidth = 1;
+        c.gridy=0;
+        JLabel peaklistSoftwareLabel = new JLabel("Peaklist Software :");
+        peaklistSoftwareLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        panel.add(peaklistSoftwareLabel, c);
+        
+        c.gridy++;                      
+        PeaklistSoftware[] peaksofts = DatabaseDataManager.getDatabaseDataManager().getPeaklistSoftwaresWithNullArray();
+        m_peaklistSoftwaresComboBox = new JComboBox();   
+        m_peaklistByIdent = new HashMap<>();
+        for(PeaklistSoftware nextPLS : peaksofts){
+            if(nextPLS == null)
+                 m_peaklistSoftwaresComboBox.addItem(null);
+            else{
+                String nextPLSIdent = getPeakListSoftwareString(nextPLS);
+                m_peaklistByIdent.put(nextPLSIdent, nextPLS);
+                m_peaklistSoftwaresComboBox.addItem(nextPLSIdent);
+            }
+        }      
+        panel.add(m_peaklistSoftwaresComboBox, c);        
+        
+        return panel;
+    }
+    
+    public String getPeakListSoftwareString(PeaklistSoftware o) {
+        String version = o.getVersion();
+        if (version == null) {
+            return o.getName();
+        }
+        return o.getName()+" "+version;
+    }
+    
+    public Long getSelectedPeaklistSoftwareId(){
+        if(m_peaklistSoftwaresComboBox.getSelectedItem() != null){
+            return m_peaklistByIdent.get(m_peaklistSoftwaresComboBox.getSelectedItem().toString()).getId();   
+        }
+            
+        return null;
+    }
+
+}

6512fc70d4ab2096096b5a8bffa01c8115a00e36
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-03-11 06:58:47 +0000

    Add action to retrieve BioSequence

---
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/RetrieveBioSeqTask.java
@@ -0,0 +1,102 @@
+package fr.proline.studio.dpm.task.jms;
+
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Message;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Notification;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import fr.proline.studio.dam.taskinfo.TaskInfo;
+import fr.proline.studio.dpm.jms.AccessJMSManagerThread;
+import fr.proline.studio.dpm.task.util.JMSConnectionManager;
+import java.util.HashMap;
+import java.util.List;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * Task used to retrieve BioSequence from source (fasta ....) to SeqRepository
+ * and then to MSI where other calculated properties are stored (MW, coverage)
+ *
+ * @author VD225637
+ */
+public class RetrieveBioSeqTask extends AbstractJMSTask {
+
+    private List<Long> m_resultSummariesIds = null;
+    private long m_projectId;
+    private boolean m_forceUpdate = false;
+    private String m_lastMsgId = null;
+    
+    public RetrieveBioSeqTask(AbstractJMSCallback callback, List<Long> rsmIds, long projectId, boolean forceUpdate) {
+        super(callback, new TaskInfo("Retrieve Protein's Sequences", false, TASK_LIST_INFO, TaskInfo.INFO_IMPORTANCE_HIGH));
+        m_resultSummariesIds = rsmIds;
+        m_projectId = projectId;
+        m_forceUpdate = forceUpdate;
+    }
+
+    
+    @Override
+    public void taskRun() throws JMSException {
+        final JSONRPC2Request jsonRequest = new JSONRPC2Request(JMSConnectionManager.PROLINE_PROCESS_METHOD_NAME, Integer.valueOf(m_id));
+        jsonRequest.setNamedParams(createParams());
+
+        final TextMessage message = AccessJMSManagerThread.getAccessJMSManagerThread().getSession().createTextMessage(jsonRequest.toJSONString());
+
+        /* ReplyTo = Temporary Destination Queue for Server -> Client response */
+        message.setJMSReplyTo(m_replyQueue);
+        message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_NAME_KEY, "proline/seq/RetrieveBioSeqForRSMs");
+        setTaskInfoRequest(message.getText());
+
+        //  Send the Message        
+        m_producer.send(message, Message .DEFAULT_DELIVERY_MODE, Message.DEFAULT_PRIORITY,50000);
+        m_lastMsgId = message.getJMSMessageID();
+        m_loggerProline.info("Message [{}] sent", m_lastMsgId);
+
+    }
+
+    private HashMap<String, Object> createParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("project_id", m_projectId);
+        params.put("result_summaries_ids", m_resultSummariesIds);
+        params.put("force_update", m_forceUpdate);
+        return params;
+    }
+
+    
+    @Override
+    public void taskDone(Message jmsMessage) throws Exception {
+        final TextMessage textMessage = (TextMessage) jmsMessage;
+        final String jsonString = textMessage.getText();
+
+        final JSONRPC2Message jsonMessage = JSONRPC2Message.parse(jsonString);
+        if(jsonMessage instanceof JSONRPC2Notification) {
+            m_loggerProline.warn("JSON Notification method: " + ((JSONRPC2Notification) jsonMessage).getMethod()+" instead of JSON Response");
+            throw new Exception("Invalid JSONRPC2Message type");
+            
+        } else if (jsonMessage instanceof JSONRPC2Response)  {
+            final JSONRPC2Response jsonResponse = (JSONRPC2Response) jsonMessage;
+	    m_loggerProline.debug("JSON Response Id: " + jsonResponse.getID());
+            
+            final JSONRPC2Error jsonError = jsonResponse.getError();
+
+	    if (jsonError != null) {
+		m_loggerProline.error("JSON Error code {}, message : \"{}\"", jsonError.getCode(), jsonError.getMessage());
+		m_loggerProline.error("JSON Throwable", jsonError);
+                throw jsonError;
+	    }
+                        
+            final Object result = jsonResponse.getResult(); //Should be String OK...
+            if ( result == null )  {
+                m_loggerProline.error(getClass().getSimpleName() + " failed : No returned values");
+                throw new Exception("Invalid result "+result);
+            }
+        }
+               
+        m_currentState = JMSState.STATE_DONE;
+    }
+    
+               
+            
+}
+
---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/RetrieveBioSeqJMSAction.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.rsmexplorer.actions.identification;
+
+import fr.proline.core.orm.uds.Project;
+import fr.proline.core.orm.uds.dto.DDataset;
+import fr.proline.studio.dam.DatabaseDataManager;
+import fr.proline.studio.dpm.jms.AccessJMSManagerThread;
+import fr.proline.studio.dpm.task.jms.AbstractJMSCallback;
+import fr.proline.studio.dpm.task.jms.RetrieveBioSeqTask;
+import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
+import fr.proline.studio.rsmexplorer.tree.AbstractNode;
+import fr.proline.studio.rsmexplorer.tree.AbstractTree;
+import fr.proline.studio.rsmexplorer.tree.DataSetNode;
+import fr.proline.studio.rsmexplorer.tree.identification.IdentificationTree;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.tree.DefaultTreeModel;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author VD225637
+ */
+public class RetrieveBioSeqJMSAction extends AbstractRSMAction {
+
+    public RetrieveBioSeqJMSAction() {
+        super(NbBundle.getMessage(RetrieveBioSeqJMSAction.class, "CTL_RetrieveBioSeq"), AbstractTree.TreeType.TREE_IDENTIFICATION);
+    }
+
+    @Override
+    public void updateEnabled(AbstractNode[] selectedNodes) {
+
+        // to execute this action, the user must be the owner of the project
+        Project selectedProject = ProjectExplorerPanel.getProjectExplorerPanel().getSelectedProject();
+        if (!DatabaseDataManager.getDatabaseDataManager().ownProject(selectedProject)) {
+            setEnabled(false);
+            return;
+        }
+
+        // note : we can ask for the retrieve bioSequence on multiple ResultSummaries in one time
+        int nbSelectedNodes = selectedNodes.length;
+        for (int i = 0; i < nbSelectedNodes; i++) {
+            AbstractNode node = selectedNodes[i];
+
+            // parent node is being created, we can not validate it (for the moment)
+            if (node.isChanging()) {
+                setEnabled(false);
+                return;
+            }
+
+            // parent node must be a dataset
+            if (node.getType() != AbstractNode.NodeTypes.DATA_SET) {
+                setEnabled(false);
+                return;
+            }
+
+            // parent node must have a ResultSummary
+            DataSetNode dataSetNode = (DataSetNode) node;
+            if (!dataSetNode.hasResultSummary()) {
+                setEnabled(false);
+                return;
+            }
+        }
+
+        setEnabled(true);
+    }
+
+    @Override
+    public void actionPerformed(AbstractNode[] selectedNodes, int x, int y) {
+
+        IdentificationTree tree = IdentificationTree.getCurrentTree();
+        DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+        List<DataSetNode> datasets = new ArrayList<>();
+        List<Long> rsmIds = new ArrayList<>();
+
+        int nbNodes = selectedNodes.length;
+        for (int i = 0; i < nbNodes; i++) {
+            final DataSetNode dataSetNode = (DataSetNode) selectedNodes[i];
+            datasets.add(dataSetNode);
+            dataSetNode.setIsChanging(true);
+            treeModel.nodeChanged(dataSetNode);
+
+            final DDataset d = dataSetNode.getDataset();
+            rsmIds.add(d.getResultSummaryId());
+        }
+
+        Long projectId = ((DataSetNode) selectedNodes[0]).getDataset().getProject().getId();
+
+        AbstractJMSCallback callback = new AbstractJMSCallback() {
+
+            @Override
+            public boolean mustBeCalledInAWT() {
+                return true;
+            }
+
+            @Override
+            public void run(boolean success) {
+                IdentificationTree tree = IdentificationTree.getCurrentTree();
+                DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
+                for (DataSetNode dataSetNode : datasets) {
+                    dataSetNode.setIsChanging(false);
+                    treeModel.nodeChanged(dataSetNode);
+                }
+            }
+        };
+
+        RetrieveBioSeqTask task = new RetrieveBioSeqTask(callback,rsmIds, projectId, false);
+        AccessJMSManagerThread.getAccessJMSManagerThread().addTask(task);
+
+    }
+
+}

441977903d1fa0e9098b1e8d80fb61ad3ebcc585
Christophe Bruley <christophe.bruley@cea.fr>
2016-03-15 16:08:20 +0000

    - new Abundance Summarizer Mode added

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/xic/QuantProfileXICPanel.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/xic/QuantProfileXICPanel.java
@@ -46,8 +46,8 @@ public class QuantProfileXICPanel extends JPanel {
     
     private JComboBox<String> m_abundanceSummarizerMethodCB;
     
-    private final static String[] ABUNDANCE_SUMMARIZER_METHOD_VALUES = {"Mean", "Mean of top 3 peptides", "Median", "Median Profile", "Sum"};
-    private final static String[] ABUNDANCE_SUMMARIZER_METHOD_KEYS = {"MEAN", "MEAN_OF_TOP3", "MEDIAN","MEDIAN_PROFILE", "SUM"};
+    private final static String[] ABUNDANCE_SUMMARIZER_METHOD_VALUES = {"Mean", "Mean of top 3 peptides", "Median", "Median Biological Profile", "Median Profile", "Sum"};
+    private final static String[] ABUNDANCE_SUMMARIZER_METHOD_KEYS = {"MEAN", "MEAN_OF_TOP3", "MEDIAN", "MEDIAN_BIOLOGICAL_PROFILE", "MEDIAN_PROFILE", "SUM"};
      
     private JTextField m_peptideStatTestsAlpha;
     private JCheckBox m_applyPepNormalizationChB;

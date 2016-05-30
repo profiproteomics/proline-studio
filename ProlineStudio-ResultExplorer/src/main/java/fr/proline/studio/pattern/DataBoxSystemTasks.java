a15e7783586177db62bad7662b9eec85d62cf2af
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-05-30 13:03:56 +0000

    Add SystemTasks/QueueMonitoring ...On going, refactoring needed !

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/pattern/DataBoxSystemTasks.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.pattern;
+
+import fr.proline.studio.rsmexplorer.gui.SystemTasksPanel;
+
+/**
+ *
+ * @author VD225637
+ */
+public class DataBoxSystemTasks extends AbstractDataBox  {
+    
+      
+    public DataBoxSystemTasks() {
+        super(DataboxType.DataBoxSystemTasks);
+//        setLayout(SplittedPanelContainer.PanelLayout.TABBED);
+        // Name of this databox
+        m_typeName = "System Tasks List";
+    }
+    
+    @Override
+    public void createPanel() {
+        SystemTasksPanel p = new SystemTasksPanel();
+        p.setName(m_typeName);
+        p.setDataBox(this);
+        m_panel = p;
+    }
+
+    @Override
+    public void dataChanged() {
+        //
+    }
+    
+}

864c01e40870c7d96af9650b1e604bca38be78b8
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2016-06-03 13:54:20 +0000

    #14905 Empty Trash > Clean up Project : Added functionality so that the user is asked before emptying the trash. User is questioned on wether he wants to clean up the database.

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/EmptyTrashAction.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/EmptyTrashAction.java
@@ -1,11 +1,11 @@
 package fr.proline.studio.rsmexplorer.actions.identification;
 
-
 import fr.proline.core.orm.uds.Project;
 import fr.proline.core.orm.uds.dto.DDataset;
 import fr.proline.studio.dam.AccessDatabaseThread;
 import fr.proline.studio.dam.DatabaseDataManager;
 import fr.proline.studio.dam.data.ClearProjectData;
+import fr.proline.studio.dam.taskinfo.TaskInfoManager;
 import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
 import fr.proline.studio.dam.tasks.DatabaseClearProjectTask;
 import fr.proline.studio.dam.tasks.DatabaseDataSetTask;
@@ -15,6 +15,8 @@ import fr.proline.studio.dpm.task.jms.AbstractJMSCallback;
 import fr.proline.studio.dpm.task.jms.ClearProjectTask;
 import fr.proline.studio.dpm.task.util.JMSConnectionManager;
 import fr.proline.studio.gui.DefaultDialog;
+import fr.proline.studio.gui.InfoDialog;
+import fr.proline.studio.gui.OptionDialog;
 import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
 import fr.proline.studio.rsmexplorer.gui.dialog.ClearProjectDialog;
 import fr.proline.studio.rsmexplorer.tree.DataSetNode;
@@ -24,23 +26,24 @@ import fr.proline.studio.rsmexplorer.tree.AbstractTree;
 import fr.proline.studio.rsmexplorer.tree.quantitation.QuantitationTree;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.tree.DefaultTreeModel;
 import org.openide.util.NbBundle;
 import org.openide.windows.WindowManager;
 
 /**
  * Action to empty the Trash
+ *
  * @author JM235353
  */
 public class EmptyTrashAction extends AbstractRSMAction {
 
     private AbstractTree.TreeType m_treeType;
-    
+
     public EmptyTrashAction(AbstractTree.TreeType treeType) {
         super(NbBundle.getMessage(EmptyTrashAction.class, "CTL_EmptyTrashAction"), treeType);
         this.m_treeType = treeType;
     }
-    
 
 //    @Override
     public void actionPerformedWOClearRsRSM(AbstractNode[] selectedNodes, int x, int y) {
@@ -49,40 +52,38 @@ public class EmptyTrashAction extends AbstractRSMAction {
         final AbstractNode n = selectedNodes[0];
         DataSetNode datasetNode = (DataSetNode) n;
         DDataset trashDataset = datasetNode.getDataset();
-        
+
         AbstractTree tree = null;
         boolean identificationDataset = false;
-        if (this.m_treeType  == AbstractTree.TreeType.TREE_IDENTIFICATION) {
+        if (this.m_treeType == AbstractTree.TreeType.TREE_IDENTIFICATION) {
             tree = IdentificationTree.getCurrentTree();
             identificationDataset = true;
-        }else if (this.m_treeType  == AbstractTree.TreeType.TREE_QUANTITATION) {
+        } else if (this.m_treeType == AbstractTree.TreeType.TREE_QUANTITATION) {
             tree = QuantitationTree.getCurrentTree();
             identificationDataset = false;
         }
-        if (tree == null){
+        if (tree == null) {
             return;
         }
         final DefaultTreeModel treeModel = (DefaultTreeModel) tree.getModel();
 
         n.setIsChanging(true);
 
-
         AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
-            
+
             @Override
             public boolean mustBeCalledInAWT() {
                 return true;
             }
-            
+
             @Override
             public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
                 n.setIsChanging(false);
                 if (success) {
-                    
+
                     n.removeAllChildren();
                     treeModel.nodeStructureChanged(n);
-                    
-                    
+
                 } else {
                     treeModel.nodeChanged(n);
                 }
@@ -92,11 +93,9 @@ public class EmptyTrashAction extends AbstractRSMAction {
         DatabaseDataSetTask task = new DatabaseDataSetTask(callback);
         task.initEmptyTrash(trashDataset, identificationDataset);
         AccessDatabaseThread.getAccessDatabaseThread().addTask(task);
-        
 
     }
 
-    
     @Override
     public void updateEnabled(AbstractNode[] selectedNodes) {
 
@@ -106,17 +105,28 @@ public class EmptyTrashAction extends AbstractRSMAction {
             setEnabled(false);
             return;
         }
-        
+
         AbstractNode n = selectedNodes[0];  // only one node can be selected
 
         setEnabled(!n.isChanging() && (!n.isLeaf()));
 
     }
-  
-    
+
     //TODO: replace current actionPerformed with this method to connect the clear rs/rsm data on the empty trash action
     @Override
     public void actionPerformed(AbstractNode[] selectedNodes, int x, int y) {
+
+        // check if user also wants to erase locally
+        InfoDialog exitDialog = new InfoDialog(WindowManager.getDefault().getMainWindow(), InfoDialog.InfoType.WARNING, "Confirm Erasing of Recycle Bin", "Are you sure you want to erase the contents of the Recycle Bin?");
+        exitDialog.setButtonName(OptionDialog.BUTTON_OK, "Yes");
+        exitDialog.setButtonName(OptionDialog.BUTTON_CANCEL, "No");
+        exitDialog.centerToWindow(WindowManager.getDefault().getMainWindow());
+        exitDialog.setVisible(true);
+
+        if (exitDialog.getButtonClicked() == OptionDialog.BUTTON_CANCEL) {
+            // No clicked
+            return;
+        }
 
         // selected node is the Trash
         final AbstractNode n = selectedNodes[0];
@@ -144,6 +154,7 @@ public class EmptyTrashAction extends AbstractRSMAction {
         List<ClearProjectData> listDataToClear = new ArrayList();
         List<ClearProjectData> openedData = ProjectExplorerPanel.getOpenedData(project);
 
+        //probably here nothing is deleted yet(could be wrong - Andreas)
         AbstractDatabaseCallback loadClearCallback = new AbstractDatabaseCallback() {
 
             @Override
@@ -164,9 +175,24 @@ public class EmptyTrashAction extends AbstractRSMAction {
                         n.setIsChanging(false);
                         if (success) {
 
+                            //I think this is local removing
                             n.removeAllChildren();
                             treeModel.nodeStructureChanged(n);
+                            
+                            
+                            // check if user also wants to delete from database
+                            InfoDialog exitDialog = new InfoDialog(WindowManager.getDefault().getMainWindow(), InfoDialog.InfoType.WARNING, "Do you agree with this action?", "We suggest you also clean the database");
+                            exitDialog.setButtonName(OptionDialog.BUTTON_OK, "Yes");
+                            exitDialog.setButtonName(OptionDialog.BUTTON_CANCEL, "No");
+                            exitDialog.centerToWindow(WindowManager.getDefault().getMainWindow());
+                            exitDialog.setVisible(true);
 
+                            if (exitDialog.getButtonClicked() == OptionDialog.BUTTON_CANCEL) {
+                                // No clicked
+                                return;
+                            }
+                            
+                           
                             ClearProjectDialog clearProjectDialog = new ClearProjectDialog(WindowManager.getDefault().getMainWindow(), trashDataset.getProject(), listDataToClear);
                             clearProjectDialog.setLocation(x, y);
                             DefaultDialog.ProgressTask task = new DefaultDialog.ProgressTask() {
@@ -242,5 +268,5 @@ public class EmptyTrashAction extends AbstractRSMAction {
         AccessDatabaseThread.getAccessDatabaseThread().addTask(clearDsTask);
 
     }
-    
+
 }

36ef059361aac84531c3d58e90bf13a8295e6163
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2016-06-27 09:28:49 +0000

    grayable possibility for renderers

---
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/table/renderer/GrayableTableCellRenderer.java
@@ -0,0 +1,12 @@
+package fr.proline.studio.table.renderer;
+
+import javax.swing.table.TableCellRenderer;
+
+/**
+ *
+ * @author JM235353
+ */
+public interface GrayableTableCellRenderer extends TableCellRenderer {
+
+    public void setGrayed(boolean v);
+}

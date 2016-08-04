18c8cda2a71af59b7f6a64d07ab59d3864d38b95
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-08-04 08:49:44 +0000

    import organize

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/util/ServiceNotificationListener.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/util/ServiceNotificationListener.java
@@ -24,9 +24,19 @@ import org.slf4j.LoggerFactory;
  */
 public class ServiceNotificationListener implements MessageListener {
 
+    public final static String NOTIFICATION_SERVICE_NAME_KEY="service_name";
+    public final static String NOTIFICATION_SERVICE_VERSION_KEY="service_version";
+    public final static String NOTIFICATION_SERVICE_SOURCE_KEY="service_source";
+    public final static String NOTIFICATION_SERVICE_MORE_INFO_KEY="complementary_info";
+    public final static String NOTIFICATION_TIMESTAMP_KEY= "event_timestamp";
+    public final static String NOTIFICATION_JMS_ID_KEY= "request_jms_message_id";
+    public final static String NOTIFICATION_JSON_REQ_ID_KEY= "json_rpc_request_id";
+    public final static String NOTIFICATION_SERVICE_STATUS_KEY= "event_type";   
+            
     protected static final Logger m_loggerProline = LoggerFactory.getLogger("ProlineStudio.DPM.Task");
     private AbstractJMSCallback m_callback = null;
     private JMSNotificationMessage[] m_replyVal = new JMSNotificationMessage[1];
+    
     public ServiceNotificationListener() {
     }
 
@@ -37,17 +47,21 @@ public class ServiceNotificationListener implements MessageListener {
 
     @Override
     public void onMessage(Message jmsMessage) {
-        m_loggerProline.debug("Notification Listener Receiving message  : " + JMSMessageUtil.formatMessage(jmsMessage));
-        if (jmsMessage instanceof TextMessage) {
-            final TextMessage textMessage = (TextMessage) jmsMessage;
+        if (m_callback == null) {
+            m_loggerProline.debug("SKIP Notification message  : " + JMSMessageUtil.formatMessage(jmsMessage));
+        } else {
+            m_loggerProline.debug("Notification Listener Receiving message  : " + JMSMessageUtil.formatMessage(jmsMessage));
+            if (jmsMessage instanceof TextMessage) {
+                final TextMessage textMessage = (TextMessage) jmsMessage;
 
-            try {
-                final String jsonString = textMessage.getText();
-                final JSONRPC2Notification  jsonNotif = JSONRPC2Notification.parse(jsonString);
-                Map<String,Object> params = jsonNotif.getNamedParams();
-                
-                JMSNotificationMessage msg = new JMSNotificationMessage(params.get("service_name").toString(),(Long) params.get("event_timestamp"), params.get("request_jms_message_id").toString(), params.get("json_rpc_request_id").toString(), params.get("event_type").toString());
-                if (m_callback != null) {
+                try {
+                    final String jsonString = textMessage.getText();
+                    final JSONRPC2Notification  jsonNotif = JSONRPC2Notification.parse(jsonString);
+                    Map<String,Object> params = jsonNotif.getNamedParams();                
+                    JMSNotificationMessage msg = new JMSNotificationMessage(params.getOrDefault(NOTIFICATION_SERVICE_NAME_KEY, "Undefined").toString(),params.getOrDefault(NOTIFICATION_SERVICE_VERSION_KEY, "default").toString(),
+                            params.getOrDefault(NOTIFICATION_SERVICE_SOURCE_KEY, "Unknown").toString(), params.getOrDefault(NOTIFICATION_SERVICE_MORE_INFO_KEY, "Unknown").toString(), (Long) params.getOrDefault(NOTIFICATION_TIMESTAMP_KEY,0l),
+                            params.getOrDefault(NOTIFICATION_JMS_ID_KEY, "Unknown").toString(), params.getOrDefault(NOTIFICATION_JSON_REQ_ID_KEY,"Unknown").toString(), 
+                            params.get(NOTIFICATION_SERVICE_STATUS_KEY).toString());
                     m_replyVal[0] =  msg;
                     if (m_callback.mustBeCalledInAWT()) {
                         // Callback must be executed in the Graphical thread (AWT)
@@ -63,14 +77,13 @@ public class ServiceNotificationListener implements MessageListener {
                         // In this case, we assume the execution is fast.
                         m_callback.run(true);                        
                     }
+                } catch (JMSException | JSONRPC2ParseException ex) {
+                    m_loggerProline.error("Error handling JMS Message", ex);
                 }
 
-            } catch (JMSException | JSONRPC2ParseException ex) {
-                m_loggerProline.error("Error handling JMS Message", ex);
+            } else {
+                m_loggerProline.warn("Invalid JMS Message type");
             }
-
-        } else {
-            m_loggerProline.warn("Invalid JMS Message type");
         }
     }
 

e1149341da92e825536f451086fe23b9e2250abc
Veronique Dupierris <veronique.dupierris@cea.fr>
2016-08-04 08:39:52 +0000

    Add QueueBrowser in JMSConnectionManager & More information in JMSMessageNotification

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/data/JMSNotificationMessage.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/data/JMSNotificationMessage.java
@@ -14,20 +14,32 @@ import java.util.Date;
 public class JMSNotificationMessage {
     
     private String m_serviceName;
+    private String m_serviceVersion;
+    private String m_serviceSource;
+    private String m_serviceInfo;
     private Date m_eventTime;
     private String m_reqJMSMsgId;
     private String m_jsonRPCMsgId;
     private String m_eventType;
     
-    public JMSNotificationMessage(String serviceName, Long  eventTimestamp, String reqJMSMsgId, String jsonRPCId, String eventType ){
+       
+    public JMSNotificationMessage(String serviceName, String serviceVersion, String serviceSource,
+            String serviceInfo, Long  eventTimestamp, String reqJMSMsgId, String jsonRPCId, String eventType ){
         m_serviceName = serviceName;
+        m_serviceVersion = serviceVersion;
+        m_serviceSource = serviceSource;
         m_reqJMSMsgId = reqJMSMsgId;
         m_jsonRPCMsgId = jsonRPCId;
+        m_serviceInfo = serviceInfo;
         m_eventType = eventType;
         m_eventTime = new Date(eventTimestamp);
         
     }
-
+    
+    public String getServiceInfo() {
+        return m_serviceInfo;
+    }
+    
     public String getServiceName() {
         return m_serviceName;
     }
@@ -47,6 +59,26 @@ public class JMSNotificationMessage {
     public String getEventType() {
         return m_eventType;
     }
+
+    public String getServiceVersion() {
+        return m_serviceVersion;
+    }
+
+    public String getServiceSource() {
+        return m_serviceSource;
+    }
     
-    
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("Message ").append(getJsonRPCMsgId()).append(" / ").append(getRequestMsgId()).append(" : {");
+        sb.append(getServiceName());
+        if(m_serviceVersion != null)
+            sb.append(" [Version ").append(m_serviceVersion).append("]");
+        
+        if(m_serviceSource != null)
+            sb.append(" From ").append(m_serviceSource);
+        
+        return sb.toString();
+    }
 }

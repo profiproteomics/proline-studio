781b9168bac5ab147fa4ef5579121c9a93338669
Andrea Kalaitzakis <andrea.kalaitzakis@cea.fr>
2016-11-18 16:20:34 +0000

    Part of the work towards the integration of mzDB-wizard in proline (forgotten class)

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/FileUploadTask.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/FileUploadTask.java
@@ -31,20 +31,35 @@ import javax.jms.TextMessage;
 import org.openide.util.Exceptions;
 
 /**
- * Upload Files for MaxQuant Result : Specific for MAxQuant by searching opnly some files of interest oin folder and create a Zip file with them
- * 
+ * Upload Files for MaxQuant Result : Specific for MAxQuant by searching opnly
+ * some files of interest oin folder and create a Zip file with them
+ *
  * @author VD225637
  */
 public class FileUploadTask extends AbstractJMSTask {
 
-    private String  m_filePath;
+    private final static int UPLOAD_GENERIC_FILE = 0;
+    private final static int UPLOAD_MZDB_FILE = 1;
+
+    private int m_action = UPLOAD_GENERIC_FILE;
+
+    private String m_destinationLabel;
+    private String m_filePath;
     private String[] m_remoteFilePath = null;
 
     public FileUploadTask(AbstractJMSCallback callback, String filePath, String[] remoteFilePath) {
         super(callback, new TaskInfo("Upload file " + filePath, true, TASK_LIST_INFO, TaskInfo.INFO_IMPORTANCE_HIGH));
-
         m_filePath = filePath;
         m_remoteFilePath = remoteFilePath;
+    }
+
+    public void initUploadGenericFile() {
+        m_action = UPLOAD_GENERIC_FILE;
+    }
+
+    public void initUploadMZDB(String destinationLabel) {
+        m_action = UPLOAD_MZDB_FILE;
+        m_destinationLabel = destinationLabel;
     }
 
     @Override
@@ -53,18 +68,32 @@ public class FileUploadTask extends AbstractJMSTask {
         InputStream in = null;
         try {
             final BytesMessage message = AccessJMSManagerThread.getAccessJMSManagerThread().getSession().createBytesMessage();
-            m_loggerProline.debug("Prepare MaxQuant Import First Step : Upload Files from " + m_filePath);
-            //Create ZIP file to upload
-            List<String> files2Zip = getZipFilesList(m_filePath);
-            File uploadFile = zipIt(m_filePath, files2Zip);
+
+            File uploadFile = null;
+
+            if (m_action == UPLOAD_GENERIC_FILE) {
+                m_loggerProline.debug("Prepare MaxQuant Import First Step : Upload Files from " + m_filePath);
+                //Create ZIP file to upload
+                List<String> files2Zip = getZipFilesList(m_filePath);
+                uploadFile = zipIt(m_filePath, files2Zip);
+            } else if (m_action == UPLOAD_MZDB_FILE) {
+                m_loggerProline.debug("Prepare to upload mzDB file " + m_filePath);
+                uploadFile = new File(m_filePath);
+            }
 
             // Upload File on server side
             message.setJMSReplyTo(m_replyQueue);
             message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_NAME_KEY, "proline/misc/FileUpload");
             message.setStringProperty("dest_file_name", uploadFile.getName());
-            addSourceToMessage(message);       
+            
+            //this needs checking!
+            if(m_action == UPLOAD_MZDB_FILE){
+                message.setStringProperty("dest_folder_path", m_destinationLabel);
+            }
+            
+            addSourceToMessage(message);
             addDescriptionToMessage(message);
-        
+
             in = new FileInputStream(uploadFile);
             BufferedInputStream inBuf = new BufferedInputStream(in);
             message.setObjectProperty(JMSConnectionManager.HORNET_Q_INPUT_STREAM_KEY, inBuf);
@@ -77,8 +106,9 @@ public class FileUploadTask extends AbstractJMSTask {
             throw new JMSException(ex.getMessage());
         } finally {
             try {
-                if(in != null)
+                if (in != null) {
                     in.close();
+                }
             } catch (IOException ex) {
                 Exceptions.printStackTrace(ex);
             }
@@ -91,37 +121,35 @@ public class FileUploadTask extends AbstractJMSTask {
         final String jsonString = textMessage.getText();
 
         final JSONRPC2Message jsonMessage = JSONRPC2Message.parse(jsonString);
-        if(jsonMessage instanceof JSONRPC2Notification) {
-            m_loggerProline.warn("JSON Notification method: " + ((JSONRPC2Notification) jsonMessage).getMethod()+" instead of JSON Response");
+        if (jsonMessage instanceof JSONRPC2Notification) {
+            m_loggerProline.warn("JSON Notification method: " + ((JSONRPC2Notification) jsonMessage).getMethod() + " instead of JSON Response");
             throw new Exception("Invalid JSONRPC2Message type");
-            
-        } else if (jsonMessage instanceof JSONRPC2Response)  {
+
+        } else if (jsonMessage instanceof JSONRPC2Response) {
             final JSONRPC2Response jsonResponse = (JSONRPC2Response) jsonMessage;
-	    m_loggerProline.debug("JSON Response Id: " + jsonResponse.getID());
-            
+            m_loggerProline.debug("JSON Response Id: " + jsonResponse.getID());
+
             final JSONRPC2Error jsonError = jsonResponse.getError();
 
-	    if (jsonError != null) {
-		m_loggerProline.error("JSON Error code {}, message : \"{}\"", jsonError.getCode(), jsonError.getMessage());
-		m_loggerProline.error("JSON Throwable", jsonError);
+            if (jsonError != null) {
+                m_loggerProline.error("JSON Error code {}, message : \"{}\"", jsonError.getCode(), jsonError.getMessage());
+                m_loggerProline.error("JSON Throwable", jsonError);
                 throw jsonError;
-	    }
-                        
-            final Object result = jsonResponse.getResult();
-            if ((result == null) || (! String.class.isInstance(result)))  {
-                m_loggerProline.error(getClass().getSimpleName() + " failed : No returned values");
-                throw new Exception("Invalid result "+result);
             }
-            
-            String returnedValues = (String) result;                    
-                    
-            m_remoteFilePath[0]  = returnedValues;                                   
+
+            final Object result = jsonResponse.getResult();
+            if ((result == null) || (!String.class.isInstance(result))) {
+                m_loggerProline.error(getClass().getSimpleName() + " failed : No returned values");
+                throw new Exception("Invalid result " + result);
+            }
+
+            String returnedValues = (String) result;
+
+            m_remoteFilePath[0] = returnedValues;
         }
-               
+
         m_currentState = JMSState.STATE_DONE;
-        
-        
-        
+
     }
 
     private static List<String> getZipFilesList(String rootFolder) {
@@ -153,7 +181,7 @@ public class FileUploadTask extends AbstractJMSTask {
     }
 
     private static File zipIt(String sourceDir, List<String> files2Zip) {
-        byte[] buffer = new byte[1024];        
+        byte[] buffer = new byte[1024];
         ZipOutputStream zos = null;
         try {
             File sourceDirFile = new File(sourceDir);

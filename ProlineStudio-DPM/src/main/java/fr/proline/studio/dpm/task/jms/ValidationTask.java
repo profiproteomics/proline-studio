929ffb4271c9a4d04afdf5e36a0be0cda62aaf38
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2016-11-18 13:18:17 +0000

    bug fix : mix between score for peptides and proteins. Adjusted Mascot e-value parameter was not taken in account

--- ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/ValidationTask.java
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/ValidationTask.java
@@ -32,6 +32,7 @@ public class ValidationTask extends AbstractJMSTask  {
     public static String PEP_LENGTH_FILTER_KEY = "PEP_SEQ_LENGTH";
     public static String PEP_LENGTH_FILTER_NAME = "Length";
     public static String MASCOT_EVAL_FILTER_KEY = "MASCOT_EVALUE";
+    public static String MASCOT_ADJUSTED_EVAL_FILTER_KEY = "MASCOT_ADJUSTED_EVALUE";
     public static String MASCOT_EVAL_FILTER_NAME = "e-Value";
     public static String MASCOT_IT_SCORE_FILTER_KEY = "SCORE_IT_P-VALUE";
     public static String MASCOT_IT_SCORE_FILTER_NAME = "Identity p-Value";
@@ -132,50 +133,56 @@ public class ValidationTask extends AbstractJMSTask  {
         // Peptide Pre-Filters
         ArrayList pepFilters = new ArrayList();
 
-        if (m_argumentsMap.containsKey(RANK_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+RANK_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", RANK_FILTER_KEY);
-            filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get(RANK_FILTER_KEY)));
+            filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get("PSM_"+RANK_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(SCORE_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+SCORE_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", SCORE_FILTER_KEY);
-            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get(SCORE_FILTER_KEY)));
+            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get("PSM_"+SCORE_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(MASCOT_EVAL_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+MASCOT_EVAL_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", MASCOT_EVAL_FILTER_KEY);
-            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get(MASCOT_EVAL_FILTER_KEY)));
+            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get("PSM_"+MASCOT_EVAL_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(PEP_LENGTH_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+MASCOT_ADJUSTED_EVAL_FILTER_KEY)) {
+                HashMap filterCfg = new HashMap();
+                filterCfg.put("parameter", MASCOT_ADJUSTED_EVAL_FILTER_KEY);
+                filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get("PSM_"+MASCOT_ADJUSTED_EVAL_FILTER_KEY)));
+                pepFilters.add(filterCfg);
+        }
+        if (m_argumentsMap.containsKey("PSM_"+PEP_LENGTH_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", PEP_LENGTH_FILTER_KEY);
-            filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get(PEP_LENGTH_FILTER_KEY)));
+            filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get("PSM_"+PEP_LENGTH_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(MASCOT_IT_SCORE_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+MASCOT_IT_SCORE_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", MASCOT_IT_SCORE_FILTER_KEY);
-            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get(MASCOT_IT_SCORE_FILTER_KEY)));
+            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get("PSM_"+MASCOT_IT_SCORE_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(MASCOT_HT_SCORE_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+MASCOT_HT_SCORE_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", MASCOT_HT_SCORE_FILTER_KEY);
-            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get(MASCOT_HT_SCORE_FILTER_KEY)));
+            filterCfg.put("threshold", Float.valueOf(m_argumentsMap.get("PSM_"+MASCOT_HT_SCORE_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(SINGLE_PSM_QUERY_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+SINGLE_PSM_QUERY_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", SINGLE_PSM_QUERY_FILTER_KEY);
             filterCfg.put("threshold", 1);
-            filterCfg.put("post_validation", Boolean.valueOf(m_argumentsMap.get(SINGLE_PSM_QUERY_FILTER_KEY)));
+            filterCfg.put("post_validation", Boolean.valueOf(m_argumentsMap.get("PSM_"+SINGLE_PSM_QUERY_FILTER_KEY)));
             pepFilters.add(filterCfg);
         }
-        if (m_argumentsMap.containsKey(SINGLE_PSM_RANK_FILTER_KEY)) {
+        if (m_argumentsMap.containsKey("PSM_"+SINGLE_PSM_RANK_FILTER_KEY)) {
             HashMap filterCfg = new HashMap();
             filterCfg.put("parameter", SINGLE_PSM_RANK_FILTER_KEY);
             filterCfg.put("threshold", 1);
@@ -203,14 +210,15 @@ public class ValidationTask extends AbstractJMSTask  {
         // Protein Pre-Filters
         ArrayList proteinFilters = new ArrayList();
 
-        for (String filterKey : FILTER_KEYS) {           
-            if (m_argumentsMap.containsKey(filterKey)) {
+        for (String filterKey : FILTER_KEYS) {   
+            String filterKeyOfInMap = "PROT_"+filterKey;
+            if (m_argumentsMap.containsKey(filterKeyOfInMap)) {
                 HashMap filterCfg = new HashMap();
                 filterCfg.put("parameter", filterKey);
-                if(filterKey.equals("SCORE")) //TODO USE ENUM
-                    filterCfg.put("threshold", Double.valueOf(m_argumentsMap.get(filterKey)));
+                if (filterKey.equals("SCORE")) //TODO USE ENUM
+                    filterCfg.put("threshold", Double.valueOf(m_argumentsMap.get(filterKeyOfInMap)));
                 else
-                    filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get(filterKey)));                
+                    filterCfg.put("threshold", Integer.valueOf(m_argumentsMap.get(filterKeyOfInMap)));                
                 proteinFilters.add(filterCfg);
             }
         }

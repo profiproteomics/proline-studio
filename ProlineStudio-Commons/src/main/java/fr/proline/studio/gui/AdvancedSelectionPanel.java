1c3a93e66d005153e080c8202b65b6208d1d13d3
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2016-12-01 10:08:23 +0000

    bug fix : when different suffixes correspond to the same columns, only keep the longest suffix

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/gui/AdvancedSelectionPanel.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/gui/AdvancedSelectionPanel.java
@@ -423,55 +423,47 @@ public class AdvancedSelectionPanel<E> extends JPanel  {
             }
             
             
-            // SUFFIX --------------
-            
-            String suffix = nohtmlValue;
-            int indexLastSpace = suffix.lastIndexOf(' ');
-            if (indexLastSpace != -1) {
-                suffix = nohtmlValue.substring(indexLastSpace, nohtmlValue.length());
-            }
-
-            if (suffix.length() > 0) {
-                Integer nb = similarColumnsNumberMap.get(suffix);
-                if (nb == null) {
-                    similarColumnsNumberMap.put(suffix, 1);
-                } else {
-                    similarColumnsNumberMap.put(suffix, nb + 1);
-                }
-
-                colorIndexStart = fullValue.indexOf("<font color='", 0);
-                colorIndexStop = fullValue.indexOf("</font>", 0);
-                if ((colorIndexStart > -1) && (colorIndexStop > colorIndexStart)) {
-                    String colorName = fullValue.substring(colorIndexStart, colorIndexStop + "</font>".length());
-                    String curColorName = similarColumnsColorsMap.get(suffix);
-                    if (curColorName != null) {
-                        if (curColorName.compareTo(colorName) != 0) {
-                            similarColumnsColorsMap.put(suffix, "");
-                        }
-                    } else {
-                        similarColumnsColorsMap.put(suffix, colorName);
-                    }
-                } else {
-                    similarColumnsColorsMap.put(suffix, "");
-                }
-
-            }
-    
-            // EXTENDED SUFFIX --------------
+            // ALL POTENTIAL SUFFIX --------------
+            int indexLastSpace = nohtmlValue.lastIndexOf(' ');
             while (indexLastSpace != -1) {
                 indexLastSpace = addSuffix(indexLastSpace, nohtmlValue, fullValue, similarColumnsNumberMap, similarColumnsColorsMap );
             }
         }
 
         // suppression des lignes à un résultat dans similarColumnsNumberMap
-        Set<String> colNamesSet = similarColumnsNumberMap.keySet();
-        String[] colNamesArray = colNamesSet.toArray(new String[colNamesSet.size()]);
-        for (int i = 0; i < colNamesArray.length; i++) {
-            String colName = colNamesArray[i];
-            Integer nb = similarColumnsNumberMap.get(colName);
-            if ((nb <= 1) || (nb>size/2)) {
-                similarColumnsNumberMap.remove(colName);
+        Set<String> suffixSet = similarColumnsNumberMap.keySet();
+        String[] suffixArray = suffixSet.toArray(new String[suffixSet.size()]);
+        for (int i = 0; i < suffixArray.length; i++) {
+            String suffixCur = suffixArray[i];
+            if (!similarColumnsNumberMap.containsKey(suffixCur)) {
+                // suffix already suppressed
+                continue;
             }
+            Integer nb = similarColumnsNumberMap.get(suffixCur);
+            
+            if (nb <= 1) {
+                similarColumnsNumberMap.remove(suffixCur);
+                continue;
+            }
+            
+            for (int j=i+1;j<suffixArray.length;j++) {
+                String suffixCur2 = suffixArray[j];
+                if (((suffixCur.length()<suffixCur2.length()) && (suffixCur2.endsWith(suffixCur))) ||
+                        ((suffixCur.length()>suffixCur2.length()) && (suffixCur.endsWith(suffixCur2)))) {
+                    // suffixCur and suffixCur2 are similar  like "vs 5%" and "5%"
+                    Integer nb2 = similarColumnsNumberMap.get(suffixCur2);
+                    if (nb == nb2) {
+                        // suffixes correspond to the same column, we remove the shortest one
+                        if (suffixCur.length()<suffixCur2.length()) {
+                            similarColumnsNumberMap.remove(suffixCur);
+                        } else {
+                            similarColumnsNumberMap.remove(suffixCur2);
+                        }
+                    }
+                    
+                }
+            }
+            
         }
         String[] groups = null;
 

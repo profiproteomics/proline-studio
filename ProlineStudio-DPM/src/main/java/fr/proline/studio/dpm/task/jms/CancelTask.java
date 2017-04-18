d4c9ce19d83a6fe58a5c0219402009ba068fe21a
Veronique Dupierris <veronique.dupierris@cea.fr>
2017-04-18 09:19:33 +0000

    [Studio] #16545 Add Cancel Tasks feature

---
+++ ProlineStudio-DPM/src/main/java/fr/proline/studio/dpm/task/jms/CancelTask.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.dpm.task.jms;
+
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Message;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Notification;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import fr.proline.studio.dam.taskinfo.TaskInfo;
+import fr.proline.studio.dpm.jms.AccessJMSManagerThread;
+import static fr.proline.studio.dpm.task.jms.AbstractJMSTask.m_loggerProline;
+import fr.proline.studio.dpm.task.util.JMSConnectionManager;
+import java.util.HashMap;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author VD225637
+ */
+public class CancelTask extends AbstractJMSTask {
+
+    private static final String m_serviceName = "proline/misc/CancelService";
+    private String m_messageId;
+
+    public CancelTask(String msgID) {
+        super(null, new TaskInfo("Send Cancel request to message " + msgID, false, TASK_LIST_INFO, TaskInfo.INFO_IMPORTANCE_LOW));
+        this.m_messageId = msgID;
+    }
+
+    @Override
+    public void taskRun() throws JMSException {
+        final JSONRPC2Request jsonRequest = new JSONRPC2Request("cancel", Integer.valueOf(m_taskInfo.getId()));
+        jsonRequest.setNamedParams(createParams());
+
+        final TextMessage message = AccessJMSManagerThread.getAccessJMSManagerThread().getSession().createTextMessage(jsonRequest.toJSONString());
+
+        /* ReplyTo = Temporary Destination Queue for Server -> Client response */
+        message.setJMSReplyTo(m_replyQueue);
+        message.setJMSCorrelationID(m_messageId);
+        message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_NAME_KEY, m_serviceName);
+//        message.setStringProperty(JMSConnectionManager.PROLINE_SERVICE_VERSION_KEY, m_version);
+        addSourceToMessage(message);
+        addDescriptionToMessage(message);
+
+        setTaskInfoRequest(message.getText());
+
+        //  Send the Message
+        m_producer.send(message);
+        m_loggerProline.info("CancelTask Message [{}] sent ", message.getJMSMessageID());
+        m_taskInfo.setJmsMessageID(message.getJMSMessageID());
+    }
+
+    private HashMap<String, Object> createParams() {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("message_id", m_messageId);
+        return params;
+    }
+
+    @Override
+    public void taskDone(final Message jmsMessage) throws Exception {
+
+        final TextMessage textMessage = (TextMessage) jmsMessage;
+        final String jsonString = textMessage.getText();
+
+        final JSONRPC2Message jsonMessage = JSONRPC2Message.parse(jsonString);
+        if (jsonMessage instanceof JSONRPC2Notification) {
+            m_loggerProline.warn("JSON Notification method: " + ((JSONRPC2Notification) jsonMessage).getMethod() + " instead of JSON Response");
+            throw new Exception("Invalid JSONRPC2Message type");
+
+        } else if (jsonMessage instanceof JSONRPC2Response) {
+
+            final JSONRPC2Response jsonResponse = (JSONRPC2Response) jsonMessage;
+            m_loggerProline.debug("JSON Response Id: " + jsonResponse.getID());
+
+            final JSONRPC2Error jsonError = jsonResponse.getError();
+
+            if (jsonError != null) {
+                m_loggerProline.error("JSON Error code {}, message : \"{}\"", jsonError.getCode(), jsonError.getMessage());
+                m_loggerProline.error("JSON Throwable", jsonError);
+                throw jsonError;
+            }
+
+            final Object result = jsonResponse.getResult();
+            if (result == null) {
+                m_loggerProline.debug("NULL result");
+                throw new Exception("NULL result ");
+            } else {
+                m_loggerProline.debug("Result :\n" + result);
+            }
+        }
+        m_currentState = AbstractJMSTask.JMSState.STATE_DONE;
+
+    }
+}

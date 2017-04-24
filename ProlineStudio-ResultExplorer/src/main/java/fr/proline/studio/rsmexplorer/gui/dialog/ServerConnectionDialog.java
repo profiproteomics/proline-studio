110cd480e1973698bd7abb45352782cdaca34c38
Andrea Kalaitzakis <andrea.kalaitzakis@cea.fr>
2017-04-24 14:47:17 +0000

    [ProlineStudio] Permanent file (server & local) trees state. Still a few bugs. (missing class)

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/ServerConnectionDialog.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/ServerConnectionDialog.java
@@ -14,8 +14,6 @@ import fr.proline.studio.rsmexplorer.TaskLogTopComponent;
 import fr.proline.studio.rsmexplorer.gui.ProjectExplorerPanel;
 import fr.proline.studio.rsmexplorer.gui.calc.DataAnalyzerPanel;
 import java.awt.Window;
-import java.io.File;
-import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 import javax.swing.*;

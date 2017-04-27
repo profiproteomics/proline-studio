8a3f417bf0c5ab33d2b306164e40f6365538ede0
Veronique Dupierris <veronique.dupierris@cea.fr>
2017-04-27 12:37:02 +0000

    [Studio] PTMSite : Workaround test for issue #16643 + added properties to store Peptide/Peptide matches information + define toString

--- ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/data/PTMSite.java
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/data/PTMSite.java
@@ -8,6 +8,7 @@ package fr.proline.studio.dam.tasks.data;
 import fr.proline.core.orm.msi.dto.DInfoPTM;
 import fr.proline.core.orm.msi.dto.DPeptideMatch;
 import fr.proline.core.orm.msi.dto.DProteinMatch;
+import fr.proline.core.orm.ps.PtmSpecificity.PtmLocation;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,17 +20,47 @@ import java.util.Map;
 public class PTMSite {
     
     public Long proteinMatchId;
-    public Long definitionId;
+    public Long definitionId; //PTM Definition ID
     public Integer seqPosition;
     public Long bestPeptideMatchId;
+    // List of peptides ID matching this PTMSite by the position of this PTM 
+    // in peptide Seq
     public Map<Integer, List<Long>> peptideIdsByPtmPosition;
+    
+    //List of all PeptidesInstances Ids (in leaf RSM)
     public Long[] peptideInstanceIds;
+        
+    //List of PepidesInstances which don't match this PTM but with same mass
     public Long[] isomericPeptideInstanceIds;
 
-    private DProteinMatch proteinMatch;
-    private DPeptideMatch bestPeptideMatch;
+    private DProteinMatch proteinMatch; //PTMSite's Protein Match
+    private DPeptideMatch bestPeptideMatch; 
     private DInfoPTM ptmSpecificity;
     private Map<Long, Integer> reversePtmPositionByPeptideId;
+    
+    private Map<Long, List<DPeptideMatch>> m_peptideMatchesByPepInstanceID;
+    private boolean m_peptideMatchesLoaded = false;
+    
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        if(proteinMatch != null)
+            builder.append(proteinMatch.getAccession()).append("-");
+        else 
+            builder.append("Protein Match ID").append(proteinMatchId).append("-");
+        if(ptmSpecificity != null) {
+            builder.append(ptmSpecificity.getPtmShortName()).append("(");
+            if (ptmSpecificity.getLocationSpecificity().equals("Anywhere")) {
+                builder.append(ptmSpecificity.getRresidueAASpecificity());
+            } else {
+                builder.append(ptmSpecificity.getLocationSpecificity());
+            }       
+            builder.append(")-");
+        } else 
+            builder.append("PTM ID").append(definitionId).append("-");
+         builder.append(seqPosition);
+        return builder.toString();
+    }
     
     public void setProteinMatch(DProteinMatch proteinMatch) {
         this.proteinMatch = proteinMatch;
@@ -42,6 +73,11 @@ public class PTMSite {
     public void setPTMSpcificity(DInfoPTM specificity) {
         this.ptmSpecificity = specificity;
     }
+    
+    public void setPepInstancePepMatchesMap(Map<Long, List<DPeptideMatch>> pepMatchesByPepInstanceID) {
+        this.m_peptideMatchesByPepInstanceID = pepMatchesByPepInstanceID;
+        m_peptideMatchesLoaded = true;
+    }
 
     public DProteinMatch getProteinMatch() {
         return proteinMatch;
@@ -51,6 +87,10 @@ public class PTMSite {
         return bestPeptideMatch;
     }
 
+    public Map<Long, List<DPeptideMatch>> getPepInstancePepMatchesMap() {
+        return m_peptideMatchesByPepInstanceID;
+    }
+     
     public DInfoPTM getPtmSpecificity() {
         return ptmSpecificity;
     }
@@ -59,6 +99,14 @@ public class PTMSite {
         return getReversePtmPositionByPeptideId().keySet().size();
     }
 
+    public boolean isAllPeptideMatchesLoaded(){
+        return m_peptideMatchesLoaded;
+    }
+    
+    public void setAllPeptideMatchesLoaded(boolean arePepMatchesLoaded){
+        m_peptideMatchesLoaded = arePepMatchesLoaded;
+    }
+    
     public Map<Long, Integer> getReversePtmPositionByPeptideId() {
         if (reversePtmPositionByPeptideId == null) {
             reversePtmPositionByPeptideId = new HashMap<>();
@@ -84,6 +132,32 @@ public class PTMSite {
         return builder.toString();
     }
     
+    // VDS Workaround test for issue #16643   
+    public String toOtherReadablePtmString(Long peptideId) {
+        StringBuilder builder = new StringBuilder(ptmSpecificity.getPtmShortName());
+        builder.append(" (");
+        PtmLocation ptmLoc = PtmLocation.withName(ptmSpecificity.getLocationSpecificity());
+        if (ptmLoc.equals(PtmLocation.ANYWHERE)) {
+            builder.append(ptmSpecificity.getRresidueAASpecificity()).append(getPtmPeptidePosition(peptideId)).append(')');
+        } else {
+            switch(ptmLoc) {
+                case ANY_C_TERM :
+                    builder.append(PtmLocation.PROT_C_TERM.toString()).append(')');
+                    break;                    
+                case PROT_C_TERM :
+                    builder.append(PtmLocation.ANY_C_TERM.toString()).append(')');
+                    break;
+                case ANY_N_TERM :
+                    builder.append(PtmLocation.PROT_N_TERM.toString()).append(')');
+                    break;
+                case PROT_N_TERM :
+                    builder.append(PtmLocation.ANY_N_TERM.toString()).append(')');                    
+                    break;                    
+            }
+        }
+        return builder.toString();
+    } 
+            
     public Integer getPtmPeptidePosition(Long peptideId) {
         return getReversePtmPositionByPeptideId().get(peptideId);
     }

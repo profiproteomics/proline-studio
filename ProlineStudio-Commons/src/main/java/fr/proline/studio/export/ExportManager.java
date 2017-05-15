956f4bdcb29592a45f7587ea0fa6ec1a361a3bca
Jean-Philippe Menetrey <jean-philippe.menetrey@cea.fr>
2017-05-15 09:07:20 +0000

    moved last code which must be executed in AWT Thread

--- ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportManager.java
+++ ProlineStudio-Commons/src/main/java/fr/proline/studio/export/ExportManager.java
@@ -2,13 +2,8 @@ package fr.proline.studio.export;
 
 import fr.proline.studio.comparedata.CompareDataInterface;
 import fr.proline.studio.gui.DefaultDialog;
-import fr.proline.studio.table.GlobalTableModelInterface;
-import java.awt.Component;
 import java.util.ArrayList;
-import javax.swing.AbstractButton;
-import javax.swing.JLabel;
 import javax.swing.JTable;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
 /**
@@ -30,7 +25,27 @@ public class ExportManager {
     }
 
     public ExportWorker getTask(ExporterInterface exporter, String name, String filePath, boolean decorated) {
-        return new ExportWorker(exporter, name, filePath, decorated);
+        
+        // get cols from the table
+        int nbCol = m_table.getColumnCount();
+        int[] colsInModel = new int[nbCol];
+        for (int i = 0; i < nbCol; i++) {
+            int colInModel = m_table.convertColumnIndexToModel(i);
+            colsInModel[i] = colInModel;
+        }
+
+        // get rows from the table
+        int nbRow = m_table.getRowCount();
+        int[] rowsInModel = new int[nbRow];
+        for (int i = 0; i < nbRow; i++) {
+            int rowInModel = m_table.convertRowIndexToModel(i);
+            rowsInModel[i] = rowInModel;
+        }
+
+        // retrieve model
+        TableModel model = m_table.getModel();
+        
+        return new ExportWorker(exporter, name, filePath, decorated, model, colsInModel, rowsInModel);
     }
     
     public void setException(Exception e) {
@@ -41,41 +56,27 @@ public class ExportManager {
         return m_exportException;
     }
 
-    private String componentToText(Component c) {
-        String text = "";
-
-        /*if (c instanceof ExportTextInterface) {
-            text = ((ExportTextInterface) c).getExportText();
-        } else*/ if (c instanceof JLabel) {
-            text = ((JLabel) c).getText();
-        } else if (c instanceof AbstractButton) {
-            text = ((AbstractButton) c).getText();
-        }
-
-        return text;
-    }
-    
-    private ArrayList<ExportFontData> componentToSubStringFonts(Component c){
-        /*if(c instanceof ExportTextInterface){
-            return ((ExportTextInterface) c).getExportFonts();
-        }else{*/
-            return null;
-        //}
-    }
-    
-
     public class ExportWorker extends DefaultDialog.ProgressTask {
 
         private final ExporterInterface m_exporter;
         private final String m_name;
         private final String m_filePath;
         private final boolean m_decorated;
+        
+        private final TableModel m_model;
+        private final int[] m_colsInModel;
+        private final int[] m_rowsInModel;
+        
+        
 
-        public ExportWorker(ExporterInterface exporter, String name, String filePath, boolean decorated) {
+        public ExportWorker(ExporterInterface exporter, String name, String filePath, boolean decorated, TableModel model, int[] colsInModel, int[] rowsInModel) {
             m_exporter = exporter;
             m_name = name;
             m_filePath = filePath;
             m_decorated = decorated;
+            m_model = model;
+            m_colsInModel = colsInModel;
+            m_rowsInModel = rowsInModel;
         }
 
         @Override
@@ -86,42 +87,24 @@ public class ExportManager {
                 m_exporter.startSheet(m_name);
 
                 m_exporter.setDecorated(m_decorated);
+ 
 
-                // --- JPM.TODO : following lines should be called in AWT
+                ExportModelInterface exportInterface = (m_model instanceof ExportModelInterface) ? (ExportModelInterface) m_model : null;
                 
-                // get cols from the table
-                int nbCol = m_table.getColumnCount();
-                int[] colsInModel = new int[nbCol];
-                for (int i=0;i<nbCol;i++) {
-                    int colInModel = m_table.convertColumnIndexToModel(i);
-                    colsInModel[i] = colInModel;
-                }
-                
-                // get rows from the table
-                int nbRow = m_table.getRowCount();
-                int[] rowsInModel = new int[nbRow];
-                for (int i = 0; i < nbRow; i++) {
-                    int rowInModel = m_table.convertRowIndexToModel(i);
-                    rowsInModel[i] = rowInModel;
-                }
-                
-                // --- JPM.TODO.END 
-                
-                // retrieve model
-                TableModel model = m_table.getModel();
-                ExportModelInterface exportInterface = (model instanceof ExportModelInterface) ? (ExportModelInterface) model : null;
+                int nbCol = m_colsInModel.length;
+                int nbRow = m_rowsInModel.length;
                 
                 // headers
                 if (exportInterface != null) {
                     m_exporter.startRow();
                     for (int j = 0; j < nbCol; j++) {
-                        String colName = exportInterface.getExportColumnName(colsInModel[j]);
+                        String colName = exportInterface.getExportColumnName(m_colsInModel[j]);
                         m_exporter.addCell(colName, null);
                     }
                 } else {
                     m_exporter.startRow();
                     for (int j = 0; j < nbCol; j++) {
-                        String colName = model.getColumnName(colsInModel[j]);
+                        String colName = m_model.getColumnName(m_colsInModel[j]);
                         m_exporter.addCell(colName, null);
                     }
                 }
@@ -132,15 +115,15 @@ public class ExportManager {
                 int lastPercentage = 0;
                 int percentage;
                 for (int j = 0; j < nbRow; j++) {
-                    int row = rowsInModel[j];
+                    int row = m_rowsInModel[j];
                     if (exportInterface != null) {
                         m_exporter.startRow();
                         for (int i = 0; i < nbCol; i++) {
-                            int col = colsInModel[i];
+                            int col = m_colsInModel[i];
                             String text = exportInterface.getExportRowCell(row, col);
                              ArrayList<ExportFontData> stringFonts = null;
                             if (text == null) {
-                                Object o = ((CompareDataInterface) model).getDataValueAt(row, col);
+                                Object o = ((CompareDataInterface) m_model).getDataValueAt(row, col);
                                 if (o != null) {
                                     text = o.toString();
                                 }
@@ -149,12 +132,12 @@ public class ExportManager {
                             }
                             m_exporter.addCell(text, stringFonts);
                         }
-                    } else if (model instanceof CompareDataInterface) {
+                    } else if (m_model instanceof CompareDataInterface) {
                         m_exporter.startRow();
                         for (int i = 0; i < nbCol; i++) {
-                            int col = colsInModel[i];
+                            int col = m_colsInModel[i];
                             String text = null;
-                            Object o = ((CompareDataInterface) model).getDataValueAt(row, col);
+                            Object o = ((CompareDataInterface) m_model).getDataValueAt(row, col);
                             if (o != null) {
                                 text = o.toString();
                             }
@@ -164,9 +147,9 @@ public class ExportManager {
                     } else {
                         m_exporter.startRow();
                         for (int i = 0; i < nbCol; i++) {
-                            int col = colsInModel[i];
+                            int col = m_colsInModel[i];
                             String text = null;
-                            Object o = model.getValueAt(row, col);
+                            Object o = m_model.getValueAt(row, col);
                             if (o != null) {
                                 text = o.toString();
                             }
@@ -174,30 +157,7 @@ public class ExportManager {
                             m_exporter.addCell(text, null);
                         }
                     }
-                    
-                    /*if (rowTextInterface) {
-                        m_exporter.startRow();
-                        for (int col = 0; col < nbCol; col++) {
-                            String text = ((ExportModelInterface) m_table).getExportRowCell(row, col);
-                            if (text == null) {
-                                TableCellRenderer renderer = m_table.getCellRenderer(row, col);
-                                Component c = m_table.prepareRenderer(renderer, row, col);
-                                text = componentToText(c);
-                            }
-                            TableCellRenderer renderer = m_table.getCellRenderer(row, col);
-                            Component c = m_table.prepareRenderer(renderer, row, col);
-                            m_exporter.addCell(text, componentToSubStringFonts(c));
-                        }
-                    } else {
-                        m_exporter.startRow();
-                        for (int col = 0; col < nbCol; col++) {
-                            TableCellRenderer renderer = m_table.getCellRenderer(row, col);
-                            Component c = m_table.prepareRenderer(renderer, row, col);
-                            String text = componentToText(c);
 
-                            m_exporter.addCell(text, componentToSubStringFonts(c));
-                        }
-                    }*/
                     percentage = (int) Math.round((((double) (j + 1)) / nbRow) * 100);
                     if (percentage > lastPercentage) {
                         setProgress(percentage);

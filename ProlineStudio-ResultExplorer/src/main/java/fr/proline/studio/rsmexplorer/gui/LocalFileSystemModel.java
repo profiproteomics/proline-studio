cd0ad093f1f9513cf7bad60439c152c641e7d335
Andrea Kalaitzakis <andrea.kalaitzakis@cea.fr>
2017-06-12 15:02:53 +0000

    [ProlineStudio] Local File System View filtered.

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/LocalFileSystemModel.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/LocalFileSystemModel.java
@@ -7,6 +7,7 @@ package fr.proline.studio.rsmexplorer.gui;
 
 import fr.proline.studio.msfiles.LocalFileSystemFile;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.HashSet;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
@@ -44,12 +45,12 @@ public class LocalFileSystemModel implements TreeModel {
 
         File directory = (File) parentNode.getUserObject();
 
-        String[] directoryMembers = directory.list();
+        String[] directoryMembers = directory.list(new Filter());
 
         DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(new LocalFileSystemFile(directory, directoryMembers[index]));
-        
+
         parentNode.add(childNode);
-        
+
         return childNode;
     }
 
@@ -63,7 +64,8 @@ public class LocalFileSystemModel implements TreeModel {
 
         File fileSystemMember = (File) parentNode.getUserObject();
         if (fileSystemMember.isDirectory()) {
-            File[] directoryMembers = fileSystemMember.listFiles();
+            File[] directoryMembers = fileSystemMember.listFiles(new Filter());
+
             if (directoryMembers == null) {
                 return 0;
             }
@@ -143,6 +145,20 @@ public class LocalFileSystemModel implements TreeModel {
         for (TreeModelListener listener : listeners) {
             listener.treeStructureChanged(e);
         }
+    }
+
+    public class Filter implements FilenameFilter {
+
+        @Override
+
+        public boolean accept(File directory, String fileName) {
+
+            File f = new File(directory, fileName);
+
+            return f.isDirectory() || fileName.toLowerCase().endsWith(".raw") || fileName.toLowerCase().endsWith(".mzdb") || fileName.toLowerCase().endsWith(".wiff");
+
+        }
+
     }
 
 }

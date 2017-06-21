c26396c40346ee8096beb0df3e29d0e24acaa869
Andrea Kalaitzakis <andrea.kalaitzakis@cea.fr>
2017-06-21 12:55:11 +0000

    [ProlineStudio] #16908

--- ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/MzdbFilesTopComponent.java
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/MzdbFilesTopComponent.java
@@ -1,8 +1,9 @@
 package fr.proline.studio.rsmexplorer;
 
+import fr.proline.studio.dpm.ServerConnectionManager;
 import fr.proline.studio.msfiles.MsFilesExplorer;
+import fr.proline.studio.rsmexplorer.gui.TreeStateUtil;
 import java.awt.BorderLayout;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import org.netbeans.api.settings.ConvertAsProperties;
@@ -32,10 +33,10 @@ import org.openide.util.NbBundle.Messages;
     "HINT_MzdbFilesTopComponent=Ms Files"
 })
 public final class MzdbFilesTopComponent extends TopComponent {
-    
+
     private static MsFilesExplorer m_explorer;
     private JTabbedPane m_tabbedPane;
-    
+
     public MzdbFilesTopComponent() {
         initComponents();
         setName(Bundle.CTL_MzdbFilesTopComponent());
@@ -47,21 +48,24 @@ public final class MzdbFilesTopComponent extends TopComponent {
 
         setLayout(new BorderLayout());
         m_tabbedPane = new JTabbedPane();
-        
+
         m_explorer = new MsFilesExplorer();
         m_tabbedPane.add("Explorer", m_explorer);
-        
-        m_tabbedPane.add("Working Sets", new JPanel());
-        
+
+        //m_tabbedPane.add("Working Sets", new JPanel());
         add(m_tabbedPane, BorderLayout.CENTER);
-        
+
+        if (!ServerConnectionManager.getServerConnectionManager().isNotConnected()) {
+                m_explorer.getTreeFileChooserPanel().initTree();
+                m_explorer.getTreeFileChooserPanel().restoreTree(TreeStateUtil.TreeType.SERVER);
+        }
 
     }
-    
-    public static MsFilesExplorer getExplorer(){
+
+    public static MsFilesExplorer getExplorer() {
         return m_explorer;
     }
-    
+
     @Override
     public void componentOpened() {
 

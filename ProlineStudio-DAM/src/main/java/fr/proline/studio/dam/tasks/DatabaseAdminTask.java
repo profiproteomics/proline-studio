b0bc27d95a45f7d5edb28495540e890f518b4364
Veronique Dupierris <veronique.dupierris@cea.fr>
2018-10-04 13:08:05 +0000

    [Studio] Merge from branch RemPS

--- ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/DatabaseAdminTask.java
+++ ProlineStudio-DAM/src/main/java/fr/proline/studio/dam/tasks/DatabaseAdminTask.java
@@ -1,11 +1,14 @@
 package fr.proline.studio.dam.tasks;
 
+import fr.proline.core.orm.uds.FragmentationRule;
+import fr.proline.core.orm.uds.FragmentationRuleSet;
 import fr.proline.core.orm.uds.PeaklistSoftware;
 import fr.proline.core.orm.uds.SpectrumTitleParsingRule;
 import fr.proline.core.orm.util.DStoreCustomPoolConnectorFactory;
 import fr.proline.studio.dam.DatabaseDataManager;
 import fr.proline.studio.dam.taskinfo.TaskError;
 import fr.proline.studio.dam.taskinfo.TaskInfo;
+import java.util.HashSet;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -17,13 +20,20 @@ import javax.persistence.TypedQuery;
 public class DatabaseAdminTask extends AbstractDatabaseTask {
 
     private long m_peaklistSoftwareId = -1;
-    private String m_name;
+    private String m_name; //For Peaklist Software & Fragmentation Rule Sets
+    
+    //Variables used for Peaklist Software
     private String m_version;
     private SpectrumTitleParsingRule m_spectrumTitleParsingRule = null;
 
+    //Variables used for Fragmentation Rule Sets
+    private List<FragmentationRule> m_fragmentationRules = null;
+    // Task attributes
     private int m_action;
     private final static int ADD_PEAKLIST_SOFTWARE = 0;
     private final static int MODIFY_PEAKLIST_SOFTWARE = 1;
+    private final static int ADD_FRAGMENTATION_RULE_SET = 2;
+//    private final static int MODIFY_FRAGMENTATION_RULE_SET = 3;
 
     public DatabaseAdminTask(AbstractDatabaseCallback callback) {
         super(callback, null);
@@ -51,6 +61,13 @@ public class DatabaseAdminTask extends AbstractDatabaseTask {
         m_version = version;
         m_action = MODIFY_PEAKLIST_SOFTWARE;
     }
+    
+    public void initAddFragmentationRuleSet(String name, List<FragmentationRule> fragmentationRules) {
+        setTaskInfo(new TaskInfo(" Add Fragmentation Rule Set " + name, false, TASK_LIST_INFO, TaskInfo.INFO_IMPORTANCE_HIGH));
+        m_name = name;
+        m_fragmentationRules = fragmentationRules;
+        m_action = ADD_FRAGMENTATION_RULE_SET;
+    }
 
     @Override
     public boolean needToFetch() {
@@ -64,8 +81,40 @@ public class DatabaseAdminTask extends AbstractDatabaseTask {
                 return addPeaklistSoftware();
             case MODIFY_PEAKLIST_SOFTWARE:
                 return modifyPeaklistSoftware();
+            case ADD_FRAGMENTATION_RULE_SET:
+                return addFragmentationRuleSet();
         }
         return false;
+    }
+    
+    public boolean addFragmentationRuleSet() {
+        EntityManager entityManagerUDS = DStoreCustomPoolConnectorFactory.getInstance().getUdsDbConnector().createEntityManager();
+        
+        try {
+            entityManagerUDS.getTransaction().begin();
+            
+            FragmentationRuleSet frs = new FragmentationRuleSet();
+            frs.setName(m_name);
+            frs.setFragmentationRules(new HashSet(m_fragmentationRules));
+               
+            entityManagerUDS.persist(frs);
+            reloadFragmentationRuleSets(entityManagerUDS);
+            
+            entityManagerUDS.getTransaction().commit();
+            
+        } catch (Exception e) {
+            m_logger.error(getClass().getSimpleName() + " failed", e);
+            m_taskError = new TaskError(e);
+            try {
+                entityManagerUDS.getTransaction().rollback();
+            } catch (Exception rollbackException) {
+                m_logger.error(getClass().getSimpleName() + " failed : potential network problem", rollbackException);
+            }
+            return false;
+        } finally {
+            entityManagerUDS.close();        
+        }   
+        return true;
     }
 
     public boolean addPeaklistSoftware() {
@@ -147,6 +196,17 @@ public class DatabaseAdminTask extends AbstractDatabaseTask {
         List<PeaklistSoftware> peaklistSoftwareList = peaklistSoftwareQuery.getResultList();
         DatabaseDataManager.getDatabaseDataManager().setPeaklistSofwares(peaklistSoftwareList);
 
+    }
+    
+      private void reloadFragmentationRuleSets(EntityManager entityManagerUDS) {
+        // Load All peaklist softwares
+
+        TypedQuery<FragmentationRuleSet> fragRuleSetQuery = entityManagerUDS.createQuery("SELECT frs FROM fr.proline.core.orm.uds.FragmentationRuleSet frs ORDER BY frs.name ASC", FragmentationRuleSet.class);
+        List<FragmentationRuleSet> frsList = fragRuleSetQuery.getResultList();
+         frsList.stream().forEach(frs -> { 
+                frs.getFragmentationRules().stream().forEach(fr -> fr.getDescription()); // init lazy collection
+            });
+        DatabaseDataManager.getDatabaseDataManager().setFragmentationRuleSets(frsList);
     }
 
 }

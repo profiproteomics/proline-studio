45137df0dbb496ba07c11dedd400517217be6f24
Christophe Bruley <christophe.bruley@cea.fr>
2018-12-18 09:20:32 +0000

    [Studio] - Display PTMs modified to use PTM objects loaded from the db

---
+++ ProlineStudio-ResultExplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/ptm/PTMMark.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.proline.studio.rsmexplorer.gui.ptm;
+
+import fr.proline.core.orm.msi.dto.DInfoPTM;
+import fr.proline.core.orm.msi.dto.DPeptidePTM;
+
+/**
+ *
+ * @author CB205360
+ */
+public class PTMMark {
+    
+    private DPeptidePTM m_peptidePtm;
+    private int m_proteinLocation;
+
+    public PTMMark(DPeptidePTM m_peptidePtm, int m_proteinLocation) {
+        this.m_peptidePtm = m_peptidePtm;
+        this.m_proteinLocation = m_proteinLocation;
+    }
+
+    public DPeptidePTM getPeptidePtm() {
+        return m_peptidePtm;
+    }
+
+    public int getProteinLocation() {
+        return m_proteinLocation;
+    }
+
+    public char getPtmSymbol() {
+        DInfoPTM ptm = DInfoPTM.getInfoPTMMap().get(m_peptidePtm.getIdPtmSpecificity());
+        return ptm.getPtmShortName().toUpperCase().charAt(0);
+    }
+
+    public Long getPtmSpecificityId() {
+        return m_peptidePtm.getIdPtmSpecificity();
+    }
+    
+    
+}

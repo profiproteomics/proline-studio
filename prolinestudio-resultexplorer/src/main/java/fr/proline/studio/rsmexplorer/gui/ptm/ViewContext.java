94d4144d2d3f2b62de57cf0738cfeff1d475e147
Veronique Dupierris <veronique.dupierris@cea.fr>
2019-05-06 10:25:07 +0000

    [Studio]   keep attribute naming rules : m_xxx instead if _xxx

--- prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/ptm/ViewContext.java
+++ prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/ptm/ViewContext.java
@@ -2,28 +2,28 @@ package fr.proline.studio.rsmexplorer.gui.ptm;
 
 public class ViewContext {
 
-  private int _ajustedLocation;
-  private int _areaWidth;
+  private int m_ajustedLocation;
+  private int m_areaWidth;
 
   public ViewContext() {
 
   }
 
   public int getAreaWidth() {
-    return _areaWidth;
+    return m_areaWidth;
   }
 
   public ViewContext setAreaWidth(int areaWidth) {
-    this._areaWidth = areaWidth;
+    this.m_areaWidth = areaWidth;
     return this;
   }
 
   public int getAjustedLocation() {
-    return _ajustedLocation;
+    return m_ajustedLocation;
   }
 
   public ViewContext setAjustedLocation(int ajustedLocation) {
-    this._ajustedLocation = ajustedLocation;
+    this.m_ajustedLocation = ajustedLocation;
     return this;
   }
 }

5c86a81b5c0ceefea9b0b627f4c271d159f86e3d
Veronique Dupierris <veronique.dupierris@cea.fr>
2019-06-07 07:25:39 +0000

    [Studio]  Organise View PTM  actions

---
+++ prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/actions/identification/DisplayPTMClustersAction.java
@@ -0,0 +1,122 @@
+package fr.proline.studio.rsmexplorer.actions.identification;
+
+import fr.proline.core.orm.msi.ResultSummary;
+import fr.proline.core.orm.uds.dto.DDataset;
+import fr.proline.studio.dam.AccessDatabaseThread;
+import fr.proline.studio.dam.data.DataSetData;
+import fr.proline.studio.dam.tasks.AbstractDatabaseCallback;
+import fr.proline.studio.dam.tasks.DatabaseDataSetTask;
+import fr.proline.studio.dam.tasks.SubTask;
+import fr.proline.studio.dam.tasks.data.ptm.PTMDataset;
+import fr.proline.studio.pattern.WindowBox;
+import fr.proline.studio.pattern.WindowBoxFactory;
+import fr.proline.studio.rsmexplorer.DataBoxViewerTopComponent;
+import fr.proline.studio.rsmexplorer.tree.AbstractNode;
+import fr.proline.studio.rsmexplorer.tree.AbstractTree;
+import fr.proline.studio.rsmexplorer.tree.DataSetNode;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author JM235353
+ */
+public class DisplayPTMClustersAction  extends AbstractRSMAction {
+
+    public DisplayPTMClustersAction(AbstractTree tree) {
+       super(NbBundle.getMessage(DisplayPTMSitesAction.class, "CTL_PtmClusterProtein"), tree);
+    }
+    
+    @Override
+    public void actionPerformed(AbstractNode[] selectedNodes, int x, int y) {
+
+        int nbNodes = selectedNodes.length;
+        for (int i = 0; i < nbNodes; i++) {
+            DataSetNode dataSetNode = (DataSetNode) selectedNodes[i];
+
+            actionImpl(dataSetNode);
+        }
+
+    }
+    
+    private void actionImpl(DataSetNode dataSetNode) {
+        
+        final DDataset dataSet = ((DataSetData) dataSetNode.getData()).getDataset();
+        
+        if (!dataSetNode.hasResultSummary()) {
+            return; // should not happen
+        }
+        
+        ResultSummary rsm = dataSetNode.getResultSummary();
+        if (rsm != null) {
+
+            // prepare window box
+            WindowBox wbox = WindowBoxFactory.getPTMDataWindowBox(dataSet.getName());
+            wbox.setEntryData(dataSet.getProject().getId(), new PTMDataset(dataSet));
+            
+            // open a window to display the window box
+            DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
+            win.open();
+            win.requestActive();
+        } else {
+
+            
+            // we have to load the result set
+            AbstractDatabaseCallback callback = new AbstractDatabaseCallback() {
+
+                @Override
+                public boolean mustBeCalledInAWT() {
+                    return true;
+                }
+
+                @Override
+                public void run(boolean success, long taskId, SubTask subTask, boolean finished) {
+                    
+                    WindowBox wbox = WindowBoxFactory.getPTMDataWindowBox(dataSet.getName());
+                    // open a window to display the window box
+                    DataBoxViewerTopComponent win = new DataBoxViewerTopComponent(wbox);
+                    win.open();
+                    win.requestActive();
+                    
+                    // prepare window box
+                    wbox.setEntryData(dataSet.getProject().getId(), new PTMDataset(dataSet));
+                }
+            };
+
+
+            // ask asynchronous loading of data
+            DatabaseDataSetTask task = new DatabaseDataSetTask(callback);
+            task.initLoadRsetAndRsm(dataSet);
+            AccessDatabaseThread.getAccessDatabaseThread().addTask(task);
+        }
+    }
+    
+
+    @Override
+    public void updateEnabled(AbstractNode[] selectedNodes) {
+        int nbSelectedNodes = selectedNodes.length;
+        
+
+        if (nbSelectedNodes <0) {
+            setEnabled(false);
+            return;
+        }
+        
+        for (int i=0;i<nbSelectedNodes;i++) {
+            AbstractNode node = selectedNodes[i];
+            if (node.getType() != AbstractNode.NodeTypes.DATA_SET && node.getType() != AbstractNode.NodeTypes.BIOLOGICAL_SAMPLE_ANALYSIS) {
+                setEnabled(false);
+                return;
+            }
+
+            DataSetNode dataSetNode = (DataSetNode) node;
+            if (! dataSetNode.hasResultSummary()) {
+                setEnabled(false);
+                return;
+            }
+        }
+
+        
+        setEnabled(true);
+    }
+    
+}

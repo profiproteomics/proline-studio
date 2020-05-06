48b8380ba712590009ba49e5b26c04ab1cc36584
karineXUE <karine.xue@cea.fr>
2020-05-06 14:11:34 +0200

    [studio] #21053 use multiple icons in StatusRender & ModifyStatus Dialog

--- prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/XicStatusRenderer.java
+++ prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/renderer/XicStatusRenderer.java
@@ -10,6 +10,11 @@ import fr.proline.studio.utils.IconManager;
 import java.util.HashMap;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
@@ -34,7 +39,6 @@ public class XicStatusRenderer extends DefaultTableCellRenderer {
             this._intValue = value;
             this._description = description;
         }
-        
 
         static {
             for (SelectLevel status : SelectLevel.values()) {
@@ -52,11 +56,18 @@ public class XicStatusRenderer extends DefaultTableCellRenderer {
 
         public static SelectLevel valueOf(int status) {
             return (SelectLevel) map.get(status);
-        }     
+        }
 
     }
 
-
+    private JPanel getRenderer(ImageIcon[] iconArray) {
+        JPanel result = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        for (ImageIcon icon : iconArray) {
+            JLabel label = new JLabel(icon);
+            result.add(label);
+        }
+        return result;
+    }
 
     public static String getPepIonStatusText(PepIonStatus status) {
         switch (status) {
@@ -78,58 +89,68 @@ public class XicStatusRenderer extends DefaultTableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Object status = value;
+        ImageIcon yesIcon = IconManager.getIcon(IconManager.IconType.TICK_SMALL);
+        ImageIcon noIcon = IconManager.getIcon(IconManager.IconType.CROSS_SMALL16);
+        ImageIcon manualIcon = new ImageIcon(IconManager.getIcon(IconManager.IconType.NAVIGATE).getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT));
+        ImageIcon unKnowIcon = IconManager.getIcon(IconManager.IconType.BOOK_QUESTION);
+        ImageIcon usedIcon = IconManager.getIcon(IconManager.IconType.PIN);
+        JPanel renderer = null;
         if (status instanceof SelectLevel) {
             SelectLevel statusValue = (SelectLevel) status;
             switch (statusValue) {
-                case DESELECTED_MANUAL: //0
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.CROSS_SMALL16));
+                case DESELECTED_MANUAL: //0 
+                    renderer = getRenderer(new ImageIcon[]{noIcon, manualIcon});
                     break;
                 case DESELECTED_AUTO: //1
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.CROSS_SMALL16));
+                    renderer = getRenderer(new ImageIcon[]{noIcon});
                     break;
                 case SELECTED_AUTO: //2
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
+                    renderer = getRenderer(new ImageIcon[]{yesIcon});
                     break;
                 case SELECTED_MANUAL: //3
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
+                    renderer = getRenderer(new ImageIcon[]{yesIcon, manualIcon});
                     break;
                 default:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TEST));
+                    renderer = getRenderer(new ImageIcon[]{unKnowIcon});
 
             }
-            this.setToolTipText(statusValue.getDescription());
+            renderer.setToolTipText(statusValue.getDescription());
 
         } else if (status instanceof PepIonStatus) {
             switch ((PepIonStatus) status) {
-                case UNKNOWN:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TEST));
-                    break;
+
                 case INVALIDATED:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.CROSS_SMALL16));
+                    renderer = getRenderer(new ImageIcon[]{noIcon});
+                    //this.setIcon(IconManager.getIcon(IconManager.IconType.CROSS_SMALL16));
                     break;
                 case VALIDATED:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
+                    renderer = getRenderer(new ImageIcon[]{yesIcon, unKnowIcon});
+                    //this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
                     break;
                 case USED_VALIDATED:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
+                    renderer = getRenderer(new ImageIcon[]{yesIcon, usedIcon});
+                    //this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
                     break;
                 case UNUSED_VALIDATED:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
+                    renderer = getRenderer(new ImageIcon[]{yesIcon});
+                    //this.setIcon(IconManager.getIcon(IconManager.IconType.TICK_SMALL));
                     break;
+                case UNKNOWN:
                 default:
-                    this.setIcon(IconManager.getIcon(IconManager.IconType.TEST));
+                    renderer = getRenderer(new ImageIcon[]{unKnowIcon});
+                //this.setIcon(IconManager.getIcon(IconManager.IconType.TEST));
             }
-            this.setToolTipText(getPepIonStatusText((PepIonStatus) status));
+            renderer.setToolTipText(getPepIonStatusText((PepIonStatus) status));
         }
 
         if (isSelected) {
-            this.setBackground(javax.swing.UIManager.getDefaults().getColor("Table.selectionBackground"));
-            this.setForeground(Color.WHITE);
+            renderer.setBackground(javax.swing.UIManager.getDefaults().getColor("Table.selectionBackground"));
+            renderer.setForeground(Color.WHITE);
         } else {
-            this.setBackground(javax.swing.UIManager.getDefaults().getColor("Table.background"));
-            this.setForeground(Color.BLACK);
+            renderer.setBackground(javax.swing.UIManager.getDefaults().getColor("Table.background"));
+            renderer.setForeground(Color.BLACK);
         }
 
-        return this;
+        return renderer;
     }
 }

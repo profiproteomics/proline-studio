0491b811ecd34b8f6181bc87e1fec3ef458ce3b2
karineXUE <karine.xue@cea.fr>
2020-06-18 09:51:07 +0200

    add HelpHeader with text and size

--- prolinestudio-commons/src/main/java/fr/proline/studio/gui/HelpHeaderPanel.java
+++ prolinestudio-commons/src/main/java/fr/proline/studio/gui/HelpHeaderPanel.java
@@ -24,7 +24,6 @@ import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 
 /**
@@ -75,24 +74,25 @@ public class HelpHeaderPanel extends JPanel {
      */
     public void htmlWizard(String htmlSupportedTitle, String htmlSupportedHelpText) {
         setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.NORTH, GridBagConstraints.NONE, null, 0,0);
-        c.insets = new Insets(10,15,10,15);
+        GridBagConstraints c = new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.NONE, null, 0, 0);
 
         setBackground(Color.white);
-        /**
-         * right icon image
-         */
-        JLabel wizardLabel = new JLabel(m_icon);
-        wizardLabel.setPreferredSize(new Dimension(30, 30));
-        add(wizardLabel, c);
+        if (m_icon != null) {
+            c.insets = new Insets(10, 15, 10, 15);
+            /**
+             * right icon image
+             */
+            JLabel wizardLabel = new JLabel(m_icon);
+            wizardLabel.setPreferredSize(new Dimension(30, 30));
+            add(wizardLabel, c);
 
-        c.gridx = 1;
-        c.fill = GridBagConstraints.BOTH;
-        c.anchor = GridBagConstraints.NORTHWEST;
-        c.weightx = 1;
-        c.weighty = 1;
-        c.insets = new Insets(5,5,10,5);
-
+            c.gridx = 1;
+            c.fill = GridBagConstraints.BOTH;
+            c.anchor = GridBagConstraints.NORTHWEST;
+            c.weightx = 1;
+            c.weighty = 1;
+        }
+        c.insets = new Insets(5, 5, 10, 5);
         JTextPane wizardPane = new JTextPane();
         wizardPane.setEditable(false);
         wizardPane.setContentType("text/html");
@@ -115,7 +115,7 @@ public class HelpHeaderPanel extends JPanel {
 //        this.setBorder(BorderFactory.createCompoundBorder(raisedbevel, loweredbevel));
         this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
     }
-
+    
     private String htmlModel() {
         return "<html> \n"
                 + "   <head>\n"
@@ -138,13 +138,13 @@ public class HelpHeaderPanel extends JPanel {
                 + "      </style> \n"
                 + "   </head> \n"
                 + "   <body>\n"
-                + "	%s <!-- title --> \n"               
+                + "	%s <!-- title --> \n"
                 + "	%s <!-- help text -->\n"
                 + "   </body> \n"
                 + "</html>";
     }
 
-    private String removeHtmlTag(String txt) {
+    static private String removeHtmlTag(String txt) {
         if (txt != null && !txt.isEmpty()) {
             if (txt.contains("<html>") || txt.contains("</html>")) {
                 String newTxt = txt.replaceAll("<html>", "").replaceAll("</html>", "");

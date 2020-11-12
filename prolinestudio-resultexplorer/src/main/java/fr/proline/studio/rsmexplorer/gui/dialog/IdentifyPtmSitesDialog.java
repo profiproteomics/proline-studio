8298930fc25588ca55529b718a048521ce0987d4
cbruley <christophe.bruley@cea.fr>
2020-11-12 14:32:28 +0100

    - improved GUI of the identify PTM dialog

--- prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/IdentifyPtmSitesDialog.java
+++ prolinestudio-resultexplorer/src/main/java/fr/proline/studio/rsmexplorer/gui/dialog/IdentifyPtmSitesDialog.java
@@ -32,7 +32,9 @@ public class IdentifyPtmSitesDialog extends DefaultDialog {
   private List<Ptm> m_ptms;
   private List<Ptm> m_selectedPtms;
 
-  private JComboBox<String> m_clusteringMethodCbx;
+  private JLabel m_clusteringMethod;
+  private JRadioButton m_clusterizePartiallyIsomorph;
+  private JRadioButton m_clusterizeIsomorphOnly;
 
   public IdentifyPtmSitesDialog(Window parent, List<Ptm> ptms) {
     super(parent, Dialog.ModalityType.APPLICATION_MODAL);
@@ -42,8 +44,8 @@ public class IdentifyPtmSitesDialog extends DefaultDialog {
 
     setTitle("Identify Ptm sites");
     setHelpHeaderText("Select the list of modifications of interest <br>" +
-            "(other modifications will be ignored during clustering) and the name <br>" +
-            "of the method that will be used to clusterize modification sites.</li></ul>");
+            "(other modifications will be ignored during clustering) and set <br>" +
+            "the method's parameters that will be used to clusterize modification sites.</li></ul>");
     initInternalPanel();
     pack();
   }
@@ -53,31 +55,46 @@ public class IdentifyPtmSitesDialog extends DefaultDialog {
     JPanel parametersPanel = new JPanel();
     parametersPanel.setLayout(new java.awt.GridBagLayout());
 
+    JPanel clusteringMethodPanel = new JPanel(new GridBagLayout());
+    clusteringMethodPanel.setBorder(BorderFactory.createTitledBorder("Clustering method"));
     GridBagConstraints c = new GridBagConstraints();
+    c.gridx = 0;
+    c.gridy = 0;
+    c.weightx = 1.0;
     c.anchor = GridBagConstraints.NORTHWEST;
     c.fill = GridBagConstraints.HORIZONTAL;
     c.insets = new java.awt.Insets(5, 5, 5, 5);
 
+    m_clusteringMethod = new JLabel("<html><b>Rely on localizations inferred from PSMs</b>: this clustering method <br/>" +
+            "use the site localization inferred from the validated PSMs, regardless <br/>of the reported localization probabilities.</html>");
+    clusteringMethodPanel.add(m_clusteringMethod, c);
+
+    m_clusterizePartiallyIsomorph = new JRadioButton("Group fully cleaved with missed cleaved peptides in same cluster");
+    c.gridy++;
+    c.insets = new java.awt.Insets(5, 15, 5, 5);
+    clusteringMethodPanel.add(m_clusterizePartiallyIsomorph, c);
+
+    m_clusterizeIsomorphOnly = new JRadioButton("Separate fully cleaved and missed cleaved peptides in different clusters");
+    c.gridy++;
+    c.insets = new java.awt.Insets(5, 15, 5, 5);
+    clusteringMethodPanel.add(m_clusterizeIsomorphOnly, c);
+
+    ButtonGroup group = new ButtonGroup();
+    group.add(m_clusterizePartiallyIsomorph);
+    group.add(m_clusterizeIsomorphOnly);
+
+    m_clusterizePartiallyIsomorph.setSelected(true);
+
+    JPanel ptmOIPanel = new JPanel(new GridBagLayout());
+    ptmOIPanel.setBorder(BorderFactory.createTitledBorder("PTMs of interest"));
+    c = new GridBagConstraints();
     c.gridx = 0;
     c.gridy = 0;
-    c.gridwidth = 1;
-
-    JLabel label = new JLabel("Clustering method:");
-    m_clusteringMethodCbx = new JComboBox<>(new String[] {"Exact Position Matching", "Isomorphic Matching"});
-    m_clusteringMethodCbx.setEnabled(true);
-    
-    parametersPanel.add(label, c);
-    c.gridx++;
-    parametersPanel.add(m_clusteringMethodCbx, c);
-
-    c.gridx = 0;
-    c.gridy++;
-    label = new JLabel("PTMs of interest:");
-    parametersPanel.add(label, c);
-
+    c.weightx = 1.0;
+    c.anchor = GridBagConstraints.NORTHWEST;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.insets = new java.awt.Insets(5, 15, 5, 5);
     for (Ptm ptm : m_ptms) {
-      c.gridx = 1;
-      c.gridy++;
       JCheckBox checkbox = new JCheckBox(ptm.getFullName(), true);
       checkbox.addActionListener(evt -> {
         if (((JCheckBox)evt.getSource()).isSelected()) {
@@ -86,12 +103,23 @@ public class IdentifyPtmSitesDialog extends DefaultDialog {
           m_selectedPtms.remove(ptm);
         }
       });
-      parametersPanel.add(checkbox, c);
+      ptmOIPanel.add(checkbox, c);
+      c.gridy++;
     }
+
+    c = new GridBagConstraints();
+    c.anchor = GridBagConstraints.NORTHWEST;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.insets = new java.awt.Insets(5, 5, 5, 5);
+    c.gridx = 0;
+    c.gridy = 0;
+
+    parametersPanel.add(clusteringMethodPanel, c);
+    c.gridy++;
+    parametersPanel.add(ptmOIPanel, c);
 
     JPanel internalPanel = new JPanel(new BorderLayout());
     internalPanel.add(parametersPanel, BorderLayout.CENTER);
-
     setInternalComponent(internalPanel);
   }
 
@@ -100,7 +128,7 @@ public class IdentifyPtmSitesDialog extends DefaultDialog {
   }
 
   public String getClusteringMethodName() {
-    return m_clusteringMethodCbx.getItemAt(m_clusteringMethodCbx.getSelectedIndex()).toUpperCase().replaceAll(" ", "_");
+//    return m_clusteringMethodCbx.getItemAt(m_clusteringMethodCbx.getSelectedIndex()).toUpperCase().replaceAll(" ", "_");
+    return (m_clusterizeIsomorphOnly.isSelected()) ? "ISOMORPHIC_MATCHING" : "EXACT_POSITION_MATCHING";
   }
-
 }

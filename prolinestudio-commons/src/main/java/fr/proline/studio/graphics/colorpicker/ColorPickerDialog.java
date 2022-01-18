16620ae0f5f331810fa8e37f9e415003a1745537
VeroDup <veronique.dupierris@cea.fr>
2022-01-18 15:16:55 +0100

    Copyright: Change header (rem VD225637)

--- prolinestudio-commons/src/main/java/fr/proline/studio/graphics/colorpicker/ColorPickerDialog.java
+++ prolinestudio-commons/src/main/java/fr/proline/studio/graphics/colorpicker/ColorPickerDialog.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2019 VD225637
+ * Copyright (C) 2019
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the CeCILL FREE SOFTWARE LICENSE AGREEMENT

c0f94a8f1370344cf685f1e5d851ed14eedd2fa9
VeroDup <veronique.dupierris@cea.fr>
2022-07-29 10:43:30 +0200

    Converter: Conversion without upload failed + 1.1 support allow 2 kind of dist, profiles full (default) & standalone ; to be used respectivly for Builder or for test (no R, no JRE, no Doc)
    (cherry picked from commit 7efbf3bb3068faf6dfcc83d12a9c2af4567a8cd7)

--- prolinestudio-resultexplorer/src/main/resources/start_studio.bat
+++ prolinestudio-resultexplorer/src/main/resources/start_studio.bat
@@ -1,2 +1,2 @@
 title Proline Studio
-java -Xmx2G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30 -cp "prolinestudio-resultexplorer-${pom.version}.jar;lib/*"  -Djava.library.path=./sqlite4java -Dlogback.configurationFile=config/logback.xml fr.proline.studio.main.Main %*
+${java.token}java -Xmx2G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30 -cp "prolinestudio-resultexplorer-${pom.version}.jar;lib/*"  -Djava.library.path=./sqlite4java -Dlogback.configurationFile=config/logback.xml fr.proline.studio.main.Main %*
